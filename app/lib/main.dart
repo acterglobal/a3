698484af10afba0596da3b5ac74696242fee3efd
Benjamin Kampmann <ben@gnunicorn.org>
2022-01-10 16:11:20 +0100

    app scaffold

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,115 +1,139 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(Effektio());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Effektio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Effektio',
+      home: EffektioHome(),
+    );
+  }
+}
+
+class EffektioHome extends StatefulWidget {
+  @override
+  _EffektioHomeState createState() => _EffektioHomeState();
+}
+
+class _EffektioHomeState extends State<EffektioHome> {
+  int _currentIndex = 0;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have clicked the button this many times:',
+        restorationScopeId: 'effektio',
+        // title: GalleryLocalizations.of(context).starterAppTitle,
+        title: "Effektio",
+        debugShowCheckedModeBanner: false,
+        //localizationsDelegates: GalleryLocalizations.localizationsDelegates,
+        //supportedLocales: GalleryLocalizations.supportedLocales,
+        // locale: GalleryOptions.of(context).locale,
+        home: Scaffold(
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+              primary: false,
+              backgroundColor: Colors.transparent,
+              foregroundColor: Colors.blue,
+              elevation: 0.0,
+              shadowColor: Colors.transparent,
+              actions: [
+                Icon(Icons.login_outlined),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Icon(Icons.settings),
+                ),
+              ]),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: const <Widget>[
+                UserAccountsDrawerHeader(
+                    accountName: Text("Ben"),
+                    accountEmail: Text("ben:effektio.org"),
+                    currentAccountPicture: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "https://matrix-client.matrix.org/_matrix/media/r0/download/matrix.org/ABFEXSDrESxovWwEnCYdNcHT"),
+                    )),
+                ListTile(
+                  leading: Icon(Icons.task_alt_outlined),
+                  title: Text('Tasks'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.calendar_today_outlined),
+                  title: Text('Events'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.real_estate_agent_outlined),
+                  title: Text('Resources'),
+                ),
+                Divider(),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _currentIndex,
+            backgroundColor: Color(0xFF6200EE),
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white.withOpacity(.6),
+            selectedFontSize: 0,
+            unselectedFontSize: 0,
+            onTap: (value) {
+              // Respond to item press.
+              setState(() => _currentIndex = value);
+            },
+            items: [
+              BottomNavigationBarItem(
+                label: 'News',
+                icon: Icon(Icons.dashboard),
+              ),
+              BottomNavigationBarItem(
+                label: 'FAQ',
+                icon: Icon(Icons.help_outline),
+              ),
+              BottomNavigationBarItem(
+                label: 'Community',
+                icon: Icon(Icons.groups),
+              ),
+              BottomNavigationBarItem(
+                label: 'Chat',
+                icon: Icon(Icons.chat),
+              ),
+            ],
+          ),
+          body: const Center(
+            child: Text('Hello World'),
+          ),
+        )
+
+        // theme: ThemeData(
+        //   primaryColor: _primaryColor,
+        //   highlightColor: Colors.transparent,
+        //   colorScheme: const ColorScheme(
+        //     primary: _primaryColor,
+        //     primaryContainer: Color(0xFF3700B3),
+        //     secondary: Color(0xFF03DAC6),
+        //     secondaryContainer: Color(0xFF018786),
+        //     background: Colors.white,
+        //     surface: Colors.white,
+        //     onBackground: Colors.black,
+        //     error: Color(0xFFB00020),
+        //     onError: Colors.white,
+        //     onPrimary: Colors.white,
+        //     onSecondary: Colors.black,
+        //     onSurface: Colors.black,
+        //     brightness: Brightness.light,
+        //   ),
+        // dividerTheme: const DividerThemeData(
+        //   thickness: 1,
+        //   color: Color(0xFFE5E5E5),
+        // ),
+        // platform: GalleryOptions.of(context).platform,
+        );
   }
 }
