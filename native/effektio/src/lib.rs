5e0005759657f0f306ba87d62c8dafd5dc49704d
Benjamin Kampmann <ben@effektio.org>
2022-02-17 21:22:13 +0100

    fixing bad lint

--- native/effektio/src/lib.rs
+++ native/effektio/src/lib.rs
@@ -1,5 +1,5 @@
 #![feature(vec_into_raw_parts)]
-#![allow(unused, dead_code, clippy::clippy::transmutes_expressible_as_ptr_casts)]
+#![allow(unused, dead_code, clippy::transmutes_expressible_as_ptr_casts)]
 
 pub use matrix_sdk;
 
