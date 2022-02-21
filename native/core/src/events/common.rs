412d3a06a32f15f609c0eaa0b744b1db4b33bff1
Benjamin Kampmann <ben@effektio.org>
2022-02-21 19:03:52 +0100

    adding todos

--- native/core/src/events/common.rs
+++ native/core/src/events/common.rs
@@ -1,12 +1,27 @@
-use matrix_sdk::ruma::events::macros::EventContent;
+pub use chrono::{DateTime, Utc};
+pub use chrono_tz::Tz as TimeZone;
+pub use csscolorparser::Color;
+use matrix_sdk::ruma::{events::macros::EventContent, EventId};
 use serde::{Deserialize, Serialize};
+
+/// Default UTC Datetime Object
+pub type UtcDateTime = DateTime<Utc>;
 
 /// Customize the color scheme
 #[derive(Clone, Debug, Deserialize, Serialize, EventContent)]
 #[ruma_event(type = "org.effektio.dev.colors")]
 pub struct Colorize {
     /// The foreground color to be used, as HEX
-    pub color: Option<String>,
+    pub color: Option<Color>,
     /// The background color to be used, as HEX
-    pub background: Option<String>,
+    pub background: Option<Color>,
 }
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(tag = "rel_type", rename = "m.thread")]
+pub struct InThread {
+    /// The event this event archives.
+    pub event_id: Box<EventId>,
+}
+
+pub type BelongsTo = InThread;
--- native/core/src/events/mod.rs
+++ native/core/src/events/mod.rs
@@ -4,7 +4,11 @@ pub use matrix_sdk::ruma::events::room::message::{
 
 mod common;
 mod news;
+mod todos;
 
-pub use common::Colorize;
-
+pub use common::{BelongsTo, Color, Colorize, TimeZone, UtcDateTime};
 pub use news::{NewsContentType, NewsEvent, NewsEventDevContent};
+pub use todos::{
+    Priority as TaskPriority, SpecialTaskListRole, Task, TaskDevContent, TaskList,
+    TaskListDevContent,
+};
