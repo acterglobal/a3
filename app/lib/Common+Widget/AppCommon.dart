4d3c939436e81cf98e5b7d68f00cdd5e8b01ec7b
Benjamin Kampmann <ben@effektio.org>
2022-03-03 18:54:00 +0100

    Importing Login and Profile Screens

---
+++ app/lib/Common+Widget/AppCommon.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Widget navBarTitle(String title) {
+  return Text(title,
+      style: GoogleFonts.montserrat(
+          fontSize: 20, fontWeight: FontWeight.w700, color: Colors.white));
+}
