6322b41dcb881644d5f570eab1d8de4ec8dec89b
Talha Gondal <gtalha@rocketmail.com>
2022-03-28 02:40:32 +0500

    delete HomeTabBar.dart (useless)

--- app/lib/screens/OnboardingScreens/Signup.dart
+++ app/lib/screens/OnboardingScreens/Signup.dart
@@ -2,7 +2,6 @@
 
 import 'package:effektio/common/store/Colors.dart';
 import 'package:effektio/common/widget/OnboardingWidget.dart';
-import 'package:effektio/screens/HomeScreens/HomeTabBar.dart';
 import 'package:effektio/screens/OnboardingScreens/LogIn.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
