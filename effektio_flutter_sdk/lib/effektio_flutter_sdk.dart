fc5274151d2fa68b1ae9f8307ff74ed8b0bea4c0
Benjamin Kampmann <ben@effektio.org>
2022-04-27 00:16:53 +0200

    integration testing setup

--- effektio_flutter_sdk/lib/effektio_flutter_sdk.dart
+++ effektio_flutter_sdk/lib/effektio_flutter_sdk.dart
@@ -11,6 +11,8 @@ export './effektio_flutter_sdk_ffi.dart' show Client;
 //   EffektioClient(this.client);
 // }
 
+const DEFAULT_SERVER = String.fromEnvironment("DEFAULT_EFFEKTIO_SERVER", defaultValue: 'https://matrix.effektio.org')
+
 class EffektioSdk {
   static EffektioSdk? _instance;
   late final Api _api;
@@ -44,7 +46,7 @@ class EffektioSdk {
 
     if (_clients.isEmpty) {
       Client client =
-          await _api.guestClient(appDocPath, 'https://matrix.effektio.org');
+          await _api.guestClient(appDocPath, DEFAULT_SERVER);
       clients.add(client);
       loggedIn = await client.loggedIn();
       await _persistSessions();
