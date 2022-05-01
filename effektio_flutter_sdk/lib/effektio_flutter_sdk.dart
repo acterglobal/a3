ef5f24a8c48236e852e8b810b01eaa0a29ec3935
Benjamin Kampmann <ben@effektio.org>
2022-05-01 14:59:05 +0200

    fix typo

--- effektio_flutter_sdk/lib/effektio_flutter_sdk.dart
+++ effektio_flutter_sdk/lib/effektio_flutter_sdk.dart
@@ -11,7 +11,8 @@ export './effektio_flutter_sdk_ffi.dart' show Client;
 //   EffektioClient(this.client);
 // }
 
-const DEFAULT_SERVER = String.fromEnvironment("DEFAULT_EFFEKTIO_SERVER", defaultValue: 'https://matrix.effektio.org')
+const DEFAULT_SERVER = String.fromEnvironment("DEFAULT_EFFEKTIO_SERVER",
+    defaultValue: 'https://matrix.effektio.org');
 
 class EffektioSdk {
   static EffektioSdk? _instance;
@@ -45,8 +46,7 @@ class EffektioSdk {
     }
 
     if (_clients.isEmpty) {
-      Client client =
-          await _api.guestClient(appDocPath, DEFAULT_SERVER);
+      Client client = await _api.guestClient(appDocPath, DEFAULT_SERVER);
       clients.add(client);
       loggedIn = await client.loggedIn();
       await _persistSessions();
