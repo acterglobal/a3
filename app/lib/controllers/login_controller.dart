f5712419b38a6f8ad23d0f1a35be838a8480b8a1
Talha Gondal <gtalha@rocketmail.com>
2022-06-18 02:33:49 +0500

    extending getX use to login/signup, remove bloc dependency

---
+++ app/lib/controllers/login_controller.dart
@@ -0,0 +1,37 @@
+import 'package:effektio_flutter_sdk/effektio_flutter_sdk.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  TextEditingController username = TextEditingController();
+  TextEditingController password = TextEditingController();
+  String isException = '';
+  bool isSubmitting = false;
+
+  Future<Client> login(String username, String password) async {
+    update();
+    final sdk = await EffektioSdk.instance;
+    if (!username.contains(':')) {
+      username = '$username:effektio.org';
+    }
+    if (!username.startsWith('@')) {
+      username = '@$username';
+    }
+    Client client = await sdk.login(username, password);
+    return client;
+  }
+
+  Future<bool> loginSubmitted() async {
+    try {
+      await login(username.text, password.text);
+      isSubmitting = false;
+      update();
+      return true;
+    } catch (e) {
+      isException = e.toString();
+      isSubmitting = false;
+      update();
+      return false;
+    }
+  }
+}
