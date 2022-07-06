072999614fa158c55054db6cbf765c189b582e0c
Benjamin Kampmann <ben@effektio.org>
2022-07-06 20:23:51 +0200

    fixing integration test

--- native/effektio/tests/integration_smoketest.rs
+++ native/effektio/tests/integration_smoketest.rs
@@ -4,7 +4,7 @@ use effektio::api::guest_client;
 #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
 #[ignore]
 async fn can_guest_login() -> Result<()> {
-    let client = guest_client(
+    let _client = guest_client(
         "test".to_string(),
         option_env!("HOMESERVER")
             .unwrap_or("http://localhost:8008")
