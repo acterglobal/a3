c5c3fff212e7862c464c788eeef4d50aec0d425a
sachintha-s <65540448+sachintha-s@users.noreply.github.com>
2022-07-20 10:57:39 +0530

    Custom chat input widget added

--- app/lib/common/store/separatedThemes.dart
+++ app/lib/common/store/separatedThemes.dart
@@ -4,6 +4,8 @@ import 'package:themed/themed.dart';
 class AppCommonTheme {
   static const primaryColor = ColorRef(Color(0xffEC2758));
   static const backgroundColor = ColorRef(Color.fromRGBO(36, 38, 50, 1));
+   static const backgroundColorLight =
+      ColorRef(Color(0xff333540), id: 'CIBC');
   static const svgIconColor = ColorRef(Colors.white, id: 'NavBar');
   static const textFieldColor = ColorRef(Color(0xff171717));
   static const darkShade = ColorRef(Color(0xff333540));
@@ -111,6 +113,7 @@ class ChatTheme01 {
   static const chatBodyTextColor = ColorRef(Colors.white, id: 'CBT');
   static const leaveBtnBg = ColorRef(Color(0xff594848));
   static const redText = ColorRef(Color(0xffFF4B4B));
+  static const chatInputTextColor = ColorRef(Colors.white, id: 'CITXC');
 
   static const chatTitleStyle = TextStyleRef(
     TextStyle(
@@ -120,6 +123,14 @@ class ChatTheme01 {
     ),
   );
 
+  static const chatInputPlaceHolderStyle = TextStyleRef(
+    TextStyle(
+      fontSize: 18,
+      color: ColorRef(
+        Color.fromARGB(255, 103, 104, 107),
+      ),
+    ),
+  );
   static const chatProfileTitleStyle = TextStyleRef(
     TextStyle(
       fontSize: 22,
