bdee234189880053fae1111185b2956323f720e6
bitfriend <bitcapture@hotmail.com>
2022-08-17 02:57:54 +0800

    Exclude session verification from integration test for git runner

--- native/effektio-test/src/lib.rs
+++ native/effektio-test/src/lib.rs
@@ -1,8 +1,8 @@
 #[cfg(test)]
 mod auth;
-// #[cfg(test)]
-// mod news;
 #[cfg(test)]
-mod session_verification;
+mod news;
 // #[cfg(test)]
-// mod typing_notification;
+// mod session_verification;
+#[cfg(test)]
+mod typing_notification;
