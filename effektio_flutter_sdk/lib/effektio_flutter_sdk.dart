c2fd59be70e8ea9c4748f1d8b919bb945ca07511
Talha Gondal <gtalha@rocketmail.com>
2022-08-31 15:36:45 +0500

    use material import

--- effektio_flutter_sdk/lib/effektio_flutter_sdk.dart
+++ effektio_flutter_sdk/lib/effektio_flutter_sdk.dart
@@ -1,10 +1,9 @@
 import 'dart:core';
 import 'dart:ffi';
 import 'dart:io';
-import 'dart:ui';
 import 'package:effektio_flutter_sdk/effektio_flutter_sdk_ffi.dart' as ffi;
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 import 'package:package_info/package_info.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
