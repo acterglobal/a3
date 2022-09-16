0b4eea1dcbcb88022fd25a67437efd8384f9ba77
bitfriend <bitcapture@hotmail.com>
2022-09-17 05:12:33 +0800

    Fix lint error from git runner

--- app/lib/screens/HomeScreens/ChatList.dart
+++ app/lib/screens/HomeScreens/ChatList.dart
@@ -12,7 +12,7 @@ import 'package:themed/themed.dart';
 
 class ChatList extends StatefulWidget {
   const ChatList({Key? key, required this.client}) : super(key: key);
-  final Future<Client> client;
+  final Client client;
 
   @override
   State<ChatList> createState() => _ChatListState();
@@ -140,78 +140,30 @@ class _ChatListState extends State<ChatList> {
   }
 
   Widget buildJoinedRooms() {
-    return FutureBuilder<Client>(
-      future: widget.client,
-      builder: (BuildContext context, AsyncSnapshot<Client> snapshot) {
-        if (snapshot.connectionState != ConnectionState.done) {
-          return SizedBox(
-            height: MediaQuery.of(context).size.height / 1.5,
-            width: MediaQuery.of(context).size.width,
-            child: Center(
-              child: CircularProgressIndicator(
-                color: AppCommonTheme.primaryColor,
-              ),
+    return FutureBuilder<FfiListConversation>(
+      future: widget.client.conversations(),
+      builder: (
+        BuildContext context,
+        AsyncSnapshot<FfiListConversation> snapshot,
+      ) {
+        if (snapshot.hasData) {
+          return Flexible(
+            child: ChatOverview(
+              user: user,
+              rooms: snapshot.requireData.toList(),
             ),
           );
-        }
-        if (snapshot.hasData) {
-          return FutureBuilder<FfiListConversation>(
-            future: snapshot.requireData.conversations(),
-            builder: (
-              BuildContext context,
-              AsyncSnapshot<FfiListConversation> snapshot,
-            ) {
-              if (snapshot.hasData) {
-                return Flexible(
-                  child: ChatOverview(
-                    user: user,
-                    rooms: snapshot.requireData.toList(),
-                  ),
-                );
-              } else {
-                return Center(
-                  child: Container(
-                    height: MediaQuery.of(context).size.height,
-                    width: MediaQuery.of(context).size.width,
-                    color: AppCommonTheme.backgroundColor,
-                    child: Text(
-                      AppLocalizations.of(context)!.loadingConvo,
-                      style: ChatTheme01.emptyMsgTitle,
-                    ),
-                  ),
-                );
-              }
-            },
-          );
         } else {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: MediaQuery.of(context).size.height / 6),
-              Center(
-                child: Container(
-                  child: SvgPicture.asset('assets/images/empty_messages.svg'),
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                AppLocalizations.of(context)!.loadingConvo + '...',
+          return Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              color: AppCommonTheme.backgroundColor,
+              child: Text(
+                AppLocalizations.of(context)!.loadingConvo,
                 style: ChatTheme01.emptyMsgTitle,
               ),
-              SizedBox(height: 5),
-              Center(
-                child: Container(
-                  height: MediaQuery.of(context).size.height / 3,
-                  width: MediaQuery.of(context).size.width / 1.5,
-                  child: Text(
-                    'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                    style: ChatTheme01.chatBodyStyle,
-                    overflow: TextOverflow.clip,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ],
+            ),
           );
         }
       },
