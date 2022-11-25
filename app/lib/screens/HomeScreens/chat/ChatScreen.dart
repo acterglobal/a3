b5de50ef31234e22c412580ea86d02bb4010eeaa
Talha Gondal <gtalha@rocketmail.com>
2022-11-25 17:31:31 +0500

    fix lint

--- app/lib/screens/HomeScreens/chat/ChatScreen.dart
+++ app/lib/screens/HomeScreens/chat/ChatScreen.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'dart:math';
-import 'dart:typed_data';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:effektio/common/store/themes/ChatTheme.dart';
