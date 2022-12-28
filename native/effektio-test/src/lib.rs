165f6d91fa89cd4f916ab0bf21072234e342b273
Benjamin Kampmann <ben@effektio.org>
2022-12-28 19:58:00 +0100

    move integration tests into subfolder

--- native/effektio-test/src/lib.rs
+++ native/effektio-test/src/lib.rs
@@ -1,14 +1,2 @@
-// #[cfg(test)]
-// mod auth;
-// #[cfg(test)]
-// mod formatted_body;
 #[cfg(test)]
-mod invitation;
-// #[cfg(test)]
-// mod news;
-// #[cfg(test)]
-// mod receipt;
-// #[cfg(test)]
-// mod typing;
-// #[cfg(test)]
-// mod verification;
+mod tests;
---
+++ native/effektio-test/src/tests.rs
@@ -0,0 +1,14 @@
+// #[cfg(test)]
+// mod auth;
+// #[cfg(test)]
+// mod formatted_body;
+#[cfg(test)]
+mod invitation;
+// #[cfg(test)]
+// mod news;
+// #[cfg(test)]
+// mod receipt;
+// #[cfg(test)]
+// mod typing;
+// #[cfg(test)]
+// mod verification;
