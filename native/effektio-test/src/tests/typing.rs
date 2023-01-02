2185e724de3f3448028215602592acd46fe556a8
bitfriend <bitcapture@hotmail.com>
2023-01-02 22:17:04 +0800

    Apply device name to integration test

--- native/effektio-test/src/tests/typing.rs
+++ native/effektio-test/src/tests/typing.rs
@@ -12,7 +12,7 @@ async fn kyra_detects_sisko_typing() -> Result<()> {
         tmp_dir.path().to_str().expect("always works").to_string(),
         "@sisko:ds9.effektio.org".to_string(),
         "sisko".to_string(),
-        None,
+        Some("SISKO_DEV".to_string()),
     )
     .await?;
     let sisko_syncer = sisko.start_sync();
@@ -30,7 +30,7 @@ async fn kyra_detects_sisko_typing() -> Result<()> {
         tmp_dir.path().to_str().expect("always works").to_string(),
         "@kyra:ds9.effektio.org".to_string(),
         "kyra".to_string(),
-        None,
+        Some("KYRA_DEV".to_string()),
     )
     .await?;
     let kyra_syncer = kyra.start_sync();
