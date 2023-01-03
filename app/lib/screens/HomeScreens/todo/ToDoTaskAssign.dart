ba42f38c2e7ec261a5fdb9a4f9a202af432a5086
inderjeet-developer7 <65702289+inderjeet-developer7@users.noreply.github.com>
2023-01-03 19:17:29 +0530

    Move dialog to seperate widget

--- app/lib/screens/HomeScreens/todo/ToDoTaskAssign.dart
+++ app/lib/screens/HomeScreens/todo/ToDoTaskAssign.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:effektio/common/store/MockData.dart';
 import 'package:effektio/common/store/themes/SeperatedThemes.dart';
+import 'package:effektio/widgets/InviteUser.dart';
 import 'package:flutter/material.dart';
 
 class ToDoTaskAssignScreen extends StatelessWidget {
@@ -186,74 +187,10 @@ class ToDoTaskAssignScreen extends StatelessWidget {
     showDialog(
       context: context,
       builder: (BuildContext ctx) {
-        return Dialog(
-          backgroundColor: ToDoTheme.backgroundGradientColor,
-          child: Wrap(
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16.0,
-                  vertical: 24.0,
-                ),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Invite Friends',
-                        style: ToDoTheme.titleTextStyle.copyWith(fontSize: 18),
-                      ),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        'You can invite your friends to ToDo today via',
-                        textAlign: TextAlign.center,
-                        style: ToDoTheme.subtitleTextStyle.copyWith(
-                            color: ToDoTheme.calendarColor,
-                            fontSize: 15,),
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      Column(
-                        children: [
-                          buildDivider(),
-                          Text('Whatsapp', style : ToDoTheme.titleTextStyle.copyWith(fontSize: 16)),
-                          buildDivider(),
-                          Text('Email', style : ToDoTheme.titleTextStyle.copyWith(fontSize: 16)),
-                          buildDivider(),
-                          Text('SMS', style : ToDoTheme.titleTextStyle.copyWith(fontSize: 16)),
-                          buildDivider(),
-                          Text('Invitation Link', style : ToDoTheme.titleTextStyle.copyWith(fontSize: 16)),
-                          buildDivider(),
-                          GestureDetector(
-                              onTap: (){
-                                Navigator.pop(context);
-                              },
-                              child: Text('Cancel', style : ToDoTheme.titleTextStyle.copyWith(fontSize: 16, color: Colors.red)),)
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-        );
+        return InviteUserDialog();
       },
     );
   }
 
-  Widget buildDivider() {
-    return const Padding(
-      padding: EdgeInsets.symmetric(vertical: 12.0),
-      child: Divider(
-        height: 2,
-        indent: 0,
-        endIndent: 0,
-        color: Colors.grey,
-      ),
-    );
-  }
+
 }
