f4b9b4a39eb3fb59f7bb82decd235dff1f89c26b
Talha Gondal <gtalha@rocketmail.com>
2023-01-06 21:22:03 +0500

    fix lint

--- app/lib/screens/HomeScreens/chat/ChatScreen.dart
+++ app/lib/screens/HomeScreens/chat/ChatScreen.dart
@@ -22,8 +22,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_link_previewer/flutter_link_previewer.dart';
-import 'package:flutter_matrix_html/flutter_html.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:string_validator/string_validator.dart';
@@ -289,27 +287,6 @@ class _ChatScreenState extends State<ChatScreen> {
       },
     );
   }
-  //  Widget _linkPreview(
-  //   types.User user,
-  //   double width,
-  //   BuildContext context,
-  //   types.TextMessage message
-  // ) {
-
-  //   return LinkPreview(
-  //     enableAnimation: true,
-  //     previewData: message.previewData,
-  //     text:message.text,
-  //      onPreviewDataFetched: _onPreviewDataFetched,
-  //     textWidget:textMessageBuilder,
-  //     width: width,
-  //   );
-  // }
-  //  void _onPreviewDataFetched(types.PreviewData previewData,types.TextMessage message) {
-  //   if (message.previewData == null) {
-  //     roomController.handlePreviewDataFetched.call(message, previewData);
-  //   }
-  // }
 
   Widget textMessageBuilder(
     types.TextMessage p1, {
