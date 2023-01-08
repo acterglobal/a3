96b8888326fef354a2c06af3fe1d12eae4e5e8cb
Benjamin Kampmann <ben@effektio.org>
2023-01-08 17:00:57 +0100

    task comment tests case, too

--- native/effektio-test/src/tests/tasks.rs
+++ native/effektio-test/src/tests/tasks.rs
@@ -384,105 +384,109 @@ async fn task_lists_comments_smoketests() -> Result<()> {
         "I think this is very important".to_owned()
     );
 
-    // let task_1 = tasks[0].clone();
-    // assert_eq!(task_1.title(), &"Testing 1".to_owned());
-    // assert!(!task_1.is_done());
+    Ok(())
+}
 
-    // let task_list_listener = task_list.subscribe();
+#[tokio::test]
+async fn task_comment_smoketests() -> Result<()> {
+    let _ = env_logger::try_init();
+    let (mut user, room_id) = random_user_with_random_space("tasks_smoketest").await?;
+    let state_sync = user.start_sync();
+    state_sync.await_has_synced_history().await?;
+    let group = user.get_group(room_id.to_string()).await?;
 
-    // let task_2_id = task_list
-    //     .task_builder()?
-    //     .title("Testing 2".into())
-    //     .send()
-    //     .await?;
+    assert_eq!(
+        group.task_lists().await?.len(),
+        0,
+        "Why are there tasks in our fresh space!?!"
+    );
 
-    // assert!(
-    //     wait_for(move || {
-    //         let mut task_list_listener = task_list_listener.clone();
-    //         async move {
-    //             if let Ok(t) = task_list_listener.try_recv() {
-    //                 Ok(Some(t))
-    //             } else {
-    //                 Ok(None)
-    //             }
-    //         }
-    //     })
-    //     .await?
-    //     .is_some(),
-    //     "Didn't receive any update on the list for the second event"
-    // );
+    let task_list_id = {
+        let mut draft = group.task_list_draft()?;
+        draft.name("Starting up".to_owned());
+        draft.send().await?
+    };
 
-    // let task_list = task_list.refresh().await?;
-    // let tasks = task_list.tasks().await?;
-    // assert_eq!(tasks.len(), 2);
-    // assert_eq!(tasks[1].event_id(), task_2_id);
+    let task_list_key = effektio_core::models::TaskList::key_from_event(&task_list_id);
 
-    // let task_2 = tasks[1].clone();
-    // assert_eq!(task_2.title(), &"Testing 2".to_owned());
-    // assert!(!task_2.is_done());
+    let wait_for_group = group.clone();
+    let Some(task_list) = wait_for(move || {
+        let group = wait_for_group.clone();
+        let task_list_key = task_list_key.clone();
+        async move {
+            Ok(group.task_list(&task_list_key).await.ok())
+    }}).await? else {
+        bail!("freshly created Task List couldn't be found");
+    };
 
-    // let task_1_updater = task_1.subscribe();
+    assert_eq!(task_list.name(), &"Starting up".to_owned());
+    assert_eq!(task_list.tasks().await?.len(), 0);
 
-    // task_1
-    //     .update_builder()?
-    //     .title("Replacement Name".into())
-    //     .mark_done()
-    //     .send()
-    //     .await?;
+    let task_list_listener = task_list.subscribe();
 
-    // assert!(
-    //     wait_for(move || {
-    //         let mut task_1_updater = task_1_updater.clone();
-    //         async move {
-    //             if let Ok(t) = task_1_updater.try_recv() {
-    //                 Ok(Some(t))
-    //             } else {
-    //                 Ok(None)
-    //             }
-    //         }
-    //     })
-    //     .await?
-    //     .is_some(),
-    //     "Didn't receive any update on the task"
-    // );
+    let task_1_id = task_list
+        .task_builder()?
+        .title("Testing 1".into())
+        .send()
+        .await?;
 
-    // let task_1 = task_1.refresh().await?;
-    // // Update has been applied properly
-    // assert_eq!(task_1.title(), &"Replacement Name".to_owned());
-    // assert!(task_1.is_done());
+    assert!(
+        wait_for(move || {
+            let mut task_list_listener = task_list_listener.clone();
+            async move {
+                if let Ok(t) = task_list_listener.try_recv() {
+                    Ok(Some(t))
+                } else {
+                    Ok(None)
+                }
+            }
+        })
+        .await?
+        .is_some(),
+        "Didn't receive any update on the list for the first event"
+    );
 
-    // let task_list_listener = task_list.subscribe();
+    let task_list = task_list.refresh().await?;
+    let mut tasks = task_list.tasks().await?;
+    assert_eq!(tasks.len(), 1);
+    assert_eq!(tasks[0].event_id(), task_1_id);
 
-    // task_list
-    //     .update_builder()?
-    //     .name("Setup".into())
-    //     .description_text("All done now".into())
-    //     .send()
-    //     .await?;
+    let task = tasks.pop().unwrap();
 
-    // assert!(
-    //     wait_for(move || {
-    //         let mut task_list_listener = task_list_listener.clone();
-    //         async move {
-    //             if let Ok(t) = task_list_listener.try_recv() {
-    //                 Ok(Some(t))
-    //             } else {
-    //                 Ok(None)
-    //             }
-    //         }
-    //     })
-    //     .await?
-    //     .is_some(),
-    //     "Didn't receive the update on the task list"
-    // );
+    // START actual comment on task
 
-    // let task_list = task_list.refresh().await?;
+    let comments_manager = task.comments().await?;
+    assert!(!comments_manager.stats().has_comments());
 
-    // assert_eq!(task_list.name(), &"Setup".to_owned());
-    // assert_eq!(
-    //     task_list.description().as_ref().unwrap().body,
-    //     "All done now".to_owned()
-    // );
+    // ---- let's make a comment
+
+    let comments_listener = comments_manager.subscribe();
+    let comment_1_id = comments_manager
+        .comment_draft()?
+        .content_text("I updated the task".to_owned())
+        .send()
+        .await?;
+
+    assert!(
+        wait_for(move || {
+            let mut comments_listener = comments_listener.clone();
+            async move {
+                if let Ok(t) = comments_listener.try_recv() {
+                    Ok(Some(t))
+                } else {
+                    Ok(None)
+                }
+            }
+        })
+        .await?
+        .is_some(),
+        "Didn't receive any update on the list for the first event"
+    );
+
+    let comments = comments_manager.comments().await?;
+    assert_eq!(comments.len(), 1);
+    assert_eq!(comments[0].event_id(), comment_1_id);
+    assert_eq!(comments[0].content().body, "I updated the task".to_owned());
 
     Ok(())
 }
