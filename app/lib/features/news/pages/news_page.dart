f1d79dd47bacdc68e82ddb40e9fe76739e84274a
Talha <gtalha@rocketmail.com>
2023-03-15 18:10:26 +0500

    fix lint error

--- app/lib/features/news/pages/news_page.dart
+++ app/lib/features/news/pages/news_page.dart
@@ -1,12 +1,10 @@
 import 'package:acter/common/animations/like_animation.dart';
-import 'package:acter/common/dialogs/logout_confirmation.dart';
 import 'package:acter/common/themes/seperated_themes.dart';
 import 'package:acter/features/home/controllers/home_controller.dart';
 import 'package:acter/features/home/widgets/user_avatar.dart';
 import 'package:acter/features/news/controllers/news_controller.dart';
 import 'package:acter/features/news/widgets/news_item.dart';
 import 'package:flutter/material.dart';
-import 'package:acter/common/utils/constants.dart' show Keys;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class NewsPage extends ConsumerStatefulWidget {
