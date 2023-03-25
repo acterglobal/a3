cbe360e8ff8638b98516c12929bb47d0fffb8255
Benjamin Kampmann <ben@effektio.org>
2023-03-25 16:22:46 +0100

    links card

--- app/lib/features/space/pages/overview_page.dart
+++ app/lib/features/space/pages/overview_page.dart
@@ -1,4 +1,5 @@
 import 'package:acter/features/space/widgets/about_card.dart';
+import 'package:acter/features/space/widgets/links_card.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -22,7 +23,10 @@ class _SpaceOverviewState extends ConsumerState<SpaceOverview> {
     // get platform of context.
     return Container(
       padding: const EdgeInsets.all(20),
-      child: AboutCard(spaceId: widget.spaceIdOrAlias),
+      child: Column(children: [
+        AboutCard(spaceId: widget.spaceIdOrAlias),
+        LinksCard(spaceId: widget.spaceIdOrAlias),
+      ]),
       // Row(
       //   children: [
       //     Column(
