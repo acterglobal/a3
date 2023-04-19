cf47429ff02785f70c2d5e51fff6d56855c18a0f
Talha Gondal <gtalha@rocketmail.com>
2023-04-19 22:06:58 +0500

    avatar border and add create news icon

---
+++ app/lib/common/widgets/user_avatar.dart
@@ -0,0 +1,38 @@
+import 'package:acter/common/widgets/custom_avatar.dart';
+import 'package:acter/features/home/states/client_state.dart';
+import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart' show UserProfile;
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final userProfileProvider = FutureProvider<UserProfile>((ref) async {
+  final client = ref.watch(clientProvider);
+  return await client!.getUserProfile();
+});
+
+class UserAvatarWidget extends ConsumerWidget {
+  const UserAvatarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final client = ref.watch(clientProvider)!;
+    final userProfile = ref.watch(userProfileProvider);
+    return userProfile.when(
+      data: (data) {
+        return CustomAvatar(
+          uniqueKey: client.userId().toString(),
+          radius: 20,
+          isGroup: false,
+          cacheHeight: 120,
+          cacheWidth: 120,
+          avatar: data.getAvatar(),
+          displayName: data.getDisplayName(),
+          stringName: data.getDisplayName() ?? '',
+        );
+      },
+      error: (error, _) => const Text('Couldn\'t load avatar'),
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
--- app/lib/features/news/pages/news_page.dart
+++ app/lib/features/news/pages/news_page.dart
@@ -1,25 +1,71 @@
-import 'package:acter/features/home/widgets/user_avatar.dart';
+import 'package:acter/common/themes/app_theme.dart';
+import 'package:acter/features/home/states/client_state.dart';
+import 'package:acter/common/widgets/user_avatar.dart';
 import 'package:acter/features/news/widgets/news_widget.dart';
 import 'package:acter/features/news/widgets/stream_types_selection.dart';
+import 'package:atlas_icons/atlas_icons.dart';
+import 'package:colorize_text_avatar/colorize_text_avatar.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class NewsPage extends ConsumerWidget {
   const NewsPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final client = ref.watch(clientProvider)!;
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
         leading: MediaQuery.of(context).size.width < 600
-            ? const UserAvatarWidget()
+            ? !client.isGuest()
+                ? Container(
+                    margin: const EdgeInsets.only(left: 8, top: 5),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Theme.of(context).colorScheme.neutral6,
+                        width: 2,
+                      ),
+                      shape: BoxShape.circle,
+                    ),
+                    child: const UserAvatarWidget(),
+                  )
+                : GestureDetector(
+                    onTap: () => context.go('/login'),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(
+                          width: 40,
+                          height: 40,
+                          child: TextAvatar(
+                            backgroundColor: Colors.grey,
+                            text: 'G',
+                            numberLetters: 1,
+                            shape: Shape.Circular,
+                            upperCase: true,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
             : const SizedBox.shrink(),
         centerTitle: true,
         title: const StreamTypesSelection(),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Atlas.plus_circle_thin,
+              color: Theme.of(context).colorScheme.neutral5,
+              size: 36,
+            ),
+          ),
+        ],
       ),
       body: const NewsWidget(),
     );
