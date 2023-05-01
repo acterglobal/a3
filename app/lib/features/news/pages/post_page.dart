60b253f53395b8fb2dbf6279feaef1655b9b0310
Talha Gondal <gtalha@rocketmail.com>
2023-05-01 18:44:57 +0500

    implement static UI for post updates

--- app/lib/features/news/pages/post_page.dart
+++ app/lib/features/news/pages/post_page.dart
@@ -1,3 +1,4 @@
+import 'package:acter/common/themes/app_theme.dart';
 import 'package:atlas_icons/atlas_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,6 +12,11 @@ class PostPage extends ConsumerStatefulWidget {
 }
 
 class _PostPageState extends ConsumerState<PostPage> {
+  final TextEditingController titleController =
+      TextEditingController(text: 'Caption your update');
+  final TextEditingController descriptionController = TextEditingController(
+    text: 'Description',
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +33,236 @@ class _PostPageState extends ConsumerState<PostPage> {
         ),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: Container(
+                  color: Theme.of(context).colorScheme.neutral5,
+                  width: 100,
+                  height: 200,
+                ),
+                title: Text(
+                  'Title',
+                  style: Theme.of(context).textTheme.labelLarge,
+                ),
+                contentPadding: const EdgeInsets.symmetric(vertical: 0),
+                subtitle: TextFormField(
+                  controller: titleController,
+                  maxLines: 5,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium!
+                      .copyWith(color: Theme.of(context).colorScheme.neutral5),
+                  onChanged: (String val) {},
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                  ),
+                  cursorColor: Theme.of(context).colorScheme.tertiary,
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            indent: 10,
+            endIndent: 10,
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: descriptionController,
+                maxLines: 5,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.neutral5),
+                onChanged: (String val) {},
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                ),
+                cursorColor: Theme.of(context).colorScheme.tertiary,
+              ),
+            ),
+          ),
+          const Divider(
+            indent: 10,
+            endIndent: 10,
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundColor: Theme.of(context).colorScheme.neutral5,
+                    radius: 18,
+                  ),
+                  DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      items: const [],
+                      onChanged: (String? val) => {},
+                      icon: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 8),
+                            child: Text(
+                              'Select Space',
+                              style: Theme.of(context).textTheme.bodySmall,
+                            ),
+                          ),
+                          Icon(
+                            Icons.keyboard_arrow_down_outlined,
+                            color: Theme.of(context).colorScheme.neutral6,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Divider(
+            indent: 10,
+            endIndent: 10,
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {},
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.add,
+                      color: Theme.of(context).colorScheme.neutral6,
+                    ),
+                    const SizedBox(width: 10),
+                    Text(
+                      'Add Message Link',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            indent: 10,
+            endIndent: 10,
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {},
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      'Add Reminder',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    const Spacer(),
+                    Icon(
+                      Icons.keyboard_arrow_right,
+                      color: Theme.of(context).colorScheme.neutral6,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            indent: 10,
+            endIndent: 10,
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {},
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Atlas.curve_arrow_right,
+                      color: Theme.of(context).colorScheme.neutral6,
+                      size: 18,
+                    ),
+                    const SizedBox(width: 10),
+                    Text(
+                      'Share to',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Visibility(
+              visible: true,
+              child: Container(
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Save to Draft',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<BeveledRectangleBorder>(
+                          const BeveledRectangleBorder(),
+                        ),
+                        fixedSize: MaterialStateProperty.all<Size>(
+                          Size(MediaQuery.of(context).size.width * 0.45, 45),
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Next',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Theme.of(context).colorScheme.tertiary,
+                        ),
+                        shape:
+                            MaterialStateProperty.all<BeveledRectangleBorder>(
+                          const BeveledRectangleBorder(),
+                        ),
+                        fixedSize: MaterialStateProperty.all<Size>(
+                          Size(MediaQuery.of(context).size.width * 0.4, 45),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
