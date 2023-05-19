c5fedaf82edec7070cf002124e5ba7a45724b3e0
kokodavid <kingkgdave@gmail.com>
2023-05-19 11:10:55 +0300

    Space Card
    Create desktop version of the Space card

--- app/lib/features/space/widgets/space_card.dart
+++ app/lib/features/space/widgets/space_card.dart
@@ -4,94 +4,199 @@ import 'package:flutter/material.dart';
 
 import 'package:acter/common/widgets/square_avatar.dart';
 
-
 class SpaceCard extends StatelessWidget {
-  const SpaceCard({
-    super.key,
+
+   String? displayImage;
+   String? title;
+   String? subtitle;
+
+
+   SpaceCard({
+    super.key, this.displayImage,  this.title,  this.subtitle,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.primary,
-          borderRadius: BorderRadius.circular(10)),
-      width: 300,
-      child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              const SquareAvatar(
-                height: 76,
-                width: 77,
-                displayImage: 'assets/icon/logo.png',
-              ),
-              Text(
-                'OceanKAN Global',
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              const Text(
-                '#oceankan:acter.global',
-                style: TextStyle(color: Color(0xff79747E)),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  SpaceCardIcons(
-                    icon: Atlas.ticket_plus,
-                    title: 'Invite',
-                  ),
-                  SpaceCardIcons(
-                    icon: Atlas.link_chain,
-                    title: 'Copy link',
-                  ),
-                  SpaceCardIcons(
-                    icon: Atlas.bell_dots,
-                    title: 'Notifications',
-                  )
-                ],
-              ),
-              const Divider(
-                thickness: 0.5,
-                indent: 1,
-                endIndent: 1,
-      
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 10, bottom: 10),
-                child: Row(
-                  children: [
-                    Container(
-                        margin: const EdgeInsets.only(right: 10),
-                        child: const Icon(Atlas.padlock)),
-                    const Text('Encryption Settings')
-                  ],
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if(constraints.maxWidth < 600){
+          return Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primary,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          width: 300,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                 SquareAvatar(  
+                  height: 76,
+                  width: 77,
+                  displayImage: displayImage,
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 10),
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(right: 10),
-                      child: const Icon(
-                        Atlas.block_shield,
-                        color: Colors.red,
-                      ),
+                Text(
+                  title!,
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                 Text(
+                  subtitle!,
+                  style: TextStyle(color: Color(0xff79747E)),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    SpaceCardIcons(
+                      icon: Atlas.ticket_plus,
+                      title: 'Invite',
                     ),
-                    const Text(
-                      'Leave Space',
-                      style: TextStyle(color: Colors.red),
+                    SpaceCardIcons(
+                      icon: Atlas.link_chain,
+                      title: 'Copy link',
+                    ),
+                    SpaceCardIcons(
+                      icon: Atlas.bell_dots,
+                      title: 'Notifications',
                     )
                   ],
                 ),
-              )
-            ],
+                const Divider(
+                  thickness: 0.5,
+                  indent: 1,
+                  endIndent: 1,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 10, bottom: 10),
+                  child: Row(
+                    children: [
+                      Container(
+                          margin: const EdgeInsets.only(right: 10),
+                          child: const Icon(Atlas.padlock)),
+                      const Text('Encryption Settings')
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 10),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        child: const Icon(
+                          Atlas.block_shield,
+                          color: Colors.red,
+                        ),
+                      ),
+                      const Text(
+                        'Leave Space',
+                        style: TextStyle(color: Colors.red),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-        )
+        );
+        }else{
+          return Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primary,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          width: 310,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
+                  children:  [
+                    const SquareAvatar(
+                  height: 76,
+                  width: 77,
+                  displayImage: 'assets/icon/logo.png',
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  children: [
+                    Text(
+                  'OceanKAN Global',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                const Text(
+                  '#oceankan:acter.global',
+                  style: TextStyle(color: Color(0xff79747E)),
+                ),
+                  ],
+                )
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    SpaceCardIcons(
+                      icon: Atlas.ticket_plus,
+                      title: 'Invite',
+                    ),
+                    SpaceCardIcons(
+                      icon: Atlas.link_chain,
+                      title: 'Copy link',
+                    ),
+                    SpaceCardIcons(
+                      icon: Atlas.bell_dots,
+                      title: 'Notifications',
+                    )
+                  ],
+                ),
+                const Divider(
+                  thickness: 0.5,
+                  indent: 1,
+                  endIndent: 1,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 10, bottom: 10),
+                  child: Row(
+                    children: [
+                      Container(
+                          margin: const EdgeInsets.only(right: 10),
+                          child: const Icon(Atlas.padlock)),
+                      const Text('Encryption Settings')
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 10),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        child: const Icon(
+                          Atlas.block_shield,
+                          color: Colors.red,
+                        ),
+                      ),
+                      const Text(
+                        'Leave Space',
+                        style: TextStyle(color: Colors.red),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+        }
+        
+      },
     );
   }
 }
--- app/lib/routing.dart
+++ app/lib/routing.dart
@@ -14,6 +14,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+
 final desktopPlatforms = [
   TargetPlatform.linux,
   TargetPlatform.macOS,
