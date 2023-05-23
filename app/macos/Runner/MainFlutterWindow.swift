60877b91d2e65025b3d91212950602f000bb17ab
bitfriend <bitcapture@hotmail.com>
2023-05-23 18:30:14 +0800

    Append some description

--- app/macos/Runner/MainFlutterWindow.swift
+++ app/macos/Runner/MainFlutterWindow.swift
@@ -7,6 +7,10 @@ class MainFlutterWindow: NSWindow {
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
+
+    // turn off mainwindow's shadow to avoid crash on resize event
+    // please refer to this issue for further information
+    // https://github.com/acterglobal/a3/issues/613
     self.hasShadow = false
 
     RegisterGeneratedPlugins(registry: flutterViewController)
