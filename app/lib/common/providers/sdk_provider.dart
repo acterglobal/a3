5e3b5290ab566ed32aab50db67b5db5bb79af426
Benjamin Kampmann <ben@effektio.org>
2023-05-30 22:48:28 +0200

    cleaning up

--- app/lib/common/providers/sdk_provider.dart
+++ app/lib/common/providers/sdk_provider.dart
@@ -1,5 +1,4 @@
 import 'package:acter_flutter_sdk/acter_flutter_sdk.dart';
-import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final sdkProvider = FutureProvider<ActerSdk>((ref) async {
