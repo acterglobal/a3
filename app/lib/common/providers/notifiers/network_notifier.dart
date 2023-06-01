b0ed91c3b0ccbe57416692fba3525be3e1badce4
Benjamin Kampmann <ben@effektio.org>
2023-06-01 14:29:27 +0200

    Switch network state to a more assume-good-until-proofen-bad approach

--- app/lib/common/providers/notifiers/network_notifier.dart
+++ app/lib/common/providers/notifiers/network_notifier.dart
@@ -3,23 +3,15 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class NetworkStateNotifier extends StateNotifier<NetworkStatus> {
-  late NetworkStatus res;
   NetworkStateNotifier() : super(NetworkStatus.NotDetermined) {
-    res = NetworkStatus.NotDetermined;
     Connectivity().onConnectivityChanged.listen((ConnectivityResult result) {
       late NetworkStatus newState;
-      switch (result) {
-        case ConnectivityResult.mobile:
-        case ConnectivityResult.wifi:
-          newState = NetworkStatus.On;
-          break;
-        case ConnectivityResult.bluetooth:
-        case ConnectivityResult.ethernet:
-        case ConnectivityResult.other:
-        case ConnectivityResult.vpn:
-        case ConnectivityResult.none:
-          newState = NetworkStatus.Off;
-          break;
+      if (result == ConnectivityResult.none) {
+        newState = NetworkStatus.Off;
+      } else if (result == ConnectivityResult.other) {
+        newState = NetworkStatus.NotDetermined;
+      } else {
+        newState = NetworkStatus.On;
       }
       if (newState != state) {
         state = newState;
