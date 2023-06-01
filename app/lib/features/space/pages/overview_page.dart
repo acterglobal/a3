e102755058d62a2a295a14276e39f531aff01fed
Talha <gtalha@rocketmail.com>
2023-06-01 23:36:55 +0500

    fix lint errors

--- app/lib/features/space/pages/overview_page.dart
+++ app/lib/features/space/pages/overview_page.dart
@@ -4,8 +4,7 @@ import 'package:acter/features/space/widgets/about_card.dart';
 import 'package:acter/features/space/widgets/chats_card.dart';
 import 'package:acter/features/space/widgets/events_card.dart';
 import 'package:acter/features/space/widgets/links_card.dart';
-import 'package:acter/features/space/widgets/space_card.dart';
-import 'package:acter/features/space/widgets/spaces_card.dart';
+import 'package:acter/features/space/widgets/related_spaces_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
@@ -29,8 +28,7 @@ class SpaceOverview extends ConsumerWidget {
           EventsCard(spaceId: spaceIdOrAlias),
           LinksCard(spaceId: spaceIdOrAlias),
           ChatsCard(spaceId: spaceIdOrAlias),
-          RelatedSpacesCard(
-              cardTitle: 'Related Spaces', spaceId: spaceIdOrAlias),
+          RelatedSpacesCard(spaceId: spaceIdOrAlias),
         ],
       ),
     );
