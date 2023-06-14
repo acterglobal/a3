b4915ad1573d97cf9d911eef155032c48296439b
bitfriend <bitcapture@hotmail.com>
2023-06-14 20:35:21 +0800

    Replace nested watch with simple watch

--- app/lib/features/home/widgets/my_spaces_section.dart
+++ app/lib/features/home/widgets/my_spaces_section.dart
@@ -13,7 +13,7 @@ class MySpacesSection extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final spaces = ref.watch(spacesProvider);
+    final spaceItems = ref.watch(spaceItemsProvider);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
       child: Column(
@@ -24,49 +24,35 @@ class MySpacesSection extends ConsumerWidget {
             style: Theme.of(context).textTheme.titleMedium,
           ),
           const SizedBox(height: 10),
-          ...spaces.when(
-            data: (spaces) => [
-              ...spaces
-                  .sublist(0, spaces.length > limit ? limit : spaces.length)
+          ...spaceItems.when(
+            data: (items) => [
+              ...items
+                  .sublist(0, items.length > limit ? limit : items.length)
                   .map(
-                (space) {
-                  final roomId = space.getRoomId().toString();
-                  final profile = ref.watch(spaceProfileDataProvider(space));
-                  return profile.when(
-                    data: (profile) => Padding(
-                      padding: const EdgeInsets.only(
-                        bottom: 10,
+                (item) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: ListTile(
+                      onTap: () => context.go('/${item.roomId}'),
+                      title: Text(
+                        item.spaceProfileData.displayName ?? item.roomId,
+                        style: Theme.of(context).textTheme.bodySmall,
                       ),
-                      child: ListTile(
-                        onTap: () => context.go('/$roomId'),
-                        title: Text(
-                          profile.displayName ?? roomId,
-                          style: Theme.of(context).textTheme.bodySmall,
-                        ),
-                        leading: ActerAvatar(
-                          mode: DisplayMode.Space,
-                          displayName: profile.displayName,
-                          uniqueId: roomId,
-                          avatar: profile.getAvatarImage(),
-                          size: 48,
-                        ),
+                      leading: ActerAvatar(
+                        mode: DisplayMode.Space,
+                        displayName: item.spaceProfileData.displayName,
+                        uniqueId: item.roomId,
+                        avatar: item.spaceProfileData.getAvatarImage(),
+                        size: 48,
                       ),
-                    ),
-                    error: (error, stack) => ListTile(
-                      title: Text('Error loading: $roomId'),
-                      subtitle: Text('$error'),
-                    ),
-                    loading: () => ListTile(
-                      title: Text(roomId),
-                      subtitle: const Text('loading'),
                     ),
                   );
                 },
               ),
-              spaces.length > limit
+              items.length > limit
                   ? Padding(
                       padding: const EdgeInsets.only(left: 30),
-                      child: Text('see all ${spaces.length} spaces'),
+                      child: Text('see all ${items.length} spaces'),
                     ) // FIXME: click and where?
                   : const Text(''),
             ],
