668575756d80e9468c5dc1163c2d74d0d3ae2ab5
Benjamin Kampmann <ben@effektio.org>
2023-07-04 18:55:59 +0200

    implement searching and server selection

--- app/lib/features/space/pages/join_space.dart
+++ app/lib/features/space/pages/join_space.dart
@@ -1,4 +1,5 @@
 import 'package:acter/common/providers/space_providers.dart';
+import 'package:acter/common/utils/utils.dart';
 import 'package:acter/features/home/providers/client_providers.dart';
 import 'package:acter/main.dart';
 import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
@@ -26,12 +27,16 @@ class Next {
 
 class PublicSearchState extends PagedState<Next?, PublicSearchResultItem> {
   // We can extends [PagedState] to add custom parameters to our state
+  final String? server;
+  final String? searchValue;
 
   const PublicSearchState({
     List<PublicSearchResultItem>? records,
     String? error,
     Next? nextPageKey = const Next(isStart: true),
     List<Next?>? previousPageKeys,
+    this.server,
+    this.searchValue,
   }) : super(records: records, error: error, nextPageKey: nextPageKey);
 
   @override
@@ -39,6 +44,8 @@ class PublicSearchState extends PagedState<Next?, PublicSearchResultItem> {
     List<PublicSearchResultItem>? records,
     dynamic error,
     dynamic nextPageKey,
+    String? server,
+    String? searchValue,
     List<Next?>? previousPageKeys,
   }) {
     final sup = super.copyWith(
@@ -52,15 +59,62 @@ class PublicSearchState extends PagedState<Next?, PublicSearchResultItem> {
       error: sup.error,
       nextPageKey: sup.nextPageKey,
       previousPageKeys: sup.previousPageKeys,
+      server: server,
+      searchValue: searchValue,
     );
   }
 }
 
+final searchController = Provider.autoDispose<TextEditingController>((ref) {
+  final controller = TextEditingController();
+  ref.onDispose(() {
+    controller.dispose();
+    ref.read(searchValueProvider.notifier).state = null;
+  });
+  return controller;
+});
+
+final searchValueProvider = StateProvider<String?>((ref) => null);
+final selectedServerProvider = StateProvider<String?>((ref) => null);
+
 class PublicSearchNotifier extends StateNotifier<PublicSearchState>
     with PagedNotifierMixin<Next?, PublicSearchResultItem, PublicSearchState> {
-  PublicSearchNotifier(this.ref) : super(const PublicSearchState());
+  PublicSearchNotifier(this.ref) : super(const PublicSearchState()) {
+    setup();
+  }
 
   final Ref ref;
+
+  void setup() {
+    ref.watch(searchValueProvider.notifier).addListener((state) {
+      readData();
+    });
+    ref.watch(selectedServerProvider.notifier).addListener((state) {
+      readData();
+    });
+  }
+
+  void readData() async {
+    try {
+      await ref.debounce(const Duration(milliseconds: 300));
+      final newSearchValue = ref.read(searchValueProvider);
+      final newSelectedSever = ref.read(selectedServerProvider);
+      refresh(newSearchValue, newSelectedSever);
+    } catch (e) {
+      // we do not care.
+    }
+  }
+
+  void refresh(searchValue, server) {
+    final nextPageKey = Next(isStart: true);
+    state = state.copyWith(
+      records: null,
+      nextPageKey: nextPageKey,
+      searchValue: searchValue,
+      server: server,
+    );
+    load(nextPageKey, 30);
+  }
 
   @override
   Future<List<PublicSearchResultItem>?> load(Next? page, int limit) async {
@@ -70,28 +124,37 @@ class PublicSearchNotifier extends StateNotifier<PublicSearchState>
 
     final pageReq = page.next ?? '';
     final client = ref.watch(clientProvider)!;
-    final res = await client.publicSpaces(null, null, pageReq);
-    final entries = res.chunks();
-    final next = res.nextBatch();
-    Next? finalPageKey;
-    if (next != null) {
-      // we are not at the end
-      finalPageKey = Next(next: next);
+    final searchValue = state.searchValue;
+    final server = state.server;
+    try {
+      final res = await client.publicSpaces(searchValue, server, pageReq);
+      final entries = res.chunks();
+      final next = res.nextBatch();
+      Next? finalPageKey;
+      if (next != null) {
+        // we are not at the end
+        finalPageKey = Next(next: next);
+      }
+      state = state.copyWith(
+        records: page.isStart
+            ? [...entries]
+            : [...(state.records ?? []), ...entries],
+        nextPageKey: finalPageKey,
+        searchValue: searchValue,
+        server: server,
+      );
+    } catch (e) {
+      state = state.copyWith(error: e.toString());
     }
-    state = state.copyWith(
-      records:
-          page.isStart ? [...entries] : [...(state.records ?? []), ...entries],
-      nextPageKey: finalPageKey,
-    );
 
     return null;
   }
 }
 
-final publicSearchProvider = StateNotifierProvider<PublicSearchNotifier,
-    PagedState<Next?, PublicSearchResultItem>>(
-  (ref) => PublicSearchNotifier(ref),
-);
+final publicSearchProvider = StateNotifierProvider.autoDispose<
+    PublicSearchNotifier, PagedState<Next?, PublicSearchResultItem>>((ref) {
+  return PublicSearchNotifier(ref);
+});
 
 class JoinSpacePage extends ConsumerStatefulWidget {
   const JoinSpacePage({super.key});
@@ -104,9 +167,10 @@ class _JoinSpacePageState extends ConsumerState<JoinSpacePage> {
   @override
   Widget build(BuildContext context) {
     final widthCount = (MediaQuery.of(context).size.width ~/ 600).toInt();
+    final searchValue = ref.watch(searchValueProvider);
+    final _searchTextCtrl = ref.watch(searchController);
     const int minCount = 2;
     return Scaffold(
-      appBar: AppBar(title: Text('Join Space')),
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -118,25 +182,84 @@ class _JoinSpacePageState extends ConsumerState<JoinSpacePage> {
             ],
           ),
         ),
-        child: RiverPagedBuilder<Next?, PublicSearchResultItem>(
-          firstPageKey: const Next(isStart: true),
-          provider: publicSearchProvider,
-          itemBuilder: (context, item, index) => ListTile(
-            leading: ActerAvatar(
-              mode: DisplayMode.Space,
-              uniqueId: item.roomIdStr(),
-              displayName: item.name(),
+        child: CustomScrollView(
+          slivers: [
+            const SliverAppBar(
+              title: Text('Join space'),
             ),
-            title: Text(item.name() ?? 'no display name'),
-          ),
-          pagedBuilder: (controller, builder) => PagedGridView(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: max(1, min(widthCount, minCount)),
-              childAspectRatio: 6,
+            SliverToBoxAdapter(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _searchTextCtrl,
+                        autofocus: true,
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(
+                            Atlas.magnifying_glass_thin,
+                            color: Colors.white,
+                          ),
+                          labelText: 'search space',
+                        ),
+                        onChanged: (String value) async {
+                          ref.read(searchValueProvider.notifier).state = value;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: DropdownMenu<String>(
+                        initialSelection:
+                            ref.read(selectedServerProvider.notifier).state,
+                        label: const Text('Server'),
+                        dropdownMenuEntries: const [
+                          DropdownMenuEntry(
+                            label: 'Acter',
+                            value: 'acter.global',
+                          ),
+                          DropdownMenuEntry(
+                            label: 'Matrix.org',
+                            value: 'matrix.org',
+                          ),
+                        ],
+                        onSelected: (String? typus) {
+                          if (typus != null) {
+                            ref.read(selectedServerProvider.notifier).state =
+                                typus;
+                          }
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            pagingController: controller,
-            builderDelegate: builder,
-          ),
+            RiverPagedBuilder<Next?, PublicSearchResultItem>.autoDispose(
+              firstPageKey: const Next(isStart: true),
+              provider: publicSearchProvider,
+              itemBuilder: (context, item, index) => ListTile(
+                leading: ActerAvatar(
+                  mode: DisplayMode.Space,
+                  uniqueId: item.roomIdStr(),
+                  displayName: item.name(),
+                ),
+                title: Text(item.name() ?? 'no display name'),
+              ),
+              pagedBuilder: (controller, builder) => PagedSliverGrid(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: max(1, min(widthCount, minCount)),
+                  childAspectRatio: 6,
+                ),
+                pagingController: controller,
+                builderDelegate: builder,
+              ),
+            ),
+          ],
         ),
       ),
     );
