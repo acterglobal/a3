07035061ddb5c837ceca2ba0efce150e4f59198e
Benjamin Kampmann <ben@effektio.org>
2023-07-07 13:12:47 +0200

    use listen and watch

--- app/lib/features/news/providers/notifiers/news_list_notifier.dart
+++ app/lib/features/news/providers/notifiers/news_list_notifier.dart
@@ -1,15 +1,18 @@
+import 'dart:async';
+
 import 'package:acter/features/home/providers/client_providers.dart';
 import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart' show NewsEntry;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AsyncNewsListNotifier extends AutoDisposeAsyncNotifier<List<NewsEntry>> {
   late Stream<void> _listener;
+  late StreamSubscription<void> _sub;
 
   @override
   Future<List<NewsEntry>> build() async {
-    final client = ref.read(clientProvider)!;
+    final client = ref.watch(clientProvider)!;
     _listener = client.subscribe('news');
-    _listener.forEach((_e) async {
+    _sub = _listener.listen((_e) async {
       print(" --- - - ----------------- new subscribe received");
       state = await AsyncValue.guard(() => _fetchNews());
     });
@@ -18,7 +21,7 @@ class AsyncNewsListNotifier extends AutoDisposeAsyncNotifier<List<NewsEntry>> {
 
   Future<List<NewsEntry>> _fetchNews() async {
     print(" -------      refreshing news");
-    final client = ref.read(clientProvider);
+    final client = ref.watch(clientProvider);
     var entries = await client!.latestNewsEntries(25).then((v) => v.toList());
     return entries;
   }
