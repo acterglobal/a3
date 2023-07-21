b8e48512ae5a46342ce3e83f78cefd1f9befc0fd
kokodavid <kingkgdave@gmail.com>
2023-07-21 10:57:43 +0300

    search case sensitive fix

--- app/lib/features/search/widgets/quick_jump.dart
+++ app/lib/features/search/widgets/quick_jump.dart
@@ -294,7 +294,7 @@ class QuickJump extends ConsumerWidget {
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          TextField(
+          TextField(                                                         
             autofocus: true,
             decoration: const InputDecoration(
               border: InputBorder.none,
