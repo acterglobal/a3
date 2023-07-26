261f906185e0c1f4ef20d7271a82d35cad1ad813
bitfriend <bitcapture@hotmail.com>
2023-07-26 21:40:04 +0800

    Improve message notifier

--- app/lib/features/chat/providers/notifiers/messages_notifier.dart
+++ app/lib/features/chat/providers/notifiers/messages_notifier.dart
@@ -3,6 +3,7 @@ import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 
 class MessagesNotifier extends StateNotifier<List<types.Message>> {
   MessagesNotifier() : super([]);
+
   // Messages CRUD
   void addMessage(types.Message m) {
     state = [...state, m];
@@ -10,22 +11,31 @@ class MessagesNotifier extends StateNotifier<List<types.Message>> {
 
   void insertMessage(int to, types.Message m) {
     List<types.Message> newState = [...state];
-    newState[to] = m;
+    if (to < newState.length) {
+      newState.insert(to, m);
+    } else {
+      newState.add(m);
+    }
     state = newState;
   }
 
   void replaceMessage(int index, types.Message m) {
-    state = List<types.Message>.of(state)..replaceRange(index, index, [m]);
+    if (index < state.length) {
+      List<types.Message> newState = [...state];
+      newState[index] = m;
+      state = newState;
+    }
   }
 
   void removeMessage(int idx) {
-    state = [
-      for (final message in state)
-        if (state[idx] != message) message
-    ];
+    if (idx < state.length) {
+      List<types.Message> newState = [...state];
+      newState.removeAt(idx);
+      state = newState;
+    }
   }
 
   void reset() {
-    state = [];
+    state.clear();
   }
 }
