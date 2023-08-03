fc04b1f2ad0afcd35f8da6f5ac559e1c5f74bf68
bitfriend <bitcapture@hotmail.com>
2023-08-03 21:45:59 +0800

    Improve randomizing user for integration tests

--- native/test/src/tests/templates.rs
+++ native/test/src/tests/templates.rs
@@ -40,7 +40,7 @@ url = "https://github.com/acterglobal/a3"
 #[ignore = "test failed in github runner, it works well in local synapse :("]
 async fn template_creates_space() -> Result<()> {
     let _ = env_logger::try_init();
-    let (user, _sync_state, _engine) = random_user_with_template("create-space-", TMPL).await?;
+    let (user, _sync_state, _engine) = random_user_with_template("template-create-space-", TMPL).await?;
 
     // wait for sync to catch up
     let retry_strategy = FibonacciBackoff::from_millis(100).map(jitter).take(10);
