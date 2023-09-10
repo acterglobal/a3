911f75ced4ed8906a7dbed5dc05b6a71ff9c9c6f
Benjamin Kampmann <ben@effektio.org>
2023-09-10 16:04:43 +0200

    fixing style

--- app/lib/features/settings/pages/blocked_users.dart
+++ app/lib/features/settings/pages/blocked_users.dart
@@ -7,7 +7,6 @@ import 'package:acter/features/settings/widgets/settings_menu.dart';
 import 'package:atlas_icons/atlas_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 class AddUserToBlock extends StatefulWidget {
   const AddUserToBlock({
