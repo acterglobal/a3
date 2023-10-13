cc596c4f0995b8f4bbb5d8324e40b10a0694f6a2
Benjamin Kampmann <ben@acter.global>
2023-10-13 18:01:46 +0100

    options on the fail screen

--- app/lib/common/pages/fatal_fail.dart
+++ app/lib/common/pages/fatal_fail.dart
@@ -1,7 +1,11 @@
 
+import 'package:acter/common/dialogs/logout_confirmation.dart';
+import 'package:acter/common/utils/routes.dart';
+import 'package:atlas_icons/atlas_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 
 class FatalFailPage extends ConsumerWidget {
   final String error;
@@ -27,21 +31,25 @@ class FatalFailPage extends ConsumerWidget {
             Text(
               'Something went terribly wrong: $error',
             ),
-            // ButtonBar(
-            //   alignment: MainAxisAlignment.center,
-            //   children: [
-            //     OutlinedButton.icon(
-            //       icon: const Icon(Atlas.home_thin),
-            //       label: const Text('Go to home'),
-            //       onPressed: () => context.goNamed(Routes.main.name),
-            //     ),
-            //     OutlinedButton.icon(
-            //       icon: const Icon(Atlas.bug_clipboard_thin),
-            //       label: const Text('Report bug'),
-            //       onPressed: () => context.goNamed(Routes.bugReport.name),
-            //     ),
-            //   ],
-            // ),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton.icon(
+                  icon:  Icon(Atlas.exit, 
+                    color: Theme.of(context).colorScheme.error),
+                  label: Text('Force Logout', 
+                    style: TextStyle(color: Theme.of(context).colorScheme.error),),
+                  onPressed: () =>
+                      logoutConfirmationDialog(context, ref),
+                              
+                ),
+                OutlinedButton.icon(
+                  icon: const Icon(Atlas.bug_clipboard_thin),
+                  label: const Text('Report bug'),
+                  onPressed: () => context.goNamed(Routes.bugReport.name),
+                ),
+              ],
+            ),
           ],
         ),
       ),
