58de8221c4560f37841b06444ae16f9d8fa2e28b
kumarpalsinh25 <info@dajiraj.com>
2023-10-31 17:49:29 +0530

    - Resolve UI-UX issue related to login and register button, fixes #932

--- app/lib/features/onboarding/pages/login_page.dart
+++ app/lib/features/onboarding/pages/login_page.dart
@@ -86,132 +86,137 @@ class _LoginPageState extends ConsumerState<LoginPage> {
             SliverToBoxAdapter(
               child: Form(
                 key: formKey,
-                child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const SizedBox(height: 20),
-                      SizedBox(
-                        height: 50,
-                        width: 50,
-                        child: SvgPicture.asset('assets/icon/acter.svg'),
-                      ),
-                      const SizedBox(height: 20),
-                      Text(AppLocalizations.of(context)!.welcomeBack),
-                      const SizedBox(height: 20),
-                      Text(AppLocalizations.of(context)!.loginContinue),
-                      const SizedBox(height: 40),
-                      TextFormField(
-                        key: LoginPageKeys.usernameField,
-                        obscureText: false,
-                        controller: username,
-                        decoration: InputDecoration(
-                          hintText: AppLocalizations.of(context)!.username,
-                          fillColor:
-                              Theme.of(context).colorScheme.primaryContainer,
-                          filled: true,
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Theme.of(context).colorScheme.tertiary,
-                              width: 0.5,
-                            ),
-                            borderRadius: BorderRadius.circular(12),
-                          ),
+                child: Center(
+                  child: Container(
+                    constraints: const BoxConstraints(
+                      maxWidth: 500,
+                    ),
+                    margin: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const SizedBox(height: 20),
+                        SizedBox(
+                          height: 50,
+                          width: 50,
+                          child: SvgPicture.asset('assets/icon/acter.svg'),
                         ),
-                        inputFormatters: [
-                          FilteringTextInputFormatter.deny(RegExp(r'\s')),
-                        ],
-                        style: Theme.of(context).textTheme.labelLarge,
-                        validator: (val) {
-                          if (val == null || val.trim().isEmpty) {
-                            return AppLocalizations.of(context)!.emptyUsername;
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          username.text = value;
-                          username.selection = TextSelection.fromPosition(
-                            TextPosition(offset: username.text.length),
-                          );
-                        },
-                      ),
-                      const SizedBox(height: 20),
-                      TextFormField(
-                        key: LoginPageKeys.passwordField,
-                        controller: password,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          hintText: AppLocalizations.of(context)!.password,
-                          fillColor:
-                              Theme.of(context).colorScheme.primaryContainer,
-                          filled: true,
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Theme.of(context).colorScheme.tertiary,
-                              width: 0.5,
+                        const SizedBox(height: 20),
+                        Text(AppLocalizations.of(context)!.welcomeBack),
+                        const SizedBox(height: 20),
+                        Text(AppLocalizations.of(context)!.loginContinue),
+                        const SizedBox(height: 40),
+                        TextFormField(
+                          key: LoginPageKeys.usernameField,
+                          obscureText: false,
+                          controller: username,
+                          decoration: InputDecoration(
+                            hintText: AppLocalizations.of(context)!.username,
+                            fillColor:
+                                Theme.of(context).colorScheme.primaryContainer,
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(12),
                             ),
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                        ),
-                        inputFormatters: [
-                          FilteringTextInputFormatter.deny(RegExp(r'\s')),
-                        ],
-                        style: Theme.of(context).textTheme.labelLarge,
-                        validator: (val) {
-                          if (val == null || val.trim().isEmpty) {
-                            return AppLocalizations.of(context)!.emptyPassword;
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          password.text = value;
-                          password.selection = TextSelection.fromPosition(
-                            TextPosition(offset: password.text.length),
-                          );
-                        },
-                      ),
-                      const SizedBox(height: 40),
-                      authState
-                          ? const CircularProgressIndicator()
-                          : DefaultButton(
-                              key: LoginPageKeys.submitBtn,
-                              onPressed: () => handleSubmit(context),
-                              title: AppLocalizations.of(context)!.logIn,
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor:
-                                    Theme.of(context).colorScheme.success,
-                              ),
-                            ),
-                      const SizedBox(height: 40),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(AppLocalizations.of(context)!.noAccount),
-                          const SizedBox(width: 2),
-                          InkWell(
-                            key: LoginPageKeys.signUpBtn,
-                            onTap: () =>
-                                context.goNamed(Routes.authRegister.name),
-                            child: Text(
-                              AppLocalizations.of(context)!.register,
-                              style: TextStyle(
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
                                 color: Theme.of(context).colorScheme.tertiary,
+                                width: 0.5,
                               ),
+                              borderRadius: BorderRadius.circular(12),
                             ),
                           ),
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                    ],
+                          inputFormatters: [
+                            FilteringTextInputFormatter.deny(RegExp(r'\s')),
+                          ],
+                          style: Theme.of(context).textTheme.labelLarge,
+                          validator: (val) {
+                            if (val == null || val.trim().isEmpty) {
+                              return AppLocalizations.of(context)!.emptyUsername;
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            username.text = value;
+                            username.selection = TextSelection.fromPosition(
+                              TextPosition(offset: username.text.length),
+                            );
+                          },
+                        ),
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          key: LoginPageKeys.passwordField,
+                          controller: password,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            hintText: AppLocalizations.of(context)!.password,
+                            fillColor:
+                                Theme.of(context).colorScheme.primaryContainer,
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Theme.of(context).colorScheme.tertiary,
+                                width: 0.5,
+                              ),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          inputFormatters: [
+                            FilteringTextInputFormatter.deny(RegExp(r'\s')),
+                          ],
+                          style: Theme.of(context).textTheme.labelLarge,
+                          validator: (val) {
+                            if (val == null || val.trim().isEmpty) {
+                              return AppLocalizations.of(context)!.emptyPassword;
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            password.text = value;
+                            password.selection = TextSelection.fromPosition(
+                              TextPosition(offset: password.text.length),
+                            );
+                          },
+                        ),
+                        const SizedBox(height: 40),
+                        authState
+                            ? const CircularProgressIndicator()
+                            : DefaultButton(
+                                key: LoginPageKeys.submitBtn,
+                                onPressed: () => handleSubmit(context),
+                                title: AppLocalizations.of(context)!.logIn,
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      Theme.of(context).colorScheme.success,
+                                ),
+                              ),
+                        const SizedBox(height: 40),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(AppLocalizations.of(context)!.noAccount),
+                            const SizedBox(width: 2),
+                            InkWell(
+                              key: LoginPageKeys.signUpBtn,
+                              onTap: () =>
+                                  context.goNamed(Routes.authRegister.name),
+                              child: Text(
+                                AppLocalizations.of(context)!.register,
+                                style: TextStyle(
+                                  color: Theme.of(context).colorScheme.tertiary,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                      ],
+                    ),
                   ),
                 ),
               ),
