1e28abaeb4e93c110668dd4eade5e6d13c622592
Benjamin Kampmann <ben@acter.global>
2023-11-03 15:51:40 +0000

    disable overflow for all tests

--- app/integration_test/tests/login.dart
+++ app/integration_test/tests/login.dart
@@ -1,5 +1,6 @@
 import 'package:convenient_test_dev/convenient_test_dev.dart';
 import '../support/login.dart';
+import '../support/setup.dart';
 
 const registrationToken = String.fromEnvironment(
   'REGISTRATION_TOKEN',
@@ -8,9 +9,11 @@ const registrationToken = String.fromEnvironment(
 
 void loginTests() {
   tTestWidgets('registration smoke test', (t) async {
+    disableOverflowErrors();
     await t.freshAccount();
   });
   tTestWidgets('register and login test', (t) async {
+    disableOverflowErrors();
     final userId = await t.freshAccount();
     await t.logout();
     await t.login(userId);
