549ea3c5b107e26c7d296c84eb78a7756571ba74
Benjamin Kampmann <ben@effektio.org>
2023-11-08 16:10:55 +0000

    simplified loop

--- native/test/src/tests/msg_edit.rs
+++ native/test/src/tests/msg_edit.rs
@@ -1,9 +1,7 @@
 use anyhow::{bail, Result};
-use futures::{pin_mut, stream::StreamExt};
-use tokio_retry::{
-    strategy::{jitter, FibonacciBackoff},
-    Retry,
-};
+use core::time::Duration;
+use futures::{pin_mut, stream::StreamExt, FutureExt};
+use tokio::time::sleep;
 use tracing::info;
 
 use crate::utils::random_user_with_random_convo;
@@ -19,43 +17,45 @@ async fn message_edit() -> Result<()> {
     let convo = user.convo(room_id.to_string()).await?;
     let timeline = convo.timeline_stream().await?;
     let stream = timeline.diff_stream();
-
     let event_id = convo.send_plain_message("Hi, everyone".to_string()).await?;
+    pin_mut!(stream);
     info!("event id: {event_id:?}");
 
-    let retry_strategy = FibonacciBackoff::from_millis(100).map(jitter).take(10);
     let fetch_event_id = event_id.clone();
-    Retry::spawn(retry_strategy, move || {
-        pin_mut!(stream);
-        let event_id = fetch_event_id.clone();
-        async move {
-            pin_mut!(stream);
-            let Some(diff) = stream.next().await else {
-                bail!("timeline stream should get diff")
-            };
-            info!("diff index: {:?}", diff.index());
-            info!("diff value: {:?}", diff.value());
-            info!("diff values: {:?}", diff.values());
-            let values = diff
-                .values()
-                .expect("diff reset action should have valid values");
-            for msg in values.iter() {
-                if msg.item_type() == "event" {
-                    let event_item =
-                        msg.event_item().expect("user already sent message edition");
-                    if event_item.event_id() == event_id.to_string() {
-                        let text_desc = event_item
-                            .text_desc()
-                            .expect("text message should have text desc");
-                        assert_eq!(text_desc.body(), "Hi, everyone");
-                        bail!("");
-                    }
-                }
+    let diff = {
+        let mut i = 3;
+        let mut res = None;
+        while i > 0 {
+            res = stream.next().now_or_never().flatten();
+            if res.is_some() {
+                // yay
+                break;
             }
-            Ok(())
+            i -= 1;
+            sleep(Duration::from_secs(1)).await;
         }
-    })
-    .await?;
+        res
+    }
+    .expect("Even after 3 seconds, no diff was found");
+
+    info!("diff index: {:?}", diff.index());
+    info!("diff value: {:?}", diff.value());
+    info!("diff values: {:?}", diff.values());
+    let values = diff
+        .values()
+        .expect("diff reset action should have valid values");
+    for msg in values.iter() {
+        if msg.item_type() == "event" {
+            let event_item = msg.event_item().expect("user already sent message edition");
+            if event_item.event_id() == event_id.to_string() {
+                let text_desc = event_item
+                    .text_desc()
+                    .expect("text message should have text desc");
+                assert_eq!(text_desc.body(), "Hi, everyone");
+                bail!("");
+            }
+        }
+    }
 
     let edited_id = convo
         .edit_plain_message(event_id.to_string(), "This is message edition".to_string())
