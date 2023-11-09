a116b7556d26dda03825e873cd2a0ee130c3fbdb
bitfriend <bitcapture@hotmail.com>
2023-11-09 20:30:19 +0800

    Replace infinite loop with finite loop

--- native/test/src/tests/msg_edit.rs
+++ native/test/src/tests/msg_edit.rs
@@ -1,4 +1,5 @@
-use anyhow::{bail, Result};
+use acter::RoomMessage;
+use anyhow::Result;
 use core::time::Duration;
 use futures::{pin_mut, stream::StreamExt, FutureExt};
 use tokio::time::sleep;
@@ -17,112 +18,95 @@ async fn message_edit() -> Result<()> {
     let convo = user.convo(room_id.to_string()).await?;
     let timeline = convo.timeline_stream().await?;
     let stream = timeline.diff_stream();
-    let event_id = convo.send_plain_message("Hi, everyone".to_string()).await?;
     pin_mut!(stream);
+
+    let event_id = convo.send_plain_message("Hi, everyone".to_string()).await?;
     info!("event id: {event_id:?}");
 
-    let fetch_event_id = event_id.clone();
-    let diff = {
-        let mut i = 3;
-        let mut res = None;
-        while i > 0 {
-            res = stream.next().now_or_never().flatten();
-            if res.is_some() {
-                // yay
+    // text msg may reach via pushback action or reset action
+    let mut i = 3;
+    let mut received = false;
+    while i > 0 {
+        if let Some(diff) = stream.next().now_or_never().flatten() {
+            match diff.action().as_str() {
+                "PushBack" => {
+                    let value = diff
+                        .value()
+                        .expect("diff pushback action should have valid value");
+                    if match_room_msg(&value, event_id.to_string(), "Hi, everyone", false) {
+                        received = true;
+                    }
+                }
+                "Reset" => {
+                    let values = diff
+                        .values()
+                        .expect("diff reset action should have valid values");
+                    for value in values.iter() {
+                        if match_room_msg(value, event_id.to_string(), "Hi, everyone", false) {
+                            received = true;
+                            break;
+                        }
+                    }
+                }
+                _ => {}
+            }
+            // yay
+            if received {
                 break;
             }
-            i -= 1;
-            sleep(Duration::from_secs(1)).await;
         }
-        res
+        i -= 1;
+        sleep(Duration::from_secs(1)).await;
     }
-    .expect("Even after 3 seconds, no diff was found");
-
-    info!("diff index: {:?}", diff.index());
-    info!("diff value: {:?}", diff.value());
-    info!("diff values: {:?}", diff.values());
-    let values = diff
-        .values()
-        .expect("diff reset action should have valid values");
-    for msg in values.iter() {
-        if msg.item_type() == "event" {
-            let event_item = msg.event_item().expect("user already sent message edition");
-            if event_item.event_id() == event_id.to_string() {
-                let text_desc = event_item
-                    .text_desc()
-                    .expect("text message should have text desc");
-                assert_eq!(text_desc.body(), "Hi, everyone");
-                bail!("");
-            }
-        }
-    }
+    assert!(received, "Even after 3 seconds, text msg not received");
 
     let edited_id = convo
         .edit_plain_message(event_id.to_string(), "This is message edition".to_string())
         .await?;
     info!("edited id: {edited_id:?}");
 
-    // while let Some(diff) = stream.next().await {
-    //     match diff.action().as_str() {
-    //         "Append" => {}
-    //         "Insert" => {}
-    //         "Set" => {
-    //             info!("diff index: {:?}", diff.index());
-    //             info!("diff value: {:?}", diff.value());
-    //             info!("diff values: {:?}", diff.values());
-    //             let value = diff
-    //                 .value()
-    //                 .expect("diff set action should have valid value");
-    //             assert_eq!(value.item_type(), "event");
-    //             let event_item = value
-    //                 .event_item()
-    //                 .expect("user already sent message edition");
-    //             assert!(
-    //                 event_item.is_edited(),
-    //                 "this event should be edition message"
-    //             );
-    //             assert_eq!(
-    //                 event_item.event_id(),
-    //                 event_id.to_string(),
-    //                 "should be original id, because original msg is replaced with msg edition"
-    //             );
-    //             let text_desc = event_item
-    //                 .text_desc()
-    //                 .expect("message edition should have text desc");
-    //             assert_eq!(text_desc.body(), "This is message edition");
-    //             break;
-    //         }
-    //         "Remove" => {}
-    //         "PushBack" => {}
-    //         "PushFront" => {}
-    //         "PopBack" => {}
-    //         "PopFront" => {}
-    //         "Clear" => {}
-    //         "Reset" => {
-    //             info!("diff index: {:?}", diff.index());
-    //             info!("diff value: {:?}", diff.value());
-    //             info!("diff values: {:?}", diff.values());
-    //             let values = diff
-    //                 .values()
-    //                 .expect("diff reset action should have valid values");
-    //             for msg in values.iter() {
-    //                 if msg.item_type() == "event" {
-    //                     let event_item =
-    //                         msg.event_item().expect("user already sent message edition");
-    //                     if event_item.event_id() == event_id.to_string() {
-    //                         let text_desc = event_item
-    //                             .text_desc()
-    //                             .expect("text message should have text desc");
-    //                         assert_eq!(text_desc.body(), "Hi, everyone");
-    //                         break;
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //         "Truncate" => {}
-    //         _ => {}
-    //     }
-    // }
+    // msg edition may reach via set action
+    i = 3;
+    received = false;
+    while i > 0 {
+        if let Some(diff) = stream.next().now_or_never().flatten() {
+            if diff.action() == "Set" {
+                let value = diff
+                    .value()
+                    .expect("diff set action should have valid value");
+                if match_room_msg(
+                    &value,
+                    event_id.to_string(), // not edited_id, because stream will replace old msg with new msg in timeline
+                    "This is message edition",
+                    true,
+                ) {
+                    received = true;
+                }
+            }
+            // yay
+            if received {
+                break;
+            }
+        }
+        i -= 1;
+        sleep(Duration::from_secs(1)).await;
+    }
+    assert!(received, "Even after 3 seconds, text msg not received");
 
     Ok(())
+}
+
+fn match_room_msg(msg: &RoomMessage, event_id: String, body: &str, modified: bool) -> bool {
+    if msg.item_type() == "event" {
+        let event_item = msg.event_item().expect("room msg should have event item");
+        if event_item.event_id() == event_id {
+            assert_eq!(event_item.is_edited(), modified);
+            let text_desc = event_item
+                .text_desc()
+                .expect("text msg should have text desc");
+            assert_eq!(text_desc.body(), body);
+            return true;
+        }
+    }
+    false
 }
