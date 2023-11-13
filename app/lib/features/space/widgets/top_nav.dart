755278357982f25508cd211436d20eeb40947b82
kumarpalsinh25 <info@dajiraj.com>
2023-11-13 13:23:03 +0530

    - Added animation effect

--- app/lib/features/space/widgets/top_nav.dart
+++ app/lib/features/space/widgets/top_nav.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:acter/features/space/providers/space_navbar_provider.dart';
@@ -55,54 +56,59 @@ class _TopNavBarState extends ConsumerState<TopNavBar>
         final tabController = tabState.ctrl;
         final tabs = tabState.tabs;
 
-        return LayoutBuilder(
-          builder: (context, constraints) {
-            final useCols = constraints.maxWidth < (150 * tabs.length);
-            int minItemWidth = useCols ? 90 : 150;
-            final minTotalWidth = minItemWidth * tabs.length;
-            bool scrollBar = false;
-            if (minTotalWidth > constraints.maxWidth) {
-              scrollBar = true;
-            }
-            return Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
+        return Animate(
+          effects: const [
+            FadeEffect(duration: Duration(milliseconds: 200)),
+            ScaleEffect(duration: Duration(milliseconds: 100)),
+          ],
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              final useCols = constraints.maxWidth < (150 * tabs.length);
+              int minItemWidth = useCols ? 90 : 150;
+              final minTotalWidth = minItemWidth * tabs.length;
+              bool scrollBar = false;
+              if (minTotalWidth > constraints.maxWidth) {
+                scrollBar = true;
+              }
+              return Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
+                  color: Theme.of(context).colorScheme.onPrimary,
                 ),
-                color: Theme.of(context).colorScheme.onPrimary,
-              ),
-              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: TabBar(
-                controller: tabController,
-                isScrollable: scrollBar,
-                labelStyle: useCols
-                    ? Theme.of(context).textTheme.labelSmall
-                    : Theme.of(context).textTheme.bodySmall,
-                labelColor: Colors.white,
-                labelPadding: useCols ? const EdgeInsets.all(8) : null,
-                indicatorColor: Theme.of(context).colorScheme.tertiary,
-                tabs: List.generate(
-                  tabs.length,
-                  (index) {
-                    final tabItem = tabs[index];
-                    final children = [
-                      tabItem.makeIcon(context),
-                      useCols
-                          ? const SizedBox(height: 5)
-                          : const SizedBox(width: 10),
-                      Text(tabItem.label),
-                    ];
-                    return InkWell(
-                      onTap: () {
-                        final target = tabs[index].target;
-                        context.goNamed(
-                          target,
-                          pathParameters: {'spaceId': widget.spaceId},
-                        );
-                      },
-                      child: Tab(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TabBar(
+                  controller: tabController,
+                  isScrollable: scrollBar,
+                  labelStyle: useCols
+                      ? Theme.of(context).textTheme.labelSmall
+                      : Theme.of(context).textTheme.bodySmall,
+                  labelColor: Colors.white,
+                  onTap: (index) {
+                    final target = tabs[index].target;
+                    context.goNamed(
+                      target,
+                      pathParameters: {'spaceId': widget.spaceId},
+                    );
+                  },
+                  labelPadding: useCols ? const EdgeInsets.all(8) : null,
+                  indicatorColor: Theme.of(context).colorScheme.tertiary,
+                  tabs: List.generate(
+                    tabs.length,
+                    (index) {
+                      final tabItem = tabs[index];
+                      final children = [
+                        tabItem.makeIcon(context),
+                        useCols
+                            ? const SizedBox(height: 5)
+                            : const SizedBox(width: 10),
+                        Text(tabItem.label),
+                      ];
+                      return Tab(
                         key: tabItem.key,
                         child: useCols
                             ? SizedBox(
@@ -117,13 +123,13 @@ class _TopNavBarState extends ConsumerState<TopNavBar>
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: children,
                               ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         );
       },
       error: (e, stack) => Text('Error loading navigation menu: $e'),
