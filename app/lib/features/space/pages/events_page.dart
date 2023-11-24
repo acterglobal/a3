6ec356a72764c886e3e97b17e5cbe3862a38ec2d
kumarpalsinh25 <info@dajiraj.com>
2023-11-24 17:12:21 +0530

    - Managed post event option based on the permission, fixes #1146

--- app/lib/features/space/pages/events_page.dart
+++ app/lib/features/space/pages/events_page.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:acter/common/providers/space_providers.dart';
 import 'package:acter/common/themes/app_theme.dart';
 import 'package:acter/common/utils/routes.dart';
 import 'package:acter/features/events/providers/event_providers.dart';
@@ -16,72 +17,97 @@ class SpaceEventsPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final spaceRelationsOverview =
+        ref.watch(spaceRelationsOverviewProvider(spaceIdOrAlias));
     final spaceEvents = ref.watch(spaceEventsProvider(spaceIdOrAlias));
-    return DecoratedBox(
-      decoration: const BoxDecoration(gradient: AppTheme.primaryGradient),
-      child: CustomScrollView(
-        slivers: <Widget>[
-          SliverToBoxAdapter(
-            child: SpaceHeader(spaceIdOrAlias: spaceIdOrAlias),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Text(
-                    'Events',
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                  const Spacer(),
-                  IconButton(
-                    icon: Icon(
-                      Atlas.plus_circle_thin,
-                      color: Theme.of(context).colorScheme.neutral5,
-                    ),
-                    iconSize: 28,
-                    color: Theme.of(context).colorScheme.surface,
-                    onPressed: () => context.pushNamed(
-                      Routes.createEvent.name,
-                      queryParameters: {'spaceId': spaceIdOrAlias},
-                    ),
-                  ),
-                ],
+
+    return spaceRelationsOverview.when(
+      data: (spaceData) {
+        bool checkPermission(String permission) {
+          if (spaceData.membership != null) {
+            return spaceData.membership!.canString(permission);
+          }
+          return false;
+        }
+
+        final canPostEvent = checkPermission('CanPostEvent');
+        return DecoratedBox(
+          decoration: const BoxDecoration(gradient: AppTheme.primaryGradient),
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverToBoxAdapter(
+                child: SpaceHeader(spaceIdOrAlias: spaceIdOrAlias),
               ),
-            ),
-          ),
-          spaceEvents.when(
-            data: (events) {
-              final widthCount =
-                  (MediaQuery.of(context).size.width ~/ 600).toInt();
-              const int minCount = 2;
-              if (events.isEmpty) {
-                return const SliverToBoxAdapter(
-                  child: Center(
-                    child: Text(
-                      'Currently there are no events planned for this space',
-                    ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Events',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      const Spacer(),
+                      Visibility(
+                        visible: canPostEvent,
+                        child: IconButton(
+                          icon: Icon(
+                            Atlas.plus_circle_thin,
+                            color: Theme.of(context).colorScheme.neutral5,
+                          ),
+                          iconSize: 28,
+                          color: Theme.of(context).colorScheme.surface,
+                          onPressed: () => context.pushNamed(
+                            Routes.createEvent.name,
+                            queryParameters: {'spaceId': spaceIdOrAlias},
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                );
-              }
-              return SliverGrid.builder(
-                itemCount: events.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: max(1, min(widthCount, minCount)),
-                  childAspectRatio: 4.0,
                 ),
-                itemBuilder: (context, index) =>
-                    EventItem(event: events[index]),
-              );
-            },
-            error: (error, stackTrace) => SliverToBoxAdapter(
-              child: Center(child: Text('Failed to load events due to $error')),
-            ),
-            loading: () => const SliverToBoxAdapter(
-              child: Center(child: CircularProgressIndicator()),
-            ),
+              ),
+              spaceEvents.when(
+                data: (events) {
+                  final widthCount =
+                      (MediaQuery.of(context).size.width ~/ 600).toInt();
+                  const int minCount = 2;
+                  if (events.isEmpty) {
+                    return const SliverToBoxAdapter(
+                      child: Center(
+                        child: Text(
+                          'Currently there are no events planned for this space',
+                        ),
+                      ),
+                    );
+                  }
+                  return SliverGrid.builder(
+                    itemCount: events.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: max(1, min(widthCount, minCount)),
+                      childAspectRatio: 4.0,
+                    ),
+                    itemBuilder: (context, index) =>
+                        EventItem(event: events[index]),
+                  );
+                },
+                error: (error, stackTrace) => SliverToBoxAdapter(
+                  child: Center(
+                      child: Text('Failed to load events due to $error')),
+                ),
+                loading: () => const SliverToBoxAdapter(
+                  child: Center(child: CircularProgressIndicator()),
+                ),
+              ),
+            ],
           ),
-        ],
+        );
+      },
+      error: (error, stack) => Center(
+        child: Text('Loading failed: $error'),
+      ),
+      loading: () => const Center(
+        child: Text('Loading'),
       ),
     );
   }
