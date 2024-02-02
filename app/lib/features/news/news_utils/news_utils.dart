dec5c2cd65bc9849352ab23852d15fbbc7a3daa3
bitfriend <bitcapture@hotmail.com>
2024-02-03 06:27:04 +0800

    Improve code

--- app/lib/features/news/news_utils/news_utils.dart
+++ app/lib/features/news/news_utils/news_utils.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:math';
+
 import 'package:acter/features/news/model/news_slide_model.dart';
 import 'package:acter/features/news/providers/news_post_editor_providers.dart';
 import 'package:fc_native_video_thumbnail/fc_native_video_thumbnail.dart';
@@ -7,14 +8,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 
 class NewsUtils {
   static Future<File?> getThumbnailData(XFile videoFile) async {
     try {
       final tempPath = await getTemporaryDirectory();
-
-      final destFile = File('${tempPath.path}/${videoFile.name}');
+      final destPath = p.join(tempPath.path, videoFile.name);
+      final destFile = File(destPath);
 
       if (await destFile.exists()) {
         return destFile;
@@ -23,7 +25,7 @@ class NewsUtils {
       final thumbnailGenerated =
           await FcNativeVideoThumbnail().getVideoThumbnail(
         srcFile: videoFile.path,
-        destFile: destFile.path,
+        destFile: destPath,
         width: 128,
         height: 128,
         keepAspectRatio: true,
@@ -43,13 +45,13 @@ class NewsUtils {
 
   //Add text slide
   static void addTextSlide(WidgetRef ref) {
+    final clr = Colors.primaries[Random().nextInt(Colors.primaries.length)];
     NewsSlideItem textSlide = NewsSlideItem(
       type: NewsSlideType.text,
       text: '',
-      backgroundColor:
-          Colors.primaries[Random().nextInt(Colors.primaries.length)],
+      backgroundColor: clr,
     );
-    ref.watch(newsStateProvider.notifier).addSlide(textSlide);
+    ref.read(newsStateProvider.notifier).addSlide(textSlide);
   }
 
   //Add image slide
@@ -57,12 +59,11 @@ class NewsUtils {
     XFile? imageFile =
         await ImagePicker().pickImage(source: ImageSource.gallery);
     if (imageFile != null) {
-      ref.watch(newsStateProvider.notifier).addSlide(
-            NewsSlideItem(
-              type: NewsSlideType.image,
-              mediaFile: imageFile,
-            ),
-          );
+      final slide = NewsSlideItem(
+        type: NewsSlideType.image,
+        mediaFile: imageFile,
+      );
+      ref.read(newsStateProvider.notifier).addSlide(slide);
     }
   }
 
@@ -72,12 +73,11 @@ class NewsUtils {
     // XFile? videoFile =
     //     await ImagePicker().pickVideo(source: ImageSource.gallery);
     // if (videoFile != null) {
-    //   ref.watch(newsStateProvider.notifier).addSlide(
-    //         NewsSlideItem(
-    //           type: NewsSlideType.video,
-    //           mediaFile: videoFile,
-    //         ),
-    //       );
+    //   final slide = NewsSlideItem(
+    //     type: NewsSlideType.video,
+    //     mediaFile: videoFile,
+    //   );
+    //   ref.read(newsStateProvider.notifier).addSlide(slide);
     // }
   }
 }
