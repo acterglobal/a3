65fd3491e6e2c0f67f783b6fdd6b23332057172a
kumarpalsinh25 <info@dajiraj.com>
2024-02-09 11:58:59 +0530

    - General UI-UX Improvements

--- app/lib/features/news/pages/add_news_page.dart
+++ app/lib/features/news/pages/add_news_page.dart
@@ -1,20 +1,29 @@
 import 'dart:io';
 
 import 'package:acter/common/providers/chat_providers.dart';
+import 'package:acter/common/providers/sdk_provider.dart';
+import 'package:acter/common/providers/space_providers.dart';
+import 'package:acter/common/snackbars/custom_msg.dart';
 import 'package:acter/common/widgets/acter_video_player.dart';
 import 'package:acter/features/chat/widgets/room_avatar.dart';
+import 'package:acter/features/home/providers/client_providers.dart';
 import 'package:acter/features/home/widgets/space_chip.dart';
 import 'package:acter/features/news/model/keys.dart';
 import 'package:acter/features/news/model/news_slide_model.dart';
 import 'package:acter/features/news/news_utils/news_utils.dart';
 import 'package:acter/features/news/providers/news_post_editor_providers.dart';
+import 'package:acter/features/news/providers/news_providers.dart';
 import 'package:acter/features/news/widgets/news_post_editor/select_action_item.dart';
 import 'package:acter/features/news/widgets/news_post_editor/news_slide_options.dart';
+import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
 import 'package:atlas_icons/atlas_icons.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mime/mime.dart';
 
 class AddNewsPage extends ConsumerStatefulWidget {
   const AddNewsPage({super.key});
@@ -50,16 +59,22 @@ class _AddNewsState extends ConsumerState<AddNewsPage> {
       backgroundColor: selectedNewsPost == null
           ? Colors.transparent
           : selectedNewsPost?.backgroundColor,
-      actions: [
-        IconButton(
-          onPressed: () {
-            ref
-                .read(newsStateProvider.notifier)
-                .changeTextSlideBackgroundColor();
-          },
-          icon: const Icon(Atlas.color),
-        ),
-      ],
+      actions: selectedNewsPost == null
+          ? []
+          : [
+              IconButton(
+                onPressed: () => selectActionItemDialog(context),
+                icon: const Icon(Atlas.plus_circle),
+              ),
+              IconButton(
+                onPressed: () {
+                  ref
+                      .read(newsStateProvider.notifier)
+                      .changeTextSlideBackgroundColor();
+                },
+                icon: const Icon(Atlas.color),
+              ),
+            ],
     );
   }
 
@@ -68,10 +83,11 @@ class _AddNewsState extends ConsumerState<AddNewsPage> {
     return Visibility(
       visible: selectedNewsPost != null,
       child: Padding(
-        padding: const EdgeInsets.only(bottom: 80),
+        padding: const EdgeInsets.only(bottom: 90),
         child: FloatingActionButton(
-          onPressed: () => selectActionItemDialog(context),
-          child: const Icon(Atlas.plus_circle),
+          key: NewsUpdateKeys.newsSubmitBtn,
+          onPressed: () => sendNews(context),
+          child: const Icon(Icons.send),
         ),
       ),
     );
@@ -108,10 +124,47 @@ class _AddNewsState extends ConsumerState<AddNewsPage> {
     return Column(
       children: [
         //News content UI
-        Expanded(child: newsContentUI()),
+        Expanded(
+          child: Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              newsContentUI(),
+              parentSpaceSelector(),
+            ],
+          ),
+        ),
         //Slide options
         const NewsSlideOptions(),
       ],
+    );
+  }
+
+  Widget parentSpaceSelector() {
+    final newsPostSpaceId = ref.read(newsStateProvider).newsPostSpaceId;
+    return Visibility(
+      visible: selectedNewsPost != null,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+        child: (newsPostSpaceId != null)
+            ? InkWell(
+                key: NewsUpdateKeys.selectSpace,
+                onTap: () async {
+                  await ref
+                      .read(newsStateProvider.notifier)
+                      .changeNewsPostSpaceId(context);
+                },
+                child: SpaceChip(spaceId: newsPostSpaceId),
+              )
+            : OutlinedButton(
+                key: NewsUpdateKeys.selectSpace,
+                onPressed: () async {
+                  await ref
+                      .read(newsStateProvider.notifier)
+                      .changeNewsPostSpaceId(context);
+                },
+                child: const Text('Select Space'),
+              ),
+      ),
     );
   }
 
@@ -304,5 +357,122 @@ class _AddNewsState extends ConsumerState<AddNewsPage> {
         videoFile: File(videoFile.path),
       ),
     );
+  }
+
+  Future<void> sendNews(BuildContext context) async {
+    final client = ref.read(alwaysClientProvider);
+    final spaceId = ref.read(newsStateProvider).newsPostSpaceId;
+    final newsSlideList = ref.watch(newsStateProvider).newsSlideList;
+
+    if (spaceId == null) {
+      customMsgSnackbar(context, 'Please first select a space');
+      return;
+    }
+
+    String displayMsg = 'Slide posting';
+    // Show loading message
+    EasyLoading.show(status: displayMsg);
+    try {
+      final space = await ref.read(spaceProvider(spaceId).future);
+      NewsEntryDraft draft = space.newsDraft();
+      for (final slidePost in newsSlideList) {
+        final sdk = await ref.read(sdkProvider.future);
+        // If slide type is text
+        if (slidePost.type == NewsSlideType.text && slidePost.text != null) {
+          if (slidePost.text!.isEmpty) {
+            EasyLoading.showError('Please add some text');
+            return;
+          }
+
+          final textDraft = client.textMarkdownDraft(slidePost.text!);
+          final textSlideDraft = textDraft.intoNewsSlideDraft();
+
+          textSlideDraft.color(
+            sdk.api.newColorizeBuilder(
+              null,
+              slidePost.backgroundColor?.value,
+            ),
+          );
+          await draft.addSlide(textSlideDraft);
+        }
+
+        // If slide type is image
+        else if (slidePost.type == NewsSlideType.image &&
+            slidePost.mediaFile != null) {
+          final file = slidePost.mediaFile!;
+          String? mimeType = file.mimeType ?? lookupMimeType(file.path);
+          if (mimeType == null) {
+            EasyLoading.showError('Invalid media format');
+            return;
+          }
+          if (!mimeType.startsWith('image/')) {
+            EasyLoading.showError(
+              'Posting of $mimeType not yet supported',
+            );
+            return;
+          }
+          Uint8List bytes = await file.readAsBytes();
+          final decodedImage = await decodeImageFromList(bytes);
+          final imageDraft = client
+              .imageDraft(file.path, mimeType)
+              .size(bytes.length)
+              .width(decodedImage.width)
+              .height(decodedImage.height);
+          final imageSlideDraft = imageDraft.intoNewsSlideDraft();
+          imageSlideDraft.color(
+            sdk.api.newColorizeBuilder(
+              null,
+              slidePost.backgroundColor?.value,
+            ),
+          );
+          await draft.addSlide(imageSlideDraft);
+        }
+
+        // If slide type is video
+        else if (slidePost.type == NewsSlideType.video &&
+            slidePost.mediaFile != null) {
+          final file = slidePost.mediaFile!;
+          String? mimeType = file.mimeType ?? lookupMimeType(file.path);
+          if (mimeType == null) {
+            EasyLoading.showError('Invalid media format');
+            return;
+          }
+          if (!mimeType.startsWith('video/')) {
+            EasyLoading.showError(
+              'Posting of $mimeType not yet supported',
+            );
+            return;
+          }
+          Uint8List bytes = await file.readAsBytes();
+          final videoDraft =
+              client.videoDraft(file.path, mimeType).size(bytes.length);
+          final videoSlideDraft = videoDraft.intoNewsSlideDraft();
+          videoSlideDraft.color(
+            sdk.api.newColorizeBuilder(
+              null,
+              slidePost.backgroundColor?.value,
+            ),
+          );
+          await draft.addSlide(videoSlideDraft);
+        }
+      }
+      await draft.send();
+
+      // close loading
+      EasyLoading.dismiss();
+
+      // We are doing as expected, but the lints triggers.
+      // ignore: use_build_context_synchronously
+      if (!context.mounted) {
+        return;
+      }
+      // FIXME due to #718. well lets at least try forcing a refresh upon route.
+      ref.invalidate(newsListProvider);
+      ref.invalidate(newsStateProvider);
+      // Navigate back to update screen.
+      Navigator.of(context).pop();
+    } catch (err) {
+      EasyLoading.showError('$displayMsg failed: \n $err');
+    }
   }
 }
--- app/lib/features/news/widgets/news_post_editor/news_slide_options.dart
+++ app/lib/features/news/widgets/news_post_editor/news_slide_options.dart
@@ -1,23 +1,12 @@
-import 'package:acter/common/providers/sdk_provider.dart';
-import 'package:acter/common/providers/space_providers.dart';
-import 'package:acter/common/snackbars/custom_msg.dart';
-import 'package:acter/features/home/providers/client_providers.dart';
-import 'package:acter/features/home/widgets/space_chip.dart';
-import 'package:acter/features/news/model/keys.dart';
 import 'package:acter/features/news/model/news_slide_model.dart';
 import 'package:acter/features/news/news_utils/news_utils.dart';
 import 'package:acter/features/news/providers/news_post_editor_providers.dart';
-import 'package:acter/features/news/providers/news_providers.dart';
 import 'package:acter/features/news/widgets/news_post_editor/post_attachment_options.dart';
-import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
 import 'package:atlas_icons/atlas_icons.dart';
 import 'package:cross_file_image/cross_file_image.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:mime/mime.dart';
 
 class NewsSlideOptions extends ConsumerStatefulWidget {
   const NewsSlideOptions({super.key});
@@ -36,69 +25,13 @@ class _NewsSlideOptionsState extends ConsumerState<NewsSlideOptions> {
   }
 
   Widget newsSlideOptionsUI(BuildContext context) {
-    final newsPostSpaceId = ref.watch(newsStateProvider).newsPostSpaceId;
     return Visibility(
       visible: ref.read(newsStateProvider).currentNewsSlide != null,
       child: Container(
         color: Theme.of(context).colorScheme.primary,
         padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            if (newsPostSpaceId != null)
-              InkWell(
-                key: NewsUpdateKeys.selectSpace,
-                onTap: () async {
-                  await ref
-                      .read(newsStateProvider.notifier)
-                      .changeNewsPostSpaceId(context);
-                },
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Space',
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                    SpaceChip(spaceId: newsPostSpaceId),
-                  ],
-                ),
-              )
-            else
-              OutlinedButton(
-                key: NewsUpdateKeys.selectSpace,
-                onPressed: () async {
-                  await ref
-                      .read(newsStateProvider.notifier)
-                      .changeNewsPostSpaceId(context);
-                },
-                child: const Text('Select Space'),
-              ),
-            verticalDivider(context),
-            Expanded(child: newsSlideListUI(context)),
-            IconButton(
-              key: NewsUpdateKeys.newsSubmitBtn,
-              onPressed: () => sendNews(context),
-              style: ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(
-                  Theme.of(context).colorScheme.background,
-                ),
-              ),
-              icon: const Icon(Icons.send),
-            ),
-          ],
-        ),
+        child: newsSlideListUI(context),
       ),
-    );
-  }
-
-  Widget verticalDivider(BuildContext context) {
-    return Container(
-      height: 50,
-      width: 2,
-      margin: const EdgeInsets.symmetric(horizontal: 10.0),
-      color: Theme.of(context).colorScheme.onPrimary,
     );
   }
 
@@ -167,16 +100,6 @@ class _NewsSlideOptionsState extends ConsumerState<NewsSlideOptions> {
         ),
         IconButton(
           onPressed: () => showPostAttachmentOptions(context),
-          style: ButtonStyle(
-            backgroundColor: MaterialStatePropertyAll(
-              Theme.of(context).colorScheme.background,
-            ),
-            shape: MaterialStatePropertyAll(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5),
-              ),
-            ),
-          ),
           icon: const Icon(Icons.add),
         ),
       ],
@@ -216,139 +139,32 @@ class _NewsSlideOptionsState extends ConsumerState<NewsSlideOptions> {
           ),
         );
       case NewsSlideType.video:
-        return FutureBuilder(
-          future: NewsUtils.getThumbnailData(mediaFile!),
-          builder: (context, snapshot) {
-            if (snapshot.hasData && snapshot.data != null) {
-              return ClipRRect(
-                borderRadius: BorderRadius.circular(5.0),
-                child: Image.file(
-                  snapshot.data!,
-                  fit: BoxFit.cover,
-                ),
-              );
-            }
-            return const SizedBox.shrink();
-          },
+        return Stack(
+          fit: StackFit.expand,
+          children: [
+            FutureBuilder(
+              future: NewsUtils.getThumbnailData(mediaFile!),
+              builder: (context, snapshot) {
+                if (snapshot.hasData && snapshot.data != null) {
+                  return ClipRRect(
+                    borderRadius: BorderRadius.circular(5.0),
+                    child: Image.file(
+                      snapshot.data!,
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+            Container(
+              color: Colors.black38,
+              child: const Icon(Icons.play_arrow_outlined, size: 32),
+            ),
+          ],
         );
       default:
         return Container();
-    }
-  }
-
-  Future<void> sendNews(BuildContext context) async {
-    final client = ref.read(alwaysClientProvider);
-    final spaceId = ref.read(newsStateProvider).newsPostSpaceId;
-
-    if (spaceId == null) {
-      customMsgSnackbar(context, 'Please first select a space');
-      return;
-    }
-
-    String displayMsg = 'Slide posting';
-    // Show loading message
-    EasyLoading.show(status: displayMsg);
-    try {
-      final space = await ref.read(spaceProvider(spaceId).future);
-      NewsEntryDraft draft = space.newsDraft();
-      for (final slidePost in newsSlideList) {
-        final sdk = await ref.read(sdkProvider.future);
-        // If slide type is text
-        if (slidePost.type == NewsSlideType.text && slidePost.text != null) {
-          if (slidePost.text!.isEmpty) {
-            EasyLoading.showError('Please add some text');
-            return;
-          }
-
-          final textDraft = client.textMarkdownDraft(slidePost.text!);
-          final textSlideDraft = textDraft.intoNewsSlideDraft();
-
-          textSlideDraft.color(
-            sdk.api.newColorizeBuilder(
-              null,
-              slidePost.backgroundColor?.value,
-            ),
-          );
-          await draft.addSlide(textSlideDraft);
-        }
-
-        // If slide type is image
-        else if (slidePost.type == NewsSlideType.image &&
-            slidePost.mediaFile != null) {
-          final file = slidePost.mediaFile!;
-          String? mimeType = file.mimeType ?? lookupMimeType(file.path);
-          if (mimeType == null) {
-            EasyLoading.showError('Invalid media format');
-            return;
-          }
-          if (!mimeType.startsWith('image/')) {
-            EasyLoading.showError(
-              'Posting of $mimeType not yet supported',
-            );
-            return;
-          }
-          Uint8List bytes = await file.readAsBytes();
-          final decodedImage = await decodeImageFromList(bytes);
-          final imageDraft = client
-              .imageDraft(file.path, mimeType)
-              .size(bytes.length)
-              .width(decodedImage.width)
-              .height(decodedImage.height);
-          final imageSlideDraft = imageDraft.intoNewsSlideDraft();
-          imageSlideDraft.color(
-            sdk.api.newColorizeBuilder(
-              null,
-              slidePost.backgroundColor?.value,
-            ),
-          );
-          await draft.addSlide(imageSlideDraft);
-        }
-
-        // If slide type is video
-        else if (slidePost.type == NewsSlideType.video &&
-            slidePost.mediaFile != null) {
-          final file = slidePost.mediaFile!;
-          String? mimeType = file.mimeType ?? lookupMimeType(file.path);
-          if (mimeType == null) {
-            EasyLoading.showError('Invalid media format');
-            return;
-          }
-          if (!mimeType.startsWith('video/')) {
-            EasyLoading.showError(
-              'Posting of $mimeType not yet supported',
-            );
-            return;
-          }
-          Uint8List bytes = await file.readAsBytes();
-          final videoDraft =
-              client.videoDraft(file.path, mimeType).size(bytes.length);
-          final videoSlideDraft = videoDraft.intoNewsSlideDraft();
-          videoSlideDraft.color(
-            sdk.api.newColorizeBuilder(
-              null,
-              slidePost.backgroundColor?.value,
-            ),
-          );
-          await draft.addSlide(videoSlideDraft);
-        }
-      }
-      await draft.send();
-
-      // close loading
-      EasyLoading.dismiss();
-
-      // We are doing as expected, but the lints triggers.
-      // ignore: use_build_context_synchronously
-      if (!context.mounted) {
-        return;
-      }
-      // FIXME due to #718. well lets at least try forcing a refresh upon route.
-      ref.invalidate(newsListProvider);
-      ref.invalidate(newsStateProvider);
-      // Navigate back to update screen.
-      Navigator.of(context).pop();
-    } catch (err) {
-      EasyLoading.showError('$displayMsg failed: \n $err');
     }
   }
 }
