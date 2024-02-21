426a04bf4ed11e9343612ba20634d3e9902c6c07
Benjamin Kampmann <ben@acter.global>
2024-02-21 18:31:59 +0000

    disable images on darwin for now

--- app/lib/common/notifications/notifications.dart
+++ app/lib/common/notifications/notifications.dart
@@ -515,11 +515,13 @@ Future<void> _showNotification(
   if (msg != null) {
     body = msg.body();
   }
-  if (notification.hasImage()) {
-    final tempDir = await getTemporaryDirectory();
-    final filePath = await notification.imagePath(tempDir.path);
-    attachments.add(DarwinNotificationAttachment(filePath));
-  }
+  // FIXME: currently failing with 
+  // Parsing Notification failed: PlatformException(Error 101, Unrecognized attachment file type, UNErrorDomain, null)
+  // if (notification.hasImage()) {
+  //   final tempDir = await getTemporaryDirectory();
+  //   final filePath = await notification.imagePath(tempDir.path);
+  //   attachments.add(DarwinNotificationAttachment(filePath));
+  // }
 
   final darwinDetails = DarwinNotificationDetails(
     threadIdentifier: notification.threadId(),
