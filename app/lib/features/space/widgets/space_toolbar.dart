80f6a6a6ebbbb4214dd270616971dca5ff999085
Benjamin Kampmann <ben@effektio.org>
2024-04-19 11:11:13 +0100

    Move leaving space into its own dialog, improve design and usability

--- app/lib/features/space/widgets/space_toolbar.dart
+++ app/lib/features/space/widgets/space_toolbar.dart
@@ -1,11 +1,9 @@
 import 'package:acter/common/providers/room_providers.dart';
-import 'package:acter/common/providers/space_providers.dart';
 import 'package:acter/common/themes/app_theme.dart';
 import 'package:acter/common/utils/routes.dart';
-import 'package:acter/common/widgets/default_dialog.dart';
+import 'package:acter/features/space/dialogs/leave_space.dart';
 import 'package:atlas_icons/atlas_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
@@ -48,8 +46,13 @@ class SpaceToolbar extends ConsumerWidget {
       ),
       const PopupMenuDivider(),
       PopupMenuItem(
-        onTap: () => _handleLeaveSpace(context, ref),
-        child: Text(L10n.of(context).leaveSpace),
+        onTap: () => showLeaveSpaceDialog(context, ref, spaceId),
+        child: Text(
+          L10n.of(context).leaveSpace,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.error,
+          ),
+        ),
       ),
     ]);
 
@@ -79,59 +82,6 @@ class SpaceToolbar extends ConsumerWidget {
               color: Theme.of(context).colorScheme.surface,
               itemBuilder: (BuildContext context) => submenu,
             ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _handleLeaveSpace(
-    BuildContext context,
-    WidgetRef ref,
-  ) {
-    showAdaptiveDialog(
-      barrierDismissible: false,
-      context: context,
-      builder: (context) => DefaultDialog(
-        title: Column(
-          children: <Widget>[
-            const Icon(Icons.person_remove_outlined),
-            const SizedBox(height: 5),
-            Text(
-              L10n.of(context).leaveSpace,
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-          ],
-        ),
-        subtitle: Text(
-          L10n.of(context).areYouSureYouWantToLeaveSpace,
-        ),
-        actions: <Widget>[
-          ElevatedButton(
-            onPressed: () => context.pop(),
-            child: Text(L10n.of(context).noIStay),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              final lang = L10n.of(context);
-              try {
-                EasyLoading.show(status: lang.leavingSpace);
-                final space = await ref.read(spaceProvider(spaceId).future);
-                await space.leave();
-                // refresh spaces list
-                ref.invalidate(spacesProvider);
-                if (!context.mounted) {
-                  return;
-                }
-                EasyLoading.showToast(lang.leavingSpaceSuccessful);
-                context.pop();
-                context.goNamed(Routes.dashboard.name);
-              } catch (error, stack) {
-                _log.severe('Error leaving space', error, stack);
-                EasyLoading.showError(lang.leavingSpaceFailed(error));
-              }
-            },
-            child: Text(L10n.of(context).yesLeave),
           ),
         ],
       ),
