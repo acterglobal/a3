2d07507f181d0acec49775bf3016f35851775e90
Benjamin Kampmann <ben@effektio.org>
2024-04-19 11:20:42 +0100

    Make space top a proper AppBar for better usability

--- app/lib/features/space/widgets/space_toolbar.dart
+++ app/lib/features/space/widgets/space_toolbar.dart
@@ -2,7 +2,6 @@ import 'package:acter/common/providers/room_providers.dart';
 import 'package:acter/common/themes/app_theme.dart';
 import 'package:acter/common/utils/routes.dart';
 import 'package:acter/features/space/dialogs/leave_space.dart';
-import 'package:atlas_icons/atlas_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -56,35 +55,20 @@ class SpaceToolbar extends ConsumerWidget {
       ),
     ]);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-      child: Row(
-        children: [
-          InkWell(
-            onTap: () => context.canPop()
-                ? context.pop()
-                : context.goNamed(Routes.dashboard.name),
-            child: Icon(
-              Atlas.arrow_left,
-              color: Theme.of(context).colorScheme.neutral5,
-            ),
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      actions: [
+        PopupMenuButton(
+          icon: Icon(
+            key: optionsMenu,
+            Icons.more_vert,
+            color: Theme.of(context).colorScheme.neutral5,
           ),
-          const Spacer(),
-          Padding(
-            padding: const EdgeInsets.only(top: 18),
-            child: PopupMenuButton(
-              icon: Icon(
-                key: optionsMenu,
-                Icons.more_vert,
-                color: Theme.of(context).colorScheme.neutral5,
-              ),
-              iconSize: 28,
-              color: Theme.of(context).colorScheme.surface,
-              itemBuilder: (BuildContext context) => submenu,
-            ),
-          ),
-        ],
-      ),
+          iconSize: 28,
+          color: Theme.of(context).colorScheme.surface,
+          itemBuilder: (BuildContext context) => submenu,
+        ),
+      ],
     );
   }
 }
