3b251ef3645e825de1e0c70d6e6c3f36e008517f
Benjamin Kampmann <ben@effektio.org>
2024-04-22 14:31:51 +0100

    removed unused log

--- app/lib/features/space/widgets/space_toolbar.dart
+++ app/lib/features/space/widgets/space_toolbar.dart
@@ -6,9 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
-import 'package:logging/logging.dart';
-
-final _log = Logger('a3::spaces::toolbar');
 
 class SpaceToolbar extends ConsumerWidget {
   static const optionsMenu = Key('space-options-menu');
