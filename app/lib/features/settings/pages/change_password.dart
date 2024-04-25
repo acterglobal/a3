9589768eeabf211616fbfe2c98bcc89f75691b40
kumarpalsinh25 <info@dajiraj.com>
2024-04-25 12:38:11 +0530

    Resolve lint error

--- app/lib/features/settings/pages/change_password.dart
+++ app/lib/features/settings/pages/change_password.dart
@@ -4,10 +4,7 @@ import 'package:acter/features/settings/pages/settings_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:logging/logging.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
-
-final _log = Logger('a3::settings::change_password');
 
 class ChangePasswordPage extends ConsumerStatefulWidget {
   const ChangePasswordPage({super.key});
