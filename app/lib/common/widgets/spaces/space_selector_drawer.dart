65cc0da2573075c2e9dc4bbff61ad6af416ce798
Benjamin Kampmann <ben@effektio.org>
2024-04-30 18:27:47 +0100

    Implement search fo selecting space drawer

--- app/lib/common/widgets/spaces/space_selector_drawer.dart
+++ app/lib/common/widgets/spaces/space_selector_drawer.dart
@@ -10,6 +10,155 @@ import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 const Key selectSpaceDrawerKey = Key('space-widgets-select-space-drawer');
 
+class _SelectSpaceDrawer extends ConsumerStatefulWidget {
+  final String canCheck;
+  final String? currentSpaceId;
+  final Widget? title;
+  const _SelectSpaceDrawer({
+    super.key,
+    this.canCheck = 'CanLinkSpaces',
+    this.currentSpaceId,
+    this.title,
+  });
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      __SelectSpaceDrawerState();
+}
+
+class __SelectSpaceDrawerState extends ConsumerState<_SelectSpaceDrawer> {
+  final TextEditingController searchTextController = TextEditingController();
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // ensure we are synced up
+    searchTextController.text = ref.read(spaceSearchValueProvider) ?? '';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        key: widget.key,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          title(context),
+          searchBar(context),
+          Flexible(
+            child: spacesList(context),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget title(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      automaticallyImplyLeading: false,
+      title: widget.title ?? Text(L10n.of(context).selectSpace),
+      actions: [
+        TextButton.icon(
+          icon: const Icon(Atlas.minus_circle_thin),
+          onPressed: () {
+            Navigator.pop(context, '');
+          },
+          label: Text(L10n.of(context).clear),
+        ),
+      ],
+    );
+  }
+
+  Widget searchBar(BuildContext context) {
+    final spaces = ref.watch(spacesProvider);
+    if (spaces.length < 10) {
+      // small list, ignore
+      return const SizedBox.shrink();
+    }
+
+    final hasSearchTerm = (ref.read(spaceSearchValueProvider) ?? '').isNotEmpty;
+
+    return SearchBar(
+      controller: searchTextController,
+      leading: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Icon(Atlas.magnifying_glass),
+      ),
+      hintText: L10n.of(context).search,
+      trailing: hasSearchTerm
+          ? [
+              InkWell(
+                onTap: () {
+                  searchTextController.clear();
+                  ref.read(spaceSearchValueProvider.notifier).state = '';
+                },
+                child: const Icon(Icons.clear),
+              ),
+            ]
+          : null,
+      onChanged: (value) {
+        ref.read(spaceSearchValueProvider.notifier).state = value;
+      },
+    );
+  }
+
+  Widget spacesList(BuildContext context) {
+    final searchValue = ref.watch(spaceSearchValueProvider);
+    if (searchValue?.isNotEmpty == true) {
+      return searchedSpaceList(context);
+    }
+
+    final spaces =
+        ref.watch(spacesProvider).map((space) => space.getRoomIdStr()).toList();
+    return spaceListUI(spaces);
+  }
+
+//Show space list based on the search term
+  Widget searchedSpaceList(BuildContext context) {
+    final searchedSpaces = ref.watch(searchedSpacesProvider);
+    return searchedSpaces.when(
+      data: (spaces) {
+        if (spaces.isEmpty) {
+          return Center(
+            heightFactor: 10,
+            child: Text(L10n.of(context).noChatsFoundMatchingYourSearchTerm),
+          );
+        }
+        return spaceListUI(spaces);
+      },
+      loading: () => const Center(
+        heightFactor: 10,
+        child: CircularProgressIndicator(),
+      ),
+      error: (e, s) => Center(child: Text(L10n.of(context).searchingFailed(e))),
+    );
+  }
+
+  Widget spaceListUI(List<String> spaces) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemCount: spaces.length,
+      itemBuilder: (context, index) {
+        final roomId = spaces[index];
+        return BriefRoomEntry(
+          roomId: roomId,
+          avatarDisplayMode: DisplayMode.Space,
+          keyPrefix: 'select-space',
+          selectedValue: current,
+          canCheck: widget.canCheck,
+          onSelect: (roomId) {
+            Navigator.pop(context, roomId);
+          },
+        );
+      },
+    );
+  }
+}
+
 Future<String?> selectSpaceDrawer({
   required BuildContext context,
   Key? key = selectSpaceDrawerKey,
@@ -22,56 +171,11 @@ Future<String?> selectSpaceDrawer({
     enableDrag: true,
     context: context,
     isDismissible: true,
-    builder: (context) => Consumer(
-      builder: (context, ref, child) {
-        final spaces = ref.watch(spacesProvider);
-        return Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            key: key,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Row(
-                children: [
-                  Expanded(
-                    child: title ?? Text(L10n.of(context).selectSpace),
-                  ),
-                  OutlinedButton.icon(
-                    icon: const Icon(Atlas.minus_circle_thin),
-                    onPressed: () {
-                      Navigator.pop(context, '');
-                    },
-                    label: Text(L10n.of(context).clear),
-                  ),
-                ],
-              ),
-              Flexible(
-                child: spaces.isEmpty
-                    ? Text(L10n.of(context).noSpacesFound)
-                    : ListView.builder(
-                        padding: const EdgeInsets.all(8),
-                        itemCount: spaces.length,
-                        itemBuilder: (context, index) {
-                          final space = spaces[index];
-                          return BriefRoomEntry(
-                            roomId: space.getRoomIdStr(),
-                            avatarDisplayMode: DisplayMode.Space,
-                            keyPrefix: 'select-space',
-                            selectedValue: current,
-                            canCheck: canCheck,
-                            onSelect: (roomId) {
-                              Navigator.pop(context, roomId);
-                            },
-                          );
-                        },
-                      ),
-              ),
-            ],
-          ),
-        );
-      },
+    builder: (context) => _SelectSpaceDrawer(
+      key: key,
+      canCheck: canCheck,
+      currentSpaceId: currentSpaceId,
+      title: title,
     ),
   );
   if (selected == null) {
