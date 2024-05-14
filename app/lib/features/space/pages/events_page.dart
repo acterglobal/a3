52a0ac80f3172033142319fb17f32f713b0ae292
kumarpalsinh25 <info@dajiraj.com>
2024-05-14 12:29:11 +0530

    Separate upcoming and past events list in space

--- app/lib/features/space/pages/events_page.dart
+++ app/lib/features/space/pages/events_page.dart
@@ -22,7 +22,8 @@ class SpaceEventsPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final spaceEvents = ref.watch(spaceEventsProvider(spaceIdOrAlias));
+    final upcoming = ref.watch(spaceUpcomingEventsProvider(spaceIdOrAlias));
+    final past = ref.watch(spacePastEventsProvider(spaceIdOrAlias));
     return DecoratedBox(
       decoration: const BoxDecoration(gradient: primaryGradient),
       child: CustomScrollView(
@@ -30,9 +31,9 @@ class SpaceEventsPage extends ConsumerWidget {
           SliverToBoxAdapter(
             child: SpaceHeader(spaceIdOrAlias: spaceIdOrAlias),
           ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            sliver: SliverToBoxAdapter(
               child: Row(
                 children: [
                   Text(
@@ -52,11 +53,87 @@ class SpaceEventsPage extends ConsumerWidget {
               ),
             ),
           ),
-          spaceEvents.when(
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            sliver: SliverToBoxAdapter(
+              child: Text(
+                L10n.of(context).upcoming,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+          ),
+          upcoming.when(
             data: (events) {
               final widthCount =
                   (MediaQuery.of(context).size.width ~/ 300).toInt();
               const int minCount = 3;
+              if (events.isEmpty) {
+                final membership =
+                    ref.watch(roomMembershipProvider(spaceIdOrAlias));
+                bool canCreateEvent =
+                    membership.requireValue!.canString('CanPostEvent');
+                return SliverToBoxAdapter(
+                  child: Center(
+                    heightFactor: 1,
+                    child: EmptyState(
+                      title: L10n.of(context).noEventsPlannedYet,
+                      subtitle:
+                          L10n.of(context).createEventAndBringYourCommunity,
+                      image: 'assets/images/empty_event.svg',
+                      primaryButton: canCreateEvent
+                          ? ActerPrimaryActionButton(
+                              onPressed: () => context.pushNamed(
+                                Routes.createEvent.name,
+                                queryParameters: {'spaceId': spaceIdOrAlias},
+                              ),
+                              child: Text(L10n.of(context).eventCreate),
+                            )
+                          : null,
+                    ),
+                  ),
+                );
+              }
+              return SliverGrid.builder(
+                itemCount: events.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: max(1, min(widthCount, minCount)),
+                  childAspectRatio: 4.0,
+                  mainAxisExtent: 120,
+                ),
+                itemBuilder: (context, index) =>
+                    EventItem(event: events[index]),
+              );
+            },
+            error: (error, stackTrace) => SliverToBoxAdapter(
+              child: Center(
+                child: Text(L10n.of(context).failedToLoadEventsDueTo(error)),
+              ),
+            ),
+            loading: () => const SliverToBoxAdapter(
+              child: Center(child: EventListSkeleton()),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+            sliver: SliverToBoxAdapter(
+              child: Text(
+                L10n.of(context).past,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+          ),
+          past.when(
+            data: (events) {
+              final widthCount =
+                  (MediaQuery.of(context).size.width ~/ 300).toInt();
+              const int minCount = 3;
+              if (events.isEmpty) {
+                return SliverToBoxAdapter(
+                  child: Center(
+                    child: Text(L10n.of(context).thereIsNothingScheduledYet),
+                  ),
+                );
+              }
               if (events.isEmpty) {
                 final membership =
                     ref.watch(roomMembershipProvider(spaceIdOrAlias));
