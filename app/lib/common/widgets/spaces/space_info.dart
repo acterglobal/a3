2fa89215c02ccd49f62a145e384c2ff911b261dd
kumarpalsinh25 <info@dajiraj.com>
2024-05-15 12:20:08 +0530

    Optimize code structure

--- app/lib/common/widgets/spaces/space_info.dart
+++ app/lib/common/widgets/spaces/space_info.dart
@@ -29,152 +29,160 @@ class SpaceInfo extends ConsumerWidget {
     final space = ref.watch(spaceProvider(spaceId));
     return space.when(
       data: (space) {
-        final isEncrypted = ref.watch(isEncryptedForSpace(space));
-        final isActerSpace = ref.watch(isActerSpaceForSpace(space));
-        final joinRuleStr = space.joinRuleStr();
-
-        Widget joinRule;
-        switch (joinRuleStr) {
-          case 'invite':
-            joinRule = Tooltip(
-              message: L10n.of(context).thisSpaceIsInviteOnly,
-              child: Icon(
-                Atlas.shield_envelope_thin,
-                size: size,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-            );
-          case 'restricted':
-            final roomIds =
-                asDartStringList(space.restrictedRoomIdsStr()).join(', ');
-            joinRule = Tooltip(
-              message: L10n.of(context).anyoneFromTheseRoomsCanJoin(roomIds),
-              child: Icon(
-                Atlas.arrow_down_shield_thin,
-                size: size,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-            );
-          case 'knock':
-            joinRule = Tooltip(
-              message: L10n.of(context).anyoneCanAskToJoinThisSpace,
-              child: Icon(
-                Atlas.arrow_down_shield_thin,
-                size: size,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-            );
-          case 'knock_restricted':
-            final roomIds =
-                asDartStringList(space.restrictedRoomIdsStr()).join(', ');
-            joinRule = Tooltip(
-              message:
-                  L10n.of(context).anyoneFromTheseRoomsCanAskToJoin(roomIds),
-              child: Icon(
-                Atlas.arrow_down_shield_thin,
-                size: size,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-            );
-          case 'public':
-            joinRule = Tooltip(
-              message: L10n.of(context).thisSpaceIsPubliclyAccessible,
-              child: Icon(
-                Atlas.glasses_vision_thin,
-                size: size,
-                color: Theme.of(context).colorScheme.error,
-              ),
-            );
-          case 'private':
-          default:
-            joinRule = Tooltip(
-              message: L10n.of(context).unclearJoinRule(joinRuleStr),
-              child: Icon(
-                Atlas.shield_exclamation_thin,
-                size: size,
-                color: Theme.of(context).colorScheme.error,
-              ),
-            );
-        }
-
         return Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            isActerSpace
-                    .whenData(
-                      (isProper) => isProper
-                          ? const SizedBox.shrink()
-                          : Padding(
-                              padding: const EdgeInsets.only(right: 3),
-                              child: Tooltip(
-                                message:
-                                    L10n.of(context).thisIsNotAProperActerSpace,
-                                child: Icon(
-                                  Atlas.triangle_exclamation_thin,
-                                  size: size,
-                                  color: Theme.of(context).colorScheme.error,
-                                ),
-                              ),
-                            ),
-                    )
-                    .valueOrNull ??
-                const SizedBox.shrink(),
-            isEncrypted
-                    .whenData(
-                      (isEnc) => isEnc
-                          ? Tooltip(
-                              message:
-                                  L10n.of(context).thisApaceIsEndToEndEncrypted,
-                              child: Icon(
-                                Atlas.lock_clipboard_thin,
-                                size: size,
-                                color: Theme.of(context).colorScheme.success,
-                              ),
-                            )
-                          : Tooltip(
-                              message: L10n.of(context)
-                                  .thisApaceIsNotEndToEndEncrypted,
-                              child: Icon(
-                                Atlas.unlock_keyhole_thin,
-                                size: size,
-                                color: Theme.of(context).colorScheme.error,
-                              ),
-                            ),
-                    )
-                    .valueOrNull ??
-                const SizedBox.shrink(),
-            joinRule,
+            acterSpaceInfoUI(context, ref, space),
+            encryptedInfoUI(context, ref, space),
+            joinRuleUI(context, space),
           ],
         );
       },
       error: (e, s) => Text(L10n.of(context).error(e)),
-      loading: () => Skeletonizer(
-        child: Row(
-          children: [
-            Tooltip(
-              message: '',
-              child: Icon(
-                Atlas.glasses_vision_thin,
-                size: size,
-              ),
+      loading: () => skeletonUI(),
+    );
+  }
+
+  Widget skeletonUI() {
+    return Skeletonizer(
+      child: Row(
+        children: [
+          Tooltip(
+            message: '',
+            child: Icon(
+              Atlas.glasses_vision_thin,
+              size: size,
             ),
-            Tooltip(
-              message: '',
-              child: Icon(
-                Atlas.lock_clipboard_thin,
-                size: size,
-              ),
+          ),
+          Tooltip(
+            message: '',
+            child: Icon(
+              Atlas.lock_clipboard_thin,
+              size: size,
             ),
-            Tooltip(
-              message: '',
-              child: Icon(
-                Atlas.shield_exclamation_thin,
-                size: size,
-              ),
+          ),
+          Tooltip(
+            message: '',
+            child: Icon(
+              Atlas.shield_exclamation_thin,
+              size: size,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
+  }
+
+  Widget acterSpaceInfoUI(BuildContext context, WidgetRef ref, Space space) {
+    final isActerSpace = ref.watch(isActerSpaceForSpace(space));
+    return isActerSpace
+            .whenData(
+              (isProper) => isProper
+                  ? const SizedBox.shrink()
+                  : Padding(
+                      padding: const EdgeInsets.only(right: 3),
+                      child: Tooltip(
+                        message: L10n.of(context).thisIsNotAProperActerSpace,
+                        child: Icon(
+                          Atlas.triangle_exclamation_thin,
+                          size: size,
+                          color: Theme.of(context).colorScheme.error,
+                        ),
+                      ),
+                    ),
+            )
+            .valueOrNull ??
+        const SizedBox.shrink();
+  }
+
+  Widget encryptedInfoUI(BuildContext context, WidgetRef ref, Space space) {
+    final isEncrypted = ref.watch(isEncryptedForSpace(space));
+    return isEncrypted
+            .whenData(
+              (isEnc) => isEnc
+                  ? Tooltip(
+                      message: L10n.of(context).thisApaceIsEndToEndEncrypted,
+                      child: Icon(
+                        Atlas.lock_clipboard_thin,
+                        size: size,
+                        color: Theme.of(context).colorScheme.success,
+                      ),
+                    )
+                  : Tooltip(
+                      message: L10n.of(context).thisApaceIsNotEndToEndEncrypted,
+                      child: Icon(
+                        Atlas.unlock_keyhole_thin,
+                        size: size,
+                        color: Theme.of(context).colorScheme.error,
+                      ),
+                    ),
+            )
+            .valueOrNull ??
+        const SizedBox.shrink();
+  }
+
+  Widget joinRuleUI(BuildContext context, Space space) {
+    final joinRuleStr = space.joinRuleStr();
+    switch (joinRuleStr) {
+      case 'invite':
+        return Tooltip(
+          message: L10n.of(context).thisSpaceIsInviteOnly,
+          child: Icon(
+            Atlas.shield_envelope_thin,
+            size: size,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        );
+      case 'restricted':
+        final roomIds =
+            asDartStringList(space.restrictedRoomIdsStr()).join(', ');
+        return Tooltip(
+          message: L10n.of(context).anyoneFromTheseRoomsCanJoin(roomIds),
+          child: Icon(
+            Atlas.arrow_down_shield_thin,
+            size: size,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        );
+      case 'knock':
+        return Tooltip(
+          message: L10n.of(context).anyoneCanAskToJoinThisSpace,
+          child: Icon(
+            Atlas.arrow_down_shield_thin,
+            size: size,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        );
+      case 'knock_restricted':
+        final roomIds =
+            asDartStringList(space.restrictedRoomIdsStr()).join(', ');
+        return Tooltip(
+          message: L10n.of(context).anyoneFromTheseRoomsCanAskToJoin(roomIds),
+          child: Icon(
+            Atlas.arrow_down_shield_thin,
+            size: size,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        );
+      case 'public':
+        return Tooltip(
+          message: L10n.of(context).thisSpaceIsPubliclyAccessible,
+          child: Icon(
+            Atlas.glasses_vision_thin,
+            size: size,
+            color: Theme.of(context).colorScheme.error,
+          ),
+        );
+      case 'private':
+      default:
+        return Tooltip(
+          message: L10n.of(context).unclearJoinRule(joinRuleStr),
+          child: Icon(
+            Atlas.shield_exclamation_thin,
+            size: size,
+            color: Theme.of(context).colorScheme.error,
+          ),
+        );
+    }
   }
 }
