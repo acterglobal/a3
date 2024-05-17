a15a2b7e1711252085aa5d5bcada5a3194ed3dd3
Benjamin Kampmann <ben@effektio.org>
2024-05-17 16:21:13 +0100

    You support for membership changes

--- app/lib/features/chat/widgets/messages/membership_update.dart
+++ app/lib/features/chat/widgets/messages/membership_update.dart
@@ -1,26 +1,49 @@
+import 'package:acter/common/providers/common_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MembershipUpdateWidget extends StatelessWidget {
+class MembershipUpdateWidget extends ConsumerWidget {
   final CustomMessage message;
   const MembershipUpdateWidget({super.key, required this.message});
 
   @override
-  Widget build(BuildContext context) {
-    final textMsg = switch (message.metadata?['msgType']) {
-      'Joined' => message.author.firstName != null
-          ? L10n.of(context).chatJoinedDisplayName(message.author.firstName!)
-          : L10n.of(context).chatJoinedUserId(message.author.id),
-      'InvitationAccepted' => message.author.firstName != null
-          ? L10n.of(context)
-              .chatInvitationAcceptedDisplayName(message.author.firstName!)
-          : L10n.of(context).chatInvitationAcceptedUserId(message.author.id),
-      'Invited' => message.author.firstName != null
-          ? L10n.of(context).chatInvitedDisplayName(message.author.firstName!)
-          : L10n.of(context).chatInvitedUserId(message.author.id),
-      _ => message.metadata?['body'] ?? '',
-    };
+  Widget build(BuildContext context, WidgetRef ref) {
+    final myUserId = ref.watch(myUserIdStrProvider);
+    String? textMsg;
+    final msgType = message.metadata?['msgType'];
+    if (msgType == 'Joined') {
+      if (message.author.id == myUserId) {
+        textMsg = L10n.of(context).chatYouJoined;
+      } else if (message.author.firstName != null) {
+        textMsg =
+            L10n.of(context).chatJoinedDisplayName(message.author.firstName!);
+      } else {
+        textMsg = L10n.of(context).chatJoinedUserId(message.author.id);
+      }
+    } else if (msgType == 'InvitationAccepted') {
+      if (message.author.id == myUserId) {
+        textMsg = L10n.of(context).chatYouAcceptedInvite;
+      } else if (message.author.firstName != null) {
+        textMsg = L10n.of(context)
+            .chatInvitationAcceptedDisplayName(message.author.firstName!);
+      } else {
+        textMsg =
+            L10n.of(context).chatInvitationAcceptedUserId(message.author.id);
+      }
+    } else if (msgType == 'Invited') {
+      if (message.author.id == myUserId) {
+        textMsg = L10n.of(context).chatYouInvited;
+      } else if (message.author.firstName != null) {
+        textMsg =
+            L10n.of(context).chatInvitedDisplayName(message.author.firstName!);
+      } else {
+        textMsg = L10n.of(context).chatInvitedUserId(message.author.id);
+      }
+    } else {
+      textMsg = message.metadata?['body'] ?? '';
+    }
     return Container(
       padding: const EdgeInsets.only(left: 10, bottom: 5),
       child: RichText(
