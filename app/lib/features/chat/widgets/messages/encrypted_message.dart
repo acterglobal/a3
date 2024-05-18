35f2e4eab9668e161d2a96b341a038fc89f3b240
Benjamin Kampmann <ben@effektio.org>
2024-05-18 17:16:38 +0100

    Dialog for encrypted message info

--- app/lib/features/chat/widgets/messages/encrypted_message.dart
+++ app/lib/features/chat/widgets/messages/encrypted_message.dart
@@ -1,4 +1,5 @@
 import 'package:acter/common/themes/app_theme.dart';
+import 'package:acter/features/chat/dialogs/encryption_info_drawer.dart';
 import 'package:atlas_icons/atlas_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
@@ -8,29 +9,32 @@ class EncryptedMessageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(18),
-      child: Text.rich(
-        TextSpan(
-          children: [
-            WidgetSpan(
-              child: Padding(
-                padding: const EdgeInsets.only(right: 5),
-                child: Icon(
-                  Atlas.block_shield_thin,
-                  size: Theme.of(context).textTheme.labelSmall?.fontSize,
-                  color: Theme.of(context).textTheme.labelSmall?.color,
+    return GestureDetector(
+      onTap: () => showEncryptionInfoBottomSheet(context: context),
+      child: Container(
+        padding: const EdgeInsets.all(18),
+        child: Text.rich(
+          TextSpan(
+            children: [
+              WidgetSpan(
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 5),
+                  child: Icon(
+                    Atlas.block_shield_thin,
+                    size: Theme.of(context).textTheme.labelSmall?.fontSize,
+                    color: Theme.of(context).textTheme.labelSmall?.color,
+                  ),
                 ),
               ),
-            ),
-            TextSpan(
-              text: L10n.of(context).encryptedChatMessage,
-              style: Theme.of(context).textTheme.labelSmall!.copyWith(
-                    color: Theme.of(context).colorScheme.neutral5,
-                    fontStyle: FontStyle.italic,
-                  ),
-            ),
-          ],
+              TextSpan(
+                text: L10n.of(context).encryptedChatMessage,
+                style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                      color: Theme.of(context).colorScheme.neutral5,
+                      fontStyle: FontStyle.italic,
+                    ),
+              ),
+            ],
+          ),
         ),
       ),
     );
