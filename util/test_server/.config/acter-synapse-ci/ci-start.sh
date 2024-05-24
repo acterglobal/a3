2c4a2865b6a3ff238aff00d9b928382c94d31e6d
bitfriend <bitcapture@hotmail.com>
2024-05-24 16:49:01 +0800

    Fix parsing error in local docker compose

--- util/test_server/.config/acter-synapse-ci/ci-start.sh
+++ util/test_server/.config/acter-synapse-ci/ci-start.sh
@@ -10,31 +10,31 @@ echo """
 email:
   smtp_host: localhost
   smtp_port: 1025
-  smtp_user: "test"
-  smtp_pass: "test"
+  smtp_user: \"test\"
+  smtp_pass: \"test\"
   force_tls: true
   require_transport_security: true
   enable_tls: true
-  notif_from: "Your Friendly %(app)s homeserver <noreply@example.com>"
+  notif_from: \"Your Friendly %(app)s homeserver <noreply@example.com>\"
   app_name: Acter
   enable_notifs: true
   notif_for_new_users: false
-  client_base_url: "http://localhost/riot"
+  client_base_url: \"http://localhost/riot\"
   validation_token_lifetime: 15m
   invite_client_location: https://app.element.io
   can_verify_email: true
 
   subjects:
-    message_from_person_in_room: "[%(app)s] You have a message on %(app)s from %(person)s in the %(room)s room..."
-    message_from_person: "[%(app)s] You have a message on %(app)s from %(person)s..."
-    messages_from_person: "[%(app)s] You have messages on %(app)s from %(person)s..."
-    messages_in_room: "[%(app)s] You have messages on %(app)s in the %(room)s room..."
-    messages_in_room_and_others: "[%(app)s] You have messages on %(app)s in the %(room)s room and others..."
-    messages_from_person_and_others: "[%(app)s] You have messages on %(app)s from %(person)s and others..."
-    invite_from_person_to_room: "[%(app)s] %(person)s has invited you to join the %(room)s room on %(app)s..."
-    invite_from_person: "[%(app)s] %(person)s has invited you to chat on %(app)s..."
-    password_reset: "[%(server_name)s] Password reset"
-    email_validation: "[%(server_name)s] Validate your email"
+    message_from_person_in_room: \"[%(app)s] You have a message on %(app)s from %(person)s in the %(room)s room...\"
+    message_from_person: \"[%(app)s] You have a message on %(app)s from %(person)s...\"
+    messages_from_person: \"[%(app)s] You have messages on %(app)s from %(person)s...\"
+    messages_in_room: \"[%(app)s] You have messages on %(app)s in the %(room)s room...\"
+    messages_in_room_and_others: \"[%(app)s] You have messages on %(app)s in the %(room)s room and others...\"
+    messages_from_person_and_others: \"[%(app)s] You have messages on %(app)s from %(person)s and others...\"
+    invite_from_person_to_room: \"[%(app)s] %(person)s has invited you to join the %(room)s room on %(app)s...\"
+    invite_from_person: \"[%(app)s] %(person)s has invited you to chat on %(app)s...\"
+    password_reset: \"[%(server_name)s] Password reset\"
+    email_validation: \"[%(server_name)s] Validate your email\"
 
 allowed_local_3pids:
   - medium: email
@@ -89,7 +89,7 @@ rc_invites:
     burst_count: 1000
 
 modules:
-  - module: "synapse_super_invites.SynapseSuperInvites"
+  - module: \"synapse_super_invites.SynapseSuperInvites\"
     config:
       sql_url: sqlite:///data/super_invites.db
       generate_registration_token: true
