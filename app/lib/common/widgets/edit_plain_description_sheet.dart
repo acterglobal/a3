3e7de705e81209df084ce4e1428ed18aaf158203
kumarpalsinh25 <info@dajiraj.com>
2024-06-19 18:08:00 +0530

    Generalised Edit Description flow for Space and Chat

---
+++ app/lib/common/widgets/edit_plain_description_sheet.dart
@@ -0,0 +1,101 @@
+import 'package:acter/common/toolkit/buttons/primary_action_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:go_router/go_router.dart';
+
+void showEditPlainDescriptionBottomSheet({
+  required BuildContext context,
+  required String descriptionValue,
+  required Function(String) onSave,
+}) {
+  showModalBottomSheet(
+    showDragHandle: true,
+    useSafeArea: true,
+    context: context,
+    isDismissible: true,
+    constraints: const BoxConstraints(maxHeight: 500),
+    builder: (context) {
+      return EditPlainDescriptionSheet(
+        descriptionValue: descriptionValue,
+        onSave: onSave,
+      );
+    },
+  );
+}
+
+class EditPlainDescriptionSheet extends StatefulWidget {
+  final String descriptionValue;
+  final Function(String) onSave;
+
+  const EditPlainDescriptionSheet({
+    super.key,
+    required this.descriptionValue,
+    required this.onSave,
+  });
+
+  @override
+  State<EditPlainDescriptionSheet> createState() =>
+      _EditPlainDescriptionSheetState();
+}
+
+class _EditPlainDescriptionSheetState extends State<EditPlainDescriptionSheet> {
+  final _descriptionController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _descriptionController.text = widget.descriptionValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Text(L10n.of(context).editDescription),
+            const SizedBox(height: 20),
+            TextFormField(
+              keyboardType: TextInputType.multiline,
+              textInputAction: TextInputAction.newline,
+              controller: _descriptionController,
+              minLines: 4,
+              autofocus: true,
+              maxLines: 4,
+              decoration: InputDecoration(
+                hintText: L10n.of(context).description,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                OutlinedButton(
+                  onPressed: () => context.pop(),
+                  child: Text(L10n.of(context).cancel),
+                ),
+                const SizedBox(width: 20),
+                ActerPrimaryActionButton(
+                  onPressed: () {
+                    final newDescription = _descriptionController.text.trim();
+                    // No need to change
+                    if (newDescription == widget.descriptionValue.trim()) {
+                      context.pop();
+                      return;
+                    }
+
+                    // Need to update change of tile
+                    widget.onSave(newDescription);
+                  },
+                  child: Text(L10n.of(context).save),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- app/lib/features/space/widgets/about_card.dart
+++ app/lib/features/space/widgets/about_card.dart
@@ -1,7 +1,11 @@
 import 'package:acter/common/providers/space_providers.dart';
+import 'package:acter/common/widgets/edit_plain_description_sheet.dart';
+import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:go_router/go_router.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
 class AboutCard extends ConsumerWidget {
@@ -33,9 +37,21 @@ class AboutCard extends ConsumerWidget {
             space.when(
               data: (space) {
                 final topic = space.topic();
-                return Text(
-                  topic ?? L10n.of(context).noTopicFound,
-                  style: Theme.of(context).textTheme.bodySmall,
+                return GestureDetector(
+                  onTap: () async {
+                    if (await editDescriptionPermissionCheck(ref) &&
+                        context.mounted) {
+                      showEditDescriptionBottomSheet(
+                        context: context,
+                        space: space,
+                        descriptionValue: topic ?? '',
+                      );
+                    }
+                  },
+                  child: Text(
+                    topic ?? L10n.of(context).noTopicFound,
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
                 );
               },
               error: (error, stack) => Text(
@@ -49,6 +65,37 @@ class AboutCard extends ConsumerWidget {
           ],
         ),
       ),
+    );
+  }
+
+  // permission check
+  Future<bool> editDescriptionPermissionCheck(WidgetRef ref) async {
+    final space = await ref.read(spaceProvider(spaceId).future);
+    final membership = await space.getMyMembership();
+    return membership.canString('CanSetTopic');
+  }
+
+  void showEditDescriptionBottomSheet({
+    required BuildContext context,
+    required Space space,
+    required String descriptionValue,
+  }) {
+    showEditPlainDescriptionBottomSheet(
+      context: context,
+      descriptionValue: descriptionValue,
+      onSave: (newDescription) async {
+        try {
+          EasyLoading.show(status: L10n.of(context).updateDescription);
+          await space.setTopic(newDescription);
+          EasyLoading.dismiss();
+          if (!context.mounted) return;
+          context.pop();
+        } catch (e) {
+          EasyLoading.dismiss();
+          if (!context.mounted) return;
+          EasyLoading.showError(L10n.of(context).updateDescriptionFailed(e));
+        }
+      },
     );
   }
 }
