734ffa09f03b8d6cbb5cda2e0975fc9351afd6af
kumarpalsinh25 <info@dajiraj.com>
2024-07-04 22:55:03 +0530

    Permission checking for space action buttons

--- app/lib/features/space/widgets/space_sections/space_actions_section.dart
+++ app/lib/features/space/widgets/space_sections/space_actions_section.dart
@@ -1,9 +1,11 @@
+import 'package:acter/common/providers/room_providers.dart';
 import 'package:acter/features/space/widgets/space_sections/section_header.dart';
 import 'package:atlas_icons/atlas_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SpaceActionsSection extends StatelessWidget {
+class SpaceActionsSection extends ConsumerWidget {
   final String spaceId;
 
   const SpaceActionsSection({
@@ -12,7 +14,7 @@ class SpaceActionsSection extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Column(
       children: [
         SectionHeader(
@@ -20,84 +22,86 @@ class SpaceActionsSection extends StatelessWidget {
           isShowSeeAllButton: false,
           onTapSeeAll: () {},
         ),
-        Container(
-          padding: const EdgeInsets.all(14),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  spaceActionButton(
-                    context,
-                    Atlas.pin,
-                    L10n.of(context).addPin,
-                  ),
-                  const SizedBox(width: 12),
-                  spaceActionButton(
-                    context,
-                    Atlas.calendar_dots,
-                    L10n.of(context).addEvent,
-                  ),
-                  const SizedBox(width: 12),
-                  spaceActionButton(
-                    context,
-                    Atlas.list,
-                    L10n.of(context).addTask,
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  spaceActionButton(
-                    context,
-                    Atlas.chats,
-                    L10n.of(context).addChat,
-                  ),
-                  const SizedBox(width: 12),
-                  spaceActionButton(
-                    context,
-                    Icons.link,
-                    L10n.of(context).linkChat,
-                  ),
-                  const SizedBox(width: 12),
-                  spaceActionButton(
-                    context,
-                    Icons.people,
-                    L10n.of(context).addSpace,
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  spaceActionButton(
-                    context,
-                    Icons.link,
-                    L10n.of(context).linkSpace,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
+        actionButtons(context, ref),
         const SizedBox(height: 300),
       ],
     );
   }
 
-  Widget spaceActionButton(
-    BuildContext context,
-    IconData iconData,
-    String title,
-  ) {
-    return OutlinedButton.icon(
-      style: ButtonStyle(
-        padding: WidgetStateProperty.all(
-            const EdgeInsets.symmetric(horizontal: 8, vertical: 4)),
+  Widget actionButtons(BuildContext context, WidgetRef ref) {
+    final membership = ref.watch(roomMembershipProvider(spaceId));
+    bool canAddPin = membership.requireValue!.canString('CanPostPin');
+    bool canAddEvent = membership.requireValue!.canString('CanPostEvent');
+    bool canAddTask = membership.requireValue!.canString('CanPostTaskList');
+    bool canLinkSpaces = membership.requireValue!.canString('CanLinkSpaces');
+
+    return Container(
+      padding: const EdgeInsets.all(14),
+      child: Wrap(
+        crossAxisAlignment: WrapCrossAlignment.start,
+        children: [
+          spaceActionButton(
+            context: context,
+            iconData: Atlas.pin,
+            title: L10n.of(context).addPin,
+            isShow: canAddPin,
+          ),
+          spaceActionButton(
+            context: context,
+            iconData: Atlas.calendar_dots,
+            title: L10n.of(context).addEvent,
+            isShow: canAddEvent,
+          ),
+          spaceActionButton(
+            context: context,
+            iconData: Atlas.list,
+            title: L10n.of(context).addTask,
+            isShow: canAddTask,
+          ),
+          spaceActionButton(
+            context: context,
+            iconData: Atlas.chats,
+            title: L10n.of(context).addChat,
+            isShow: canLinkSpaces,
+          ),
+          spaceActionButton(
+            context: context,
+            iconData: Icons.people,
+            title: L10n.of(context).addSpace,
+            isShow: canLinkSpaces,
+          ),
+          spaceActionButton(
+            context: context,
+            iconData: Icons.link,
+            title: L10n.of(context).linkChat,
+            isShow: canLinkSpaces,
+          ),
+          spaceActionButton(
+            context: context,
+            iconData: Icons.link,
+            title: L10n.of(context).linkSpace,
+            isShow: canLinkSpaces,
+          ),
+        ],
       ),
-      onPressed: () {},
-      icon: Icon(iconData),
-      label: Text(
-        title,
-        style: Theme.of(context).textTheme.bodyMedium,
+    );
+  }
+
+  Widget spaceActionButton({
+    required BuildContext context,
+    required IconData iconData,
+    required String title,
+    bool isShow = true,
+  }) {
+    return Visibility(
+      visible: isShow,
+      child: TextButton.icon(
+        onPressed: () {},
+        icon: Icon(iconData),
+        label: Text(
+          title,
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
       ),
     );
   }
