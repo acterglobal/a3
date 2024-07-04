25a6f373186d9240c5125e619329b8cccfa213d5
kumarpalsinh25 <info@dajiraj.com>
2024-07-04 20:05:14 +0530

    Added space action buttons

--- app/lib/features/space/widgets/space_sections/space_actions_section.dart
+++ app/lib/features/space/widgets/space_sections/space_actions_section.dart
@@ -1,4 +1,7 @@
+import 'package:acter/features/space/widgets/space_sections/section_header.dart';
+import 'package:atlas_icons/atlas_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 class SpaceActionsSection extends StatelessWidget {
   final String spaceId;
@@ -10,9 +13,75 @@ class SpaceActionsSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 500,
-        color: Colors.black45,
-        child: const Placeholder());
+    return Column(
+      children: [
+        SectionHeader(
+          title: 'Actions',
+          isShowSeeAllButton: false,
+          onTapSeeAll: () {},
+        ),
+        Wrap(
+          children: [
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Atlas.pin),
+              label: Text(
+                L10n.of(context).addPin,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Atlas.calendar_dots),
+              label: Text(
+                L10n.of(context).addEvent,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Atlas.list),
+              label: Text(
+                L10n.of(context).addTask,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Atlas.chats),
+              label: Text(
+                L10n.of(context).addChat,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Atlas.users),
+              label: Text(
+                L10n.of(context).addSpace,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Atlas.link),
+              label: Text(
+                L10n.of(context).linkChat,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Atlas.link),
+              label: Text(
+                L10n.of(context).linkSpace,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 300),
+      ],
+    );
   }
 }
