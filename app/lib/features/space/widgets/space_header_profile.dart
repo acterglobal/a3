d5caa102b18bd6fabc792ae1a2775727c38d84b2
kumarpalsinh25 <info@dajiraj.com>
2024-07-04 13:27:32 +0530

    Remove join and member ui from space header

--- app/lib/features/space/widgets/space_header_profile.dart
+++ app/lib/features/space/widgets/space_header_profile.dart
@@ -1,9 +1,7 @@
 import 'package:acter/common/providers/room_providers.dart';
 import 'package:acter/common/providers/space_providers.dart';
-import 'package:acter/common/utils/routes.dart';
 import 'package:acter/common/widgets/edit_title_sheet.dart';
 import 'package:acter/common/widgets/spaces/space_info.dart';
-import 'package:acter/features/space/widgets/member_avatar.dart';
 import 'package:acter/router/utils.dart';
 import 'package:acter_avatar/acter_avatar.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +9,6 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:logging/logging.dart';
-import 'package:skeletonizer/skeletonizer.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 final _log = Logger('a3::space::space_header');
@@ -75,74 +72,9 @@ class SpaceHeaderProfile extends ConsumerWidget {
                 padding: const EdgeInsets.only(bottom: 10, left: 10),
                 child: SpaceInfo(spaceId: spaceId),
               ),
-              Consumer(builder: spaceMembersBuilder),
             ],
           ),
         ],
-      ),
-    );
-  }
-
-  Widget spaceMembersBuilder(
-    BuildContext context,
-    WidgetRef ref,
-    Widget? child,
-  ) {
-    final spaceMembers = ref.watch(membersIdsProvider(spaceId));
-    return spaceMembers.when(
-      data: (members) {
-        final membersCount = members.length;
-        if (membersCount > 5) {
-          // too many to display, means we limit to 5
-          members = members.sublist(0, 5);
-        }
-        return Padding(
-          padding: const EdgeInsets.only(left: 10),
-          child: GestureDetector(
-            onTap: () => context.goNamed(
-              Routes.spaceMembers.name,
-              pathParameters: {'spaceId': spaceId},
-            ),
-            child: Wrap(
-              direction: Axis.horizontal,
-              spacing: -12,
-              children: [
-                ...members.map(
-                  (a) => MemberAvatar(memberId: a, roomId: spaceId),
-                ),
-                if (membersCount > 5)
-                  CircleAvatar(
-                    child: Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Text(
-                        '+${membersCount - 5}',
-                        textAlign: TextAlign.center,
-                        textScaler: const TextScaler.linear(0.8),
-                        style: Theme.of(context).textTheme.labelMedium,
-                      ),
-                    ),
-                  ),
-              ],
-            ),
-          ),
-        );
-      },
-      error: (error, stack) => Text(
-        L10n.of(context).loadingMembersFailed(error),
-      ),
-      loading: () => const Skeletonizer(
-        child: Wrap(
-          direction: Axis.horizontal,
-          spacing: -12,
-          children: [
-            CircleAvatar(child: Text('0')),
-            CircleAvatar(child: Text('1')),
-            CircleAvatar(child: Text('2')),
-            CircleAvatar(child: Text('3')),
-            CircleAvatar(child: Text('4')),
-            CircleAvatar(child: Text('5')),
-          ],
-        ),
       ),
     );
   }
