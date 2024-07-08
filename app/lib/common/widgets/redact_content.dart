b3600be50c226c0908a38ea4489b8c7527d773c3
bitfriend <bitcapture@hotmail.com>
2024-07-08 18:56:11 +0800

    Remove unnecessary calling of ref.invalidate(spaceEventsProvider()) for non-space

--- app/lib/common/widgets/redact_content.dart
+++ app/lib/common/widgets/redact_content.dart
@@ -1,10 +1,8 @@
 import 'package:acter/common/providers/chat_providers.dart';
 import 'package:acter/common/providers/space_providers.dart';
-
 import 'package:acter/common/toolkit/buttons/primary_action_button.dart';
 import 'package:acter/common/widgets/default_dialog.dart';
 import 'package:acter/common/widgets/input_text_field.dart';
-import 'package:acter/features/events/providers/event_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
@@ -99,7 +97,6 @@ class RedactContentWidget extends ConsumerWidget {
       } else {
         final room = await ref.read(chatProvider(roomId).future);
         final redactedId = await room.redactContent(eventId, reason);
-        ref.invalidate(spaceEventsProvider(roomId));
         _log.info(
           'Content from user:{$senderId redacted $redactedId reason:$reason}',
         );
