2bf0d56351ddcb0162f4225d9618318a80f3e074
Benjamin Kampmann <ben@effektio.org>
2024-07-16 15:10:23 +0100

    Add an additional isNightly flag to differentiate releases

--- app/lib/common/utils/constants.dart
+++ app/lib/common/utils/constants.dart
@@ -51,9 +51,9 @@ const autoGuestLogin = bool.fromEnvironment(
   defaultValue: false,
 );
 
-const giphyKey = String.fromEnvironment(
-  'GIPHY_KEY',
-  defaultValue: 'C4dMA7Q19nqEGdpfj82T8ssbOeZIylD4',
+const isNightly = bool.fromEnvironment(
+  'IS_NIGHTLY',
+  defaultValue: false,
 );
 
 const defaultServersStr = String.fromEnvironment(
