478146d0736c84bd46da8db47174cd4c310b0e6b
Benjamin Kampmann <ben@effektio.org>
2024-07-26 17:25:55 +0100

    Remove unused _log

--- app/lib/features/space/widgets/related/chats_helpers.dart
+++ app/lib/features/space/widgets/related/chats_helpers.dart
@@ -7,10 +7,7 @@ import 'package:acter/router/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
-import 'package:logging/logging.dart';
 import 'package:skeletonizer/skeletonizer.dart';
-
-final _log = Logger('a3::space::widget::related::chat_helpers');
 
 Widget chatsListUI(WidgetRef ref, List<String> chats, int chatsLimit) {
   return ListView.builder(
