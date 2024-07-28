dfbaf01e6b608e74453433ed8ef0b63e3301a70f
Benjamin Kampmann <ben@effektio.org>
2024-07-28 17:00:46 +0100

    Support for closing rooms

---
+++ app/lib/common/widgets/room/room_profile_header.dart
@@ -0,0 +1,26 @@
+import 'package:acter/common/providers/room_providers.dart';
+import 'package:acter_avatar/acter_avatar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class RoomProfileHeader extends ConsumerWidget {
+  final String roomId;
+  const RoomProfileHeader({required this.roomId, super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final roomAvatarInfo = ref.watch(roomAvatarInfoProvider(roomId));
+    return Column(
+      children: [
+        ActerAvatar(
+          options: AvatarOptions(
+            roomAvatarInfo,
+            size: 50,
+          ),
+        ),
+        const SizedBox(height: 10),
+        Text(roomAvatarInfo.displayName ?? ''),
+      ],
+    );
+  }
+}
