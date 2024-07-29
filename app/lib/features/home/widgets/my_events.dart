787545cb84e321433527eabccb70574979d8231d
kumarpalsinh25 <info@dajiraj.com>
2024-07-29 19:14:18 +0530

    Dashboard : Only show events section if there is data for it

--- app/lib/features/home/widgets/my_events.dart
+++ app/lib/features/home/widgets/my_events.dart
@@ -1,7 +1,8 @@
 import 'package:acter/common/toolkit/buttons/inline_text_button.dart';
 import 'package:acter/common/utils/routes.dart';
 import 'package:acter/features/events/providers/event_providers.dart';
-import 'package:acter/features/events/widgets/events_list.dart';
+import 'package:acter/features/events/widgets/event_item.dart';
+import 'package:acter/features/events/widgets/skeletons/event_list_skeleton_widget.dart';
 import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -20,6 +21,7 @@ class MyEventsSection extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    //Get my events data
     AsyncValue<List<CalendarEvent>> myEvents;
     String sectionTitle = '';
     if (eventFilters == EventFilters.ongoing) {
@@ -30,27 +32,56 @@ class MyEventsSection extends ConsumerWidget {
       sectionTitle = L10n.of(context).myUpcomingEvents;
     }
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Row(
-          children: [
-            Text(
-              sectionTitle,
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
-            const Spacer(),
-            ActerInlineTextButton(
-              onPressed: () => context.pushNamed(Routes.calendarEvents.name),
-              child: Text(L10n.of(context).seeAll),
-            ),
-          ],
+    return myEvents.when(
+      data: (events) {
+        return events.isNotEmpty
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  sectionHeader(context, sectionTitle),
+                  eventListUI(context, events),
+                ],
+              )
+            : const SizedBox.shrink();
+      },
+      error: (error, stackTrace) => Text(
+        L10n.of(context).loadingEventsFailed(error),
+      ),
+      loading: () => const EventListSkeleton(),
+    );
+  }
+
+  Widget sectionHeader(BuildContext context, String sectionTitle) {
+    return Row(
+      children: [
+        Text(
+          sectionTitle,
+          style: Theme.of(context).textTheme.titleSmall,
         ),
-        EventsList(
-          limit: limit,
-          events: myEvents,
+        const Spacer(),
+        ActerInlineTextButton(
+          onPressed: () => context.pushNamed(Routes.calendarEvents.name),
+          child: Text(L10n.of(context).seeAll),
         ),
       ],
+    );
+  }
+
+  Widget eventListUI(
+    BuildContext context,
+    List<CalendarEvent> events,
+  ) {
+    int eventsLimit =
+        (limit != null && events.length > limit!) ? limit! : events.length;
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: eventsLimit,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, idx) => EventItem(
+        isShowSpaceName: true,
+        margin: const EdgeInsets.only(bottom: 14),
+        event: events[idx],
+      ),
     );
   }
 }
