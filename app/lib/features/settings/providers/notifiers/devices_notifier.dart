6a165ce352300bdfe7854f1bc66a7dea92559289
bitfriend <bitcapture@hotmail.com>
2024-07-31 14:33:31 +0800

    Add the missed log

--- app/lib/features/settings/providers/notifiers/devices_notifier.dart
+++ app/lib/features/settings/providers/notifiers/devices_notifier.dart
@@ -2,7 +2,10 @@ import 'dart:async';
 
 import 'package:acter/features/home/providers/client_providers.dart';
 import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
+import 'package:logging/logging.dart';
 import 'package:riverpod/riverpod.dart';
+
+final _log = Logger('a3::settings::devices');
 
 class AsyncDevicesNotifier extends AsyncNotifier<List<DeviceRecord>> {
   Stream<DeviceNewEvent>? _newListener;
@@ -22,8 +25,12 @@ class AsyncDevicesNotifier extends AsyncNotifier<List<DeviceRecord>> {
         final sessions = (await manager.allSessions()).toList();
         state = AsyncValue.data(sessions);
       },
-      onError: (e, stack) {},
-      onDone: () {},
+      onError: (e, stack) {
+        _log.severe('stream errored', e, stack);
+      },
+      onDone: () {
+        _log.info('stream ended');
+      },
     );
     ref.onDispose(() => _newPoller?.cancel());
 
@@ -33,8 +40,12 @@ class AsyncDevicesNotifier extends AsyncNotifier<List<DeviceRecord>> {
         final sessions = (await manager.allSessions()).toList();
         state = AsyncValue.data(sessions);
       },
-      onError: (e, stack) {},
-      onDone: () {},
+      onError: (e, stack) {
+        _log.severe('stream errored', e, stack);
+      },
+      onDone: () {
+        _log.info('stream ended');
+      },
     );
     ref.onDispose(() => _changedPoller?.cancel());
 
