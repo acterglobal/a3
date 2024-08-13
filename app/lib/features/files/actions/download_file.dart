42f9870aedbda93521a81c6cf0a23490f27e5ab8
Benjamin Kampmann <ben@effektio.org>
2024-08-13 17:55:34 +0100

    Implement new open-or-share bottom drawer

---
+++ app/lib/features/files/actions/download_file.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'package:path/path.dart';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
+Future<bool> downloadFile(BuildContext context, File file) async {
+  final lang = L10n.of(context);
+  final filename = basename(file.path);
+  String? outputFile = await FilePicker.platform.saveFile(
+    dialogTitle: lang.downloadFileDialogTitle,
+    fileName: filename,
+  );
+
+  if (outputFile == null) {
+    return false;
+  }
+
+  await file.copy(outputFile);
+  EasyLoading.showToast(lang.downloadFileSuccess(outputFile));
+  return true;
+}
