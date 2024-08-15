454f5657f9d951636a25767f6031f0838e7eed12
Benjamin Kampmann <ben@effektio.org>
2024-08-15 12:28:45 +0100

    test for showing actual input

--- app/test/features/chat/custom_input.dart
+++ app/test/features/chat/custom_input.dart
@@ -1,3 +1,4 @@
+import 'package:acter/features/chat/providers/chat_providers.dart';
 import 'package:acter/features/chat/widgets/custom_input.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -6,7 +7,7 @@ import '../../helpers/test_wrapper_widget.dart';
 
 void main() {
   group(
-    'Custom Chat Input General States',
+    'Custom Chat Input - General States',
     () {
       testWidgets(
         'No access, no show',
@@ -48,6 +49,30 @@ void main() {
           expect(find.byKey(CustomChatInput.loadingKey), findsOneWidget);
         },
       );
+
+      testWidgets(
+        'Show input',
+        (tester) async {
+          await tester.pumpWidget(
+            ProviderScope(
+              overrides: [
+                // Same as before
+                canSendProvider.overrideWith((ref, roomId) => true),
+                isRoomEncryptedProvider.overrideWith((ref, roomId) => true),
+              ],
+              child: const InActerContextTestWrapper(
+                child: CustomChatInput(
+                  roomId: 'roomId',
+                ),
+              ),
+            ),
+          );
+          expect(find.byKey(CustomChatInput.noAccessKey), findsNothing);
+          expect(find.byKey(CustomChatInput.loadingKey), findsNothing);
+        },
+      );
     },
   );
+
+  group('Custom Chat Input - Edit States', () {});
 }
--- app/test/helpers/test_wrapper_widget.dart
+++ app/test/helpers/test_wrapper_widget.dart
@@ -1,6 +1,7 @@
 import 'package:acter/common/themes/acter_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:acter_trigger_auto_complete/acter_trigger_autocomplete.dart';
 
 class InActerContextTestWrapper extends StatelessWidget {
   final Widget child;
@@ -9,14 +10,16 @@ class InActerContextTestWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ActerTheme.theme,
-      title: 'Acter',
-      builder: (context, other) => Scaffold(body: child),
-      locale: const Locale('en', 'US'),
-      localizationsDelegates: L10n.localizationsDelegates,
-      supportedLocales: L10n.supportedLocales,
-      // MaterialApp contains our top-level Navigator
+    return Portal(
+      child: MaterialApp(
+        theme: ActerTheme.theme,
+        title: 'Acter',
+        builder: (context, other) => Scaffold(body: child),
+        locale: const Locale('en', 'US'),
+        localizationsDelegates: L10n.localizationsDelegates,
+        supportedLocales: L10n.supportedLocales,
+        // MaterialApp contains our top-level Navigator
+      ),
     );
   }
 }
