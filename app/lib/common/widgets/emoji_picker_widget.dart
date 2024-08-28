6dfcd74bd9b046453cdab1de1df0fe738556129c
Benjamin Kampmann <ben@effektio.org>
2024-08-28 15:17:04 +0100

    Refactor emoji search widget for simplicity

--- app/lib/common/widgets/emoji_picker_widget.dart
+++ app/lib/common/widgets/emoji_picker_widget.dart
@@ -164,78 +164,83 @@ class _CustomSearchView extends SearchView {
 class _CustomSearchViewState extends SearchViewState<_CustomSearchView> {
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints) {
-      final emojiSize =
-          widget.config.emojiViewConfig.getEmojiSize(constraints.maxWidth);
-      final emojiBoxSize =
-          widget.config.emojiViewConfig.getEmojiBoxSize(constraints.maxWidth);
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final emojiSize =
+            widget.config.emojiViewConfig.getEmojiSize(constraints.maxWidth);
+        final emojiBoxSize =
+            widget.config.emojiViewConfig.getEmojiBoxSize(constraints.maxWidth);
 
-      return Container(
-        color: widget.config.searchViewConfig.backgroundColor,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Material(
-              color: Colors.transparent,
-              child: SizedBox(
-                height: emojiBoxSize + 8.0,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: ListView.builder(
-                        padding: const EdgeInsets.symmetric(vertical: 4.0),
-                        scrollDirection: Axis.horizontal,
-                        itemCount: results.length,
-                        itemBuilder: (context, index) {
-                          return buildEmoji(
-                            results[index],
-                            emojiSize,
-                            emojiBoxSize,
-                          );
-                        },
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        widget.closePicker();
-                      },
-                      color: widget.config.searchViewConfig.buttonIconColor,
-                      icon: Icon(PhosphorIcons.xCircle()),
-                    ),
-                  ],
+        return Container(
+          color: widget.config.searchViewConfig.backgroundColor,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Material(
+                color: Colors.transparent,
+                child: SizedBox(
+                  height: emojiBoxSize + 8.0,
+                  child: _renderResultRow(emojiSize, emojiBoxSize),
                 ),
               ),
-            ),
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    widget.showEmojiView();
-                  },
-                  color: widget.config.searchViewConfig.buttonIconColor,
-                  icon: const Icon(
-                    Icons.arrow_back,
-                  ),
-                ),
-                Expanded(
-                  child: TextField(
-                    onChanged: onTextInputChanged,
-                    focusNode: focusNode,
-                    style: widget.config.searchViewConfig.inputTextStyle,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: widget.config.searchViewConfig.hintText,
-                      hintStyle: widget.config.searchViewConfig.hintTextStyle,
-                      contentPadding:
-                          const EdgeInsets.symmetric(horizontal: 16),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      );
-    },);
+              _renderSearchBox(),
+            ],
+          ),
+        );
+      },
+    );
   }
+
+  Widget _renderResultRow(double emojiSize, double emojiBoxSize) => Row(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(vertical: 4.0),
+              scrollDirection: Axis.horizontal,
+              itemCount: results.length,
+              itemBuilder: (context, index) {
+                return buildEmoji(
+                  results[index],
+                  emojiSize,
+                  emojiBoxSize,
+                );
+              },
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              widget.closePicker();
+            },
+            color: widget.config.searchViewConfig.buttonIconColor,
+            icon: Icon(PhosphorIcons.xCircle()),
+          ),
+        ],
+      );
+
+  Widget _renderSearchBox() => Row(
+        children: [
+          IconButton(
+            onPressed: () {
+              widget.showEmojiView();
+            },
+            color: widget.config.searchViewConfig.buttonIconColor,
+            icon: const Icon(
+              Icons.arrow_back,
+            ),
+          ),
+          Expanded(
+            child: TextField(
+              onChanged: onTextInputChanged,
+              focusNode: focusNode,
+              style: widget.config.searchViewConfig.inputTextStyle,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: widget.config.searchViewConfig.hintText,
+                hintStyle: widget.config.searchViewConfig.hintTextStyle,
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+              ),
+            ),
+          ),
+        ],
+      );
 }
