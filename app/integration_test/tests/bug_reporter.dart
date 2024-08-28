de3a81126e4195535bb5eda6470f97514590c848
Benjamin Kampmann <ben@effektio.org>
2024-08-28 14:49:23 +0100

    Fix missing import

--- app/integration_test/tests/bug_reporter.dart
+++ app/integration_test/tests/bug_reporter.dart
@@ -1,3 +1,4 @@
+import 'package:acter/features/bug_report/actions/open_bug_report.dart';
 import 'package:acter/features/bug_report/pages/bug_report_page.dart';
 import 'package:acter/features/home/data/keys.dart';
 import 'package:acter/config/app_shell.dart';
