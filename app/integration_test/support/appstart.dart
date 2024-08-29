7ab14967e1ddd1cb52688fad376599dff7d349bb
Benjamin Kampmann <ben@effektio.org>
2024-08-29 23:36:24 +0100

    don't forget to setup the SDK in tests

--- app/integration_test/support/appstart.dart
+++ app/integration_test/support/appstart.dart
@@ -1,10 +1,17 @@
+import 'package:acter/config/setup.dart';
 import 'package:acter_flutter_sdk/acter_flutter_sdk.dart';
 import 'package:convenient_test/convenient_test.dart';
 import 'package:acter/main.dart' as app;
 import 'package:flutter_test/flutter_test.dart';
 
+bool hasSetup = false;
+
 Future<void> startFreshTestApp(String key) async {
   TestWidgetsFlutterBinding.ensureInitialized();
+  if (!hasSetup) {
+    hasSetup = true;
+    configSetup();
+  }
   await ActerSdk.resetSessionsAndClients(key);
   await app.startAppForTesting(
     ConvenientTestWrapperWidget(child: app.makeApp()),
