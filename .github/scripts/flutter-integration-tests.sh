bffc39f7d7f9677d8e1a48b37192498c4da4a66f
Benjamin Kampmann <ben@effektio.org>
2024-08-29 23:46:47 +0100

    Delay manager start

--- .github/scripts/flutter-integration-tests.sh
+++ .github/scripts/flutter-integration-tests.sh
@@ -11,6 +11,18 @@ flutter run integration_test/main_test.dart  \
     &
 subscript_pid=$!
 
+# in our experience it takes about four mins on CI to startup the app
+
+BAR='#######################'   # this is full bar, e.g. 20 chars
+
+for i in {1..24}; do
+    echo -ne "\r$i/24 ${BAR:0:$i}" # print $i chars of $BAR from 0 position
+    sleep 10                 # wait 10s between "frames"
+done
+echo "\n"
+
+echo "Starting manager"
+
 # actual manager runner
 dart run convenient_test_manager_dart --enable-report-saver "$@"
 exit_status=$?
