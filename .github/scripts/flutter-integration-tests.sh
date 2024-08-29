6b26cc92f8456680dd7f28a97575c9e3abd71b4e
Benjamin Kampmann <ben@effektio.org>
2024-08-29 18:36:20 +0100

    Switch to enven

--- .github/scripts/flutter-integration-tests.sh
+++ .github/scripts/flutter-integration-tests.sh
@@ -5,14 +5,9 @@ flutter run integration_test/main_test.dart  \
     --disable-service-auth-codes \
     --dart-define CONVENIENT_TEST_APP_CODE_DIR=lib \
     --dart-define CONVENIENT_TEST_MANAGER_HOST=10.0.2.2 \
-    --dart-define DEFAULT_HOMESERVER_URL=http://10.0.2.2:8118/ \
-    --dart-define DEFAULT_HOMESERVER_NAME=10.0.2.2 \
     --dart-define RAGESHAKE_URL=http://10.0.2.2:9110/api/submit \
     --dart-define MAILHOG_URL=http://10.0.2.2:8025 \
     --dart-define RAGESHAKE_LISTING_URL=http://10.0.2.2:9110/api/listing \
-    --dart-define DEFAULT_HOMESERVER_URL=http://10.0.2.2:8118 \
-    --dart-define DEFAULT_HOMESERVER_NAME=10.0.2.2 \
-    --dart-define CI=true \
     &
 subscript_pid=$!
 
