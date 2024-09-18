f5ba435cf7980b39f5d716887ed2cf2f9a07fa35
bitfriend <bitcapture@hotmail.com>
2024-09-18 22:29:06 +0800

    Fix bug in letAsync

--- app/lib/common/utils/utils.dart
+++ app/lib/common/utils/utils.dart
@@ -395,7 +395,7 @@ extension Let<T> on T? {
   }
 
   // it supports async callback too unlike `extension_nullable`
-  Future<R?> letAsync<R>(R? Function(T) op) async {
+  Future<R?> letAsync<R>(Future<R?> Function(T) op) async {
     final T? value = this;
     return value == null ? null : op(value);
   }
