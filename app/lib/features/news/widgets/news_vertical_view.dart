5cab890c9502dcb35c0ea0dc5fab18467b709005
kumarpalsinh25 <info@dajiraj.com>
2024-09-25 19:00:47 +0530

    Code formating

---
+++ app/lib/features/news/widgets/news_vertical_view.dart
@@ -0,0 +1,56 @@
+import 'package:acter/common/animations/like_animation.dart';
+import 'package:acter/features/news/providers/news_providers.dart';
+import 'package:acter/features/news/widgets/news_item/news_item.dart';
+import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class NewsVerticalView extends ConsumerStatefulWidget {
+  final List<NewsEntry> newsList;
+
+  const NewsVerticalView({super.key, required this.newsList});
+
+  @override
+  ConsumerState<NewsVerticalView> createState() => _NewsVerticalViewState();
+}
+
+class _NewsVerticalViewState extends ConsumerState<NewsVerticalView> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 0);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return buildPagerView();
+  }
+
+  Widget buildPagerView() {
+    return PageView.builder(
+      controller: _pageController,
+      itemCount: widget.newsList.length,
+      scrollDirection: Axis.vertical,
+      itemBuilder: (context, index) => InkWell(
+        onDoubleTap: () async {
+          LikeAnimation.run(index);
+          final news = widget.newsList[index];
+          final manager = await ref.read(newsReactionsProvider(news).future);
+          final status = manager.likedByMe();
+          if (!status) {
+            await manager.sendLike();
+          }
+        },
+        child: NewsItem(news: widget.newsList[index]),
+      ),
+    );
+  }
+}
--- app/lib/features/news/widgets/news_widget.dart
+++ app/lib/features/news/widgets/news_widget.dart
@@ -1,9 +1,8 @@
-import 'package:acter/common/animations/like_animation.dart';
 import 'package:acter/common/toolkit/buttons/primary_action_button.dart';
 import 'package:acter/common/utils/routes.dart';
 import 'package:acter/common/widgets/empty_state_widget.dart';
 import 'package:acter/features/news/providers/news_providers.dart';
-import 'package:acter/features/news/widgets/news_item/news_item.dart';
+import 'package:acter/features/news/widgets/news_vertical_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,78 +11,50 @@ import 'package:logging/logging.dart';
 
 final _log = Logger('a3::news::news_widget');
 
-class NewsWidget extends ConsumerStatefulWidget {
+class NewsWidget extends ConsumerWidget {
   const NewsWidget({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _NewsWidgetState();
-}
-
-class _NewsWidgetState extends ConsumerState<NewsWidget> {
-  late PageController _pageController;
-
-  @override
-  void initState() {
-    super.initState();
-    _pageController = PageController(initialPage: 0);
-  }
-
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final newsListLoader = ref.watch(newsListProvider(null));
     return newsListLoader.when(
       data: (newsList) {
-        if (newsList.isEmpty) {
-          return Center(
-            child: EmptyState(
-              title: L10n.of(context).youHaveNoUpdates,
-              subtitle: L10n.of(context).createPostsAndEngageWithinSpace,
-              image: 'assets/images/empty_updates.svg',
-              primaryButton: ActerPrimaryActionButton(
-                onPressed: () => context.pushNamed(Routes.actionAddUpdate.name),
-                child: Text(L10n.of(context).createNewUpdate),
-              ),
-            ),
-          );
-        }
-        return PageView.builder(
-          controller: _pageController,
-          itemCount: newsList.length,
-          scrollDirection: Axis.vertical,
-          itemBuilder: (context, index) => InkWell(
-            onDoubleTap: () async {
-              LikeAnimation.run(index);
-              final news = newsList[index];
-              final manager =
-                  await ref.read(newsReactionsProvider(news).future);
-              final status = manager.likedByMe();
-              if (!status) {
-                await manager.sendLike();
-              }
-            },
-            child: NewsItem(news: newsList[index]),
-          ),
-        );
+        if (newsList.isEmpty) return newsEmptyStateUI(context);
+        return NewsVerticalView(newsList: newsList);
       },
-      error: (e, s) {
-        _log.severe('Failed to load news list', e, s);
-        return Center(
-          child: Text(L10n.of(context).couldNotFetchNews),
-        );
-      },
-      loading: () => const Center(
-        child: SizedBox(
-          height: 50,
-          width: 50,
-          child: CircularProgressIndicator(),
+      error: (e, s) => newsErrorUI(context, e, s),
+      loading: () => newsLoadingUI(),
+    );
+  }
+
+  Widget newsEmptyStateUI(BuildContext context) {
+    return Center(
+      child: EmptyState(
+        title: L10n.of(context).youHaveNoUpdates,
+        subtitle: L10n.of(context).createPostsAndEngageWithinSpace,
+        image: 'assets/images/empty_updates.svg',
+        primaryButton: ActerPrimaryActionButton(
+          onPressed: () => context.pushNamed(Routes.actionAddUpdate.name),
+          child: Text(L10n.of(context).createNewUpdate),
         ),
       ),
+    );
+  }
+
+  Widget newsLoadingUI() {
+    return const Center(
+      child: SizedBox(
+        height: 50,
+        width: 50,
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  Widget newsErrorUI(BuildContext context, e, s) {
+    _log.severe('Failed to load news list', e, s);
+    return Center(
+      child: Text(L10n.of(context).couldNotFetchNews),
     );
   }
 }
