c7f064991857f63daf01d579de02ddf7a1e5a3a8
kumarpalsinh25 <info@dajiraj.com>
2024-09-27 12:26:31 +0530

    Slide stack indication in grid mode

--- app/lib/features/news/widgets/news_grid_view.dart
+++ app/lib/features/news/widgets/news_grid_view.dart
@@ -4,6 +4,7 @@ import 'package:acter/features/news/widgets/news_item_slide/news_slide_item.dart
 import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class NewsGridView extends StatelessWidget {
   final List<NewsEntry> newsList;
@@ -43,12 +44,32 @@ class NewsGridView extends StatelessWidget {
 
   Widget newsItemUI(NewsEntry newsEntry) {
     final List<NewsSlide> newsSlides = newsEntry.slides().toList();
+    final slideCount = newsSlides.length;
     final slide = newsSlides[0];
 
     return Container(
       height: 300,
       margin: const EdgeInsets.all(6),
-      child: NewsSlideItem(slide: slide, showRichContent: false),
+      child: Stack(
+        alignment: Alignment.bottomLeft,
+        children: [
+          NewsSlideItem(slide: slide, showRichContent: false),
+          if (slideCount > 1) slideStackCountView(slideCount),
+        ],
+      ),
+    );
+  }
+
+  Widget slideStackCountView(int slideCount) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Icon(PhosphorIcons.stack(), size: 32),
+          const SizedBox(width: 4),
+          Text(slideCount.toString()),
+        ],
+      ),
     );
   }
 }
