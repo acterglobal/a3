f27dae7f67565d55eee721e49e882202a765ce82
bitfriend <bitcapture@hotmail.com>
2024-09-30 17:23:59 +0800

    Implement thumbnail test for attachment

--- native/test/src/tests/thumbnail.rs
+++ native/test/src/tests/thumbnail.rs
@@ -1,4 +1,4 @@
-use acter::api::{MediaSource, RoomMessage, ThumbnailInfo};
+use acter::api::{ActerModel, MediaSource, RoomMessage, ThumbnailInfo};
 use anyhow::{bail, Context, Result};
 use core::time::Duration;
 use futures::{pin_mut, stream::StreamExt, FutureExt};
@@ -10,7 +10,7 @@ use tokio_retry::{
     Retry,
 };
 
-use crate::utils::{random_user_with_random_convo, random_user_with_random_space};
+use crate::utils::{random_user_with_random_convo, random_user_with_random_space, random_user_with_template};
 
 #[tokio::test]
 async fn room_msg_can_support_image_thumbnail() -> Result<()> {
@@ -397,6 +397,216 @@ async fn news_can_support_video_thumbnail() -> Result<()> {
     assert_eq!(video_slide.type_str(), "video");
 
     let msg_content = video_slide.msg_content();
+    assert!(
+        msg_content.thumbnail_source().is_some(),
+        "we sent thumbnail, but thumbnail source not available",
+    );
+    let thumbnail_info = msg_content
+        .thumbnail_info()
+        .context("we sent thumbnail, but thumbnail info not available")?;
+    assert_eq!(
+        thumbnail_info.mimetype(),
+        Some("image/png".to_owned()),
+        "we sent thumbnail in png format",
+    );
+    assert_eq!(
+        thumbnail_info.size(),
+        Some(size),
+        "wrong file size in thumbnail",
+    );
+
+    Ok(())
+}
+
+const TMPL: &str = r#"
+version = "0.1"
+name = "Smoketest Template"
+
+[inputs]
+main = { type = "user", is-default = true, required = true, description = "The starting user" }
+
+[objects]
+main_space = { type = "space", is-default = true, name = "{{ main.display_name }}’s pins test space"}
+
+[objects.acter-website-pin]
+type = "pin"
+title = "Acter Website"
+url = "https://acter.global"
+
+[objects.acter-source-pin]
+type = "pin"
+title = "Acter Source Code"
+url = "https://github.com/acterglobal/a3"
+
+[objects.example-data-pin]
+type = "pin"
+title = "Acter example pin"
+content = { body = "example pin data" }
+"#;
+
+#[tokio::test]
+async fn image_attachment_can_support_thumbnail() -> Result<()> {
+    let _ = env_logger::try_init();
+    let (user, _sync_state, _engine) = random_user_with_template("image_attachment_thumbnail", TMPL).await?;
+
+    let retry_strategy = FibonacciBackoff::from_millis(100).map(jitter).take(10);
+    let fetcher_client = user.clone();
+    Retry::spawn(retry_strategy, move || {
+        let client = fetcher_client.clone();
+        async move {
+            if client.pins().await?.len() != 3 {
+                bail!("not all pins found");
+            }
+            Ok(())
+        }
+    })
+    .await?;
+
+    let pin = user
+        .pins()
+        .await?
+        .into_iter()
+        .find(|p| !p.is_link())
+        .expect("we’ve created one non-link pin");
+
+    // START actual attachment on pin
+
+    let attachments_manager = pin.attachments().await?;
+    assert!(!attachments_manager.stats().has_attachments());
+
+    // ---- let’s make a attachment
+
+    let bytes = include_bytes!("./fixtures/kingfisher.jpg");
+    let mut jpg_file = NamedTempFile::new()?;
+    jpg_file.as_file_mut().write_all(bytes)?;
+
+    let bytes = include_bytes!("./fixtures/PNG_transparency_demonstration_1.png");
+    let size = bytes.len() as u64;
+    let mut png_file = NamedTempFile::new()?;
+    png_file.as_file_mut().write_all(bytes)?;
+
+    let attachments_listener = attachments_manager.subscribe();
+    let base_draft = user
+        .image_draft(
+            jpg_file.path().to_string_lossy().to_string(),
+            "image/jpeg".to_string(),
+        )
+        .thumbnail_source(png_file.path().to_string_lossy().to_string())
+        .thumbnail_info(None, None, Some("image/png".to_owned()), Some(size));
+    let attachment_id = attachments_manager
+        .content_draft(Box::new(base_draft))
+        .await?
+        .send()
+        .await?;
+
+    let retry_strategy = FibonacciBackoff::from_millis(500).map(jitter).take(10);
+    Retry::spawn(retry_strategy.clone(), || async {
+        if attachments_listener.is_empty() {
+            bail!("all still empty");
+        }
+        Ok(())
+    })
+    .await?;
+
+    let attachments = attachments_manager.attachments().await?;
+    assert_eq!(attachments.len(), 1);
+    let attachment = attachments.first().unwrap();
+    assert_eq!(attachment.event_id(), attachment_id);
+    assert_eq!(attachment.type_str(), "image");
+
+    let msg_content = attachment.msg_content();
+    assert!(
+        msg_content.thumbnail_source().is_some(),
+        "we sent thumbnail, but thumbnail source not available",
+    );
+    let thumbnail_info = msg_content
+        .thumbnail_info()
+        .context("we sent thumbnail, but thumbnail info not available")?;
+    assert_eq!(
+        thumbnail_info.mimetype(),
+        Some("image/png".to_owned()),
+        "we sent thumbnail in png format",
+    );
+    assert_eq!(
+        thumbnail_info.size(),
+        Some(size),
+        "wrong file size in thumbnail",
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn video_attachment_can_support_thumbnail() -> Result<()> {
+    let _ = env_logger::try_init();
+    let (user, _sync_state, _engine) = random_user_with_template("image_attachment_thumbnail", TMPL).await?;
+
+    let retry_strategy = FibonacciBackoff::from_millis(100).map(jitter).take(10);
+    let fetcher_client = user.clone();
+    Retry::spawn(retry_strategy, move || {
+        let client = fetcher_client.clone();
+        async move {
+            if client.pins().await?.len() != 3 {
+                bail!("not all pins found");
+            }
+            Ok(())
+        }
+    })
+    .await?;
+
+    let pin = user
+        .pins()
+        .await?
+        .into_iter()
+        .find(|p| !p.is_link())
+        .expect("we’ve created one non-link pin");
+
+    // START actual attachment on pin
+
+    let attachments_manager = pin.attachments().await?;
+    assert!(!attachments_manager.stats().has_attachments());
+
+    // ---- let’s make a attachment
+
+    let bytes = include_bytes!("./fixtures/big_buck_bunny.mp4");
+    let mut mp4_file = NamedTempFile::new()?;
+    mp4_file.as_file_mut().write_all(bytes)?;
+
+    let bytes = include_bytes!("./fixtures/PNG_transparency_demonstration_1.png");
+    let size = bytes.len() as u64;
+    let mut png_file = NamedTempFile::new()?;
+    png_file.as_file_mut().write_all(bytes)?;
+
+    let attachments_listener = attachments_manager.subscribe();
+    let base_draft = user
+        .video_draft(
+            mp4_file.path().to_string_lossy().to_string(),
+            "video/mp4".to_string(),
+        )
+        .thumbnail_source(png_file.path().to_string_lossy().to_string())
+        .thumbnail_info(None, None, Some("image/png".to_owned()), Some(size));
+    let attachment_id = attachments_manager
+        .content_draft(Box::new(base_draft))
+        .await?
+        .send()
+        .await?;
+
+    let retry_strategy = FibonacciBackoff::from_millis(500).map(jitter).take(10);
+    Retry::spawn(retry_strategy.clone(), || async {
+        if attachments_listener.is_empty() {
+            bail!("all still empty");
+        }
+        Ok(())
+    })
+    .await?;
+
+    let attachments = attachments_manager.attachments().await?;
+    assert_eq!(attachments.len(), 1);
+    let attachment = attachments.first().unwrap();
+    assert_eq!(attachment.event_id(), attachment_id);
+    assert_eq!(attachment.type_str(), "video");
+
+    let msg_content = attachment.msg_content();
     assert!(
         msg_content.thumbnail_source().is_some(),
         "we sent thumbnail, but thumbnail source not available",
