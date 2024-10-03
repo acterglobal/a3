59edf93457f72e491fe555f32b08fb1f344ef9b5
kumarpalsinh25 <info@dajiraj.com>
2024-10-03 14:02:54 +0530

    Lint error remove

--- app/lib/features/home/widgets/in_dashboard.dart
+++ app/lib/features/home/widgets/in_dashboard.dart
@@ -1,11 +1,6 @@
-import 'package:acter/common/providers/space_providers.dart';
-import 'package:acter/common/utils/routes.dart';
-import 'package:acter/features/news/model/keys.dart';
 import 'package:acter/features/news/pages/news_list_page.dart';
-import 'package:atlas_icons/atlas_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 class InDashboard extends ConsumerWidget {
   final Widget child;
@@ -14,12 +9,6 @@ class InDashboard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final canPostNews = ref
-            .watch(
-              hasSpaceWithPermissionProvider('CanPostNews'),
-            )
-            .valueOrNull ==
-        true;
     return LayoutBuilder(
       builder: (context, constrains) {
         if (constrains.maxWidth > 770) {
