d3a0e3cd5e9a2683c58c65f537feabc22b32a01d
kumarpalsinh25 <info@dajiraj.com>
2024-10-28 20:10:06 +0530

    Manage comment input box

--- app/lib/features/comments/widgets/add_comment_widget.dart
+++ app/lib/features/comments/widgets/add_comment_widget.dart
@@ -3,11 +3,13 @@ import 'package:acter/common/widgets/edit_html_description_sheet.dart';
 import 'package:acter/features/comments/actions/sbumit_comment.dart';
 import 'package:acter_avatar/acter_avatar.dart';
 import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
+import 'package:atlas_icons/atlas_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
-class AddCommentWidget extends ConsumerWidget {
+class AddCommentWidget extends ConsumerStatefulWidget {
   final CommentsManager manager;
 
   const AddCommentWidget({
@@ -16,50 +18,91 @@ class AddCommentWidget extends ConsumerWidget {
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<AddCommentWidget> createState() => _AddCommentWidgetState();
+}
+
+class _AddCommentWidgetState extends ConsumerState<AddCommentWidget> {
+  final ValueNotifier<bool> showSendButton = ValueNotifier(false);
+  final TextEditingController _commentController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
     final avatarInfo = ref.watch(accountAvatarInfoProvider);
-    return InkWell(
-      onTap: () {
-        showEditHtmlDescriptionBottomSheet(
-          context: context,
-          bottomSheetTitle: L10n.of(context).addComment,
-          onSave: (htmlBodyDescription, plainDescription) async {
-            await submitComment(
-              context,
-              plainDescription,
-              htmlBodyDescription,
-              manager,
-            );
-            if (!context.mounted) return;
-            Navigator.pop(context);
-          },
-        );
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Row(
-          children: [
-            ActerAvatar(options: AvatarOptions.DM(avatarInfo, size: 18)),
-            const SizedBox(width: 12),
-            Expanded(child: addCommentBoxUI(context)),
-          ],
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ActerAvatar(options: AvatarOptions.DM(avatarInfo, size: 18)),
+          const SizedBox(width: 12),
+          Expanded(child: addCommentBoxUI(context)),
+          sendButton(),
+        ],
       ),
     );
   }
 
   Widget addCommentBoxUI(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(14),
-      decoration: BoxDecoration(
-        color: Theme.of(context).unselectedWidgetColor.withAlpha(30),
-        border: Border.all(color: Theme.of(context).unselectedWidgetColor),
-        borderRadius: const BorderRadius.all(Radius.circular(24)),
-      ),
-      child: Text(
-        L10n.of(context).addComment,
-        style: Theme.of(context).textTheme.labelMedium,
+    final lang = L10n.of(context);
+    return TextField(
+      keyboardType: TextInputType.text,
+      textInputAction: TextInputAction.next,
+      controller: _commentController,
+      onChanged: (value) => showSendButton.value = value.isNotEmpty,
+      decoration: InputDecoration(
+        hintText: lang.addComment,
+        suffixIcon: IconButton(
+          onPressed: () => showEditHtmlDescriptionBottomSheet(
+              context: context,
+              bottomSheetTitle: L10n.of(context).addComment,
+              descriptionHtmlValue: _commentController.text,
+              onSave: (htmlBodyDescription, plainDescription) async {
+                await addComment(
+                  plainDescription: plainDescription,
+                  htmlBodyDescription: htmlBodyDescription,
+                );
+                if (!context.mounted) return;
+                Navigator.pop(context);
+              }),
+          icon: const Icon(Atlas.arrows_up_right_down_left, size: 14),
+        ),
       ),
     );
+  }
+
+  Widget sendButton() {
+    return ValueListenableBuilder(
+      valueListenable: showSendButton,
+      builder: (context, value, child) {
+        return value
+            ? Container(
+                margin: const EdgeInsets.only(left: 12),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: const BorderRadius.all(Radius.circular(100)),
+                ),
+                child: IconButton(
+                  onPressed: () =>
+                      addComment(plainDescription: _commentController.text),
+                  icon: Icon(PhosphorIcons.paperPlaneTilt()),
+                ),
+              )
+            : const SizedBox.shrink();
+      },
+    );
+  }
+
+  Future<void> addComment({
+    required String plainDescription,
+    String? htmlBodyDescription,
+  }) async {
+    await submitComment(
+      context,
+      plainDescription,
+      htmlBodyDescription ?? plainDescription,
+      widget.manager,
+    );
+    _commentController.clear();
+    showSendButton.value = false;
   }
 }
