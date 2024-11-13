22e4216a151950edab73ba705356540400cd1c09
kumarpalsinh25 <info@dajiraj.com>
2024-11-13 14:37:45 +0530

    Lint error fixes

--- app/lib/features/home/widgets/my_events.dart
+++ app/lib/features/home/widgets/my_events.dart
@@ -1,7 +1,6 @@
 import 'package:acter/common/utils/routes.dart';
 import 'package:acter/features/events/providers/event_providers.dart';
 import 'package:acter/features/events/widgets/event_list_widget.dart';
-import 'package:acter/features/space/widgets/space_sections/section_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
