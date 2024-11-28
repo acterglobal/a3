d35c093755f2a5dc1d41f0f16f96a0a36cf2adc9
kumarpalsinh25 <info@dajiraj.com>
2024-11-28 12:36:43 +0530

    Improvement in AttachedTo Widget Code

--- app/lib/common/widgets/Share/options/attach_options.dart
+++ app/lib/common/widgets/Share/options/attach_options.dart
@@ -1,6 +1,7 @@
 import 'package:acter/common/themes/colors/color_scheme.dart';
 import 'package:atlas_icons/atlas_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 class AttachOptions extends StatelessWidget {
   final String data;
@@ -9,39 +10,83 @@ class AttachOptions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final lang = L10n.of(context);
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Text(
-          'Attache To',
+          lang.attachedTo,
           textAlign: TextAlign.start,
           style: Theme.of(context).textTheme.labelLarge,
         ),
         SizedBox(height: 12),
         Wrap(
           children: [
-            iconItem('Boost', Atlas.megaphone_thin, boastFeatureColor),
-            iconItem('Pin', Atlas.pin, pinFeatureColor),
-            iconItem('Event', Atlas.calendar, eventFeatureColor),
-            iconItem('TaskList', Atlas.list, taskFeatureColor),
-            iconItem('Task', Atlas.list, taskFeatureColor),
-            iconItem('Space', Atlas.group_people_arrow_up, Colors.grey),
-            iconItem('Chat', Atlas.chats, Colors.brown),
+            attachToItemUI(
+              name: lang.boost,
+              iconData: Atlas.megaphone_thin,
+              color: boastFeatureColor,
+              onTap: () {},
+            ),
+            attachToItemUI(
+              name: lang.pin,
+              iconData: Atlas.pin,
+              color: pinFeatureColor,
+              onTap: () {},
+            ),
+            attachToItemUI(
+              name: lang.event,
+              iconData: Atlas.calendar,
+              color: eventFeatureColor,
+              onTap: () {},
+            ),
+            attachToItemUI(
+              name: lang.taskList,
+              iconData: Atlas.list,
+              color: taskFeatureColor,
+              onTap: () {},
+            ),
+            attachToItemUI(
+              name: lang.tasks,
+              iconData: Atlas.list,
+              color: taskFeatureColor,
+              onTap: () {},
+            ),
+            attachToItemUI(
+              name: lang.space,
+              iconData: Atlas.group_people_arrow_up,
+              color: Colors.grey,
+              onTap: () {},
+            ),
+            attachToItemUI(
+              name: lang.chat,
+              iconData: Atlas.chats,
+              color: Colors.brown,
+              onTap: () {},
+            ),
           ],
         ),
       ],
     );
   }
 
-  Widget iconItem(String name, IconData iconData, Color color) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
+  Widget attachToItemUI({
+    required String name,
+    required IconData iconData,
+    required Color color,
+    GestureTapCallback? onTap,
+  }) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(16),
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
               padding: const EdgeInsets.all(14),
               decoration: BoxDecoration(
                 color: color.withOpacity(0.3),
@@ -52,10 +97,12 @@ class AttachOptions extends StatelessWidget {
                   width: 1.0,
                 ),
               ),
-              child: Icon(iconData)),
-          SizedBox(height: 6),
-          Text(name),
-        ],
+              child: Icon(iconData),
+            ),
+            SizedBox(height: 6),
+            Text(name),
+          ],
+        ),
       ),
     );
   }
