c8d8e9496bfc004bca7106a5dfedfc1ee42acfae
bitfriend <bitcapture@hotmail.com>
2024-11-29 21:59:35 +0800

    Detect debug mode in platform code of windows

--- app/windows/runner/main.cpp
+++ app/windows/runner/main.cpp
@@ -8,6 +8,11 @@
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
+#ifdef _DEBUG
+  println("This app works in debug mode");
+#else
+  println("This app works in release mode");
+#endif
 
   HWND hwnd = ::FindWindow(L"FLUTTER_RUNNER_WIN32_WINDOW", L"Acter");
   if (hwnd != NULL) {
