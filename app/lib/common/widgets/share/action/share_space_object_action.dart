0ff78567d65d207980e9728350cdbb870a201109
kumarpalsinh25 <info@dajiraj.com>
2024-11-29 19:56:04 +0530

    Share space object directly to Create Boost

--- app/lib/common/widgets/share/action/share_space_object_action.dart
+++ app/lib/common/widgets/share/action/share_space_object_action.dart
@@ -1,11 +1,14 @@
+import 'package:acter/common/utils/routes.dart';
 import 'package:acter/common/widgets/share/widgets/attach_options.dart';
 import 'package:acter/common/widgets/share/widgets/external_share_options.dart';
 import 'package:acter/features/deep_linking/actions/show_qr_code.dart';
 import 'package:acter/features/deep_linking/types.dart';
+import 'package:acter/features/news/model/news_references_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:go_router/go_router.dart';
 import 'package:share_plus/share_plus.dart';
 
 Future<void> openShareSpaceObjectDialog({
@@ -49,7 +52,7 @@ class ShareSpaceObjectActionUI extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            attachmentOptionsUI(),
+            attachmentOptionsUI(context),
             SizedBox(height: 16),
             externalShareOptionsUI(context)
           ],
@@ -58,9 +61,22 @@ class ShareSpaceObjectActionUI extends StatelessWidget {
     );
   }
 
-  Widget attachmentOptionsUI() {
+  Widget attachmentOptionsUI(BuildContext context) {
+    final newsRefType = getNewsRefTypeFromObjType(objectType);
     return AttachOptions(
-      onTapBoost: () {},
+      onTapBoost: () {
+        Navigator.pop(context);
+        context.pushNamed(
+          Routes.actionAddUpdate.name,
+          queryParameters: {'spaceId': spaceId},
+          extra: newsRefType != null
+              ? NewsReferencesModel(
+                  type: newsRefType,
+                  id: objectId,
+                )
+              : null,
+        );
+      },
     );
   }
 
@@ -85,5 +101,14 @@ class ShareSpaceObjectActionUI extends StatelessWidget {
         await Share.share(link);
       },
     );
+  }
+
+  NewsReferencesType? getNewsRefTypeFromObjType(ObjectType objectType) {
+    return switch (objectType) {
+      ObjectType.pin => NewsReferencesType.pin,
+      ObjectType.calendarEvent => NewsReferencesType.calendarEvent,
+      ObjectType.taskList => NewsReferencesType.taskList,
+      _ => null,
+    };
   }
 }
