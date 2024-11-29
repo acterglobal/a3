41f8be078159ddc916765ef2f62dde39d446c12a
bitfriend <bitcapture@hotmail.com>
2024-11-29 22:00:32 +0800

    Detect debug mode in platform code of linux

--- app/linux/main.cc
+++ app/linux/main.cc
@@ -1,6 +1,12 @@
 #include "my_application.h"
 
 int main(int argc, char** argv) {
+#ifndef NDEBUG
+  printf("This app works in debug mode\n");
+#else
+  printf("This app works in release mode\n");
+#endif
+
   g_autoptr(MyApplication) app = my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
 }
