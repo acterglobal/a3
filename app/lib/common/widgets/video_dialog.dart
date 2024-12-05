c3eb9f1ef910a2b690bd52ae79696f7459d488e7
kumarpalsinh25 <info@dajiraj.com>
2024-12-05 15:55:21 +0530

    Fixes appbar safe area issue

--- app/lib/common/widgets/video_dialog.dart
+++ app/lib/common/widgets/video_dialog.dart
@@ -17,33 +17,23 @@ class VideoDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return Dialog(
       insetPadding: EdgeInsets.zero,
-      child: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Text(
-                    title,
-                    style: Theme.of(context).textTheme.titleSmall,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                ShareFileButton(file: videoFile),
-                IconButton(
-                  onPressed: () => Navigator.pop(context),
-                  icon: const Icon(Icons.close),
-                ),
-              ],
-            ),
-            const SizedBox(height: 15),
-            Expanded(child: ActerVideoPlayer(videoFile: videoFile)),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            title,
+            style: Theme.of(context).textTheme.titleSmall,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+          actions: [
+            ShareFileButton(file: videoFile)
           ],
+        ),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.all(8.0),
+          child: ActerVideoPlayer(videoFile: videoFile),
         ),
       ),
     );
