a4a72c8f94bd4651b965cd2a60f09e757b419d3b
Benjamin Kampmann <ben@acter.global>
2024-12-11 17:18:21 +0000

    Seperate into more consumers to avoid flickers

--- app/lib/features/space/widgets/space_info.dart
+++ app/lib/features/space/widgets/space_info.dart
@@ -5,10 +5,6 @@ import 'package:atlas_icons/atlas_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:logging/logging.dart';
-import 'package:skeletonizer/skeletonizer.dart';
-
-final _log = Logger('a3::space::space_info');
 
 final isActerSpaceForSpace =
     FutureProvider.autoDispose.family<bool, Space>((ref, space) async {
@@ -27,57 +23,32 @@ class SpaceInfo extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final spaceLoader = ref.watch(spaceProvider(spaceId));
-    return spaceLoader.when(
-      data: (space) => Wrap(
-        children: [
-          VisibilityChip(roomId: spaceId),
-          const SizedBox(width: 5),
-          acterSpaceInfoUI(context, ref, space),
-        ],
-      ),
-      error: (e, s) {
-        _log.severe('Failed to load space', e, s);
-        return Text(L10n.of(context).loadingFailed(e));
-      },
-      loading: () => skeletonUI(),
+    return Wrap(
+      children: [
+        VisibilityChip(roomId: spaceId),
+        const SizedBox(width: 5),
+        Consumer(builder: acterSpaceInfoUI),
+      ],
     );
   }
 
-  Widget skeletonUI() {
-    return Skeletonizer(
-      child: Row(
-        children: [
-          Icon(
-            Atlas.glasses_vision_thin,
-            size: size,
+  Widget acterSpaceInfoUI(BuildContext context, WidgetRef ref, Widget? child) {
+    final space = ref.watch(spaceProvider(spaceId)).valueOrNull;
+    if (space != null) {
+      final isActerSpace = ref.watch(isActerSpaceForSpace(space)).valueOrNull;
+      if (isActerSpace == false) {
+        return Padding(
+          padding: const EdgeInsets.only(right: 3),
+          child: Tooltip(
+            message: L10n.of(context).thisIsNotAProperActerSpace,
+            child: Icon(
+              Atlas.triangle_exclamation_thin,
+              size: size,
+              color: Theme.of(context).colorScheme.error,
+            ),
           ),
-          Icon(
-            Atlas.lock_clipboard_thin,
-            size: size,
-          ),
-          Icon(
-            Atlas.shield_exclamation_thin,
-            size: size,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget acterSpaceInfoUI(BuildContext context, WidgetRef ref, Space space) {
-    if (ref.watch(isActerSpaceForSpace(space)).valueOrNull != true) {
-      return Padding(
-        padding: const EdgeInsets.only(right: 3),
-        child: Tooltip(
-          message: L10n.of(context).thisIsNotAProperActerSpace,
-          child: Icon(
-            Atlas.triangle_exclamation_thin,
-            size: size,
-            color: Theme.of(context).colorScheme.error,
-          ),
-        ),
-      );
+        );
+      }
     }
     return const SizedBox.shrink();
   }
