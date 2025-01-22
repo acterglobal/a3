2c2790b49123c85c985fea6a3eca9e095157ba21
kumarpalsinh25 <info@dajiraj.com>
2025-01-22 20:35:03 +0530

    Some improvements in Test code

--- packages/acter_notifify/test/comment_notification_test.dart
+++ packages/acter_notifify/test/comment_notification_test.dart
@@ -7,50 +7,54 @@ import 'mock_data/mock_notification_parent.dart';
 import 'mock_data/mock_notification_sender.dart';
 
 void main() {
+  late MockNotificationItem item;
+  late MockNotificationParent parent;
+  late MockMsgContent msg;
+
+  setUpAll(() {
+    //Mack declaration
+    item = MockNotificationItem();
+    parent = MockNotificationParent();
+
+    //Set pushStyle
+    when(() => item.pushStyle()).thenReturn("comment");
+
+    //Set parent
+    when(() => item.parent()).thenReturn(parent);
+
+    //Set send user name
+    final sender = MockNotificationSender(name: "Washington Johnson");
+    when(() => item.sender()).thenReturn(sender);
+
+    //Set message content
+    msg = MockMsgContent(content: "This is great");
+    when(() => item.body()).thenReturn(msg);
+  });
+
   group("Title and body generation", () {
     test("Comment on boost", () {
-      final item = MockNotificationItem();
-      final parent = MockNotificationParent();
-      final sender = MockNotificationSender(name: "Washington Simp");
-      final msg = MockMsgContent(content: "This is great");
-      when(() => item.pushStyle()).thenReturn("comment");
-      when(() => item.parent()).thenReturn(parent);
-      when(() => item.sender()).thenReturn(sender);
-      when(() => item.body()).thenReturn(msg);
       when(() => parent.objectTypeStr()).thenReturn("news");
       when(() => parent.emoji()).thenReturn("🚀");
       final (title, body) = genTitleAndBody(item);
       expect(title, "💬 Comment on 🚀 boost");
-      expect(body, "Washington Simp: This is great");
+      expect(body, "Washington Johnson: This is great");
     });
     test("Comment on Pin", () {
-      final item = MockNotificationItem();
-      final parent = MockNotificationParent();
-      final sender = MockNotificationSender(name: "User name");
-      final msg = MockMsgContent(content: "Where can I find it?");
-      when(() => item.pushStyle()).thenReturn("comment");
-      when(() => item.parent()).thenReturn(parent);
-      when(() => item.sender()).thenReturn(sender);
-      when(() => item.body()).thenReturn(msg);
       when(() => parent.objectTypeStr()).thenReturn("pin");
       when(() => parent.emoji()).thenReturn("📌");
       when(() => parent.title()).thenReturn("The house");
       final (title, body) = genTitleAndBody(item);
       expect(title, "💬 Comment on 📌 The house");
-      expect(body, "User name: Where can I find it?");
+      expect(body, "Washington Johnson: This is great");
     });
     test("Comment with no parent", () {
-      final item = MockNotificationItem();
-      final msg = MockMsgContent(content: "This looks good");
       final sender = MockNotificationSender(
           username: "@id:acter.global"); // no display name
-      when(() => item.body()).thenReturn(msg);
       when(() => item.sender()).thenReturn(sender);
-      when(() => item.pushStyle()).thenReturn("comment");
       when(() => item.parent()).thenReturn(null);
       final (title, body) = genTitleAndBody(item);
       expect(title, "💬 Comment");
-      expect(body, "@id:acter.global: This looks good");
+      expect(body, "@id:acter.global: This is great");
     });
   });
 }
--- packages/acter_notifify/test/reaction_notification_test.dart
+++ packages/acter_notifify/test/reaction_notification_test.dart
@@ -7,44 +7,53 @@ import 'mock_data/mock_notification_parent.dart';
 import 'mock_data/mock_notification_sender.dart';
 
 void main() {
+  late MockNotificationItem item;
+  late MockNotificationParent parent;
+  late MockMsgContent msg;
+
+  setUpAll(() {
+    //Mack declaration
+    item = MockNotificationItem();
+    parent = MockNotificationParent();
+
+    //Set pushStyle
+    when(() => item.pushStyle()).thenReturn("reaction");
+
+    //Set parent
+    when(() => item.parent()).thenReturn(parent);
+
+    //Set send user name
+    final sender = MockNotificationSender(name: "Washington Johnson");
+    when(() => item.sender()).thenReturn(sender);
+
+    //Set message content
+    msg = MockMsgContent(content: "This is great");
+    when(() => item.body()).thenReturn(msg);
+  });
+
+
   group("Title and Body generation", () {
     test("Reaction on boost", () {
-      final item = MockNotificationItem();
-      final parent = MockNotificationParent();
-      final sender = MockNotificationSender(name: "Michael Joker");
-      when(() => item.pushStyle()).thenReturn("reaction");
-      when(() => item.parent()).thenReturn(parent);
-      when(() => item.sender()).thenReturn(sender);
       when(() => item.reactionKey()).thenReturn("❤️");
       when(() => parent.objectTypeStr()).thenReturn("news");
       when(() => parent.emoji()).thenReturn("🚀");
       final (title, body) = genTitleAndBody(item);
       expect(title, '"❤️" to 🚀 boost');
-      expect(body, "Michael Joker");
+      expect(body, "Washington Johnson");
     });
     test("Reaction on Pin", () {
-      final item = MockNotificationItem();
-      final parent = MockNotificationParent();
-      final sender = MockNotificationSender(name: "User name");
-      when(() => item.pushStyle()).thenReturn("reaction");
-      when(() => item.parent()).thenReturn(parent);
-      when(() => item.sender()).thenReturn(sender);
       when(() => parent.objectTypeStr()).thenReturn("pin");
       when(() => parent.emoji()).thenReturn("📌");
       when(() => item.reactionKey()).thenReturn("❤️");
       when(() => parent.title()).thenReturn("Candlesticks");
       final (title, body) = genTitleAndBody(item);
       expect(title, '"❤️" to 📌 Candlesticks');
-      expect(body, "User name");
+      expect(body, "Washington Johnson");
     });
     test("Reaction with parent", () {
-      final item = MockNotificationItem();
-      final msg = MockMsgContent(content: "This looks good");
       final sender = MockNotificationSender(
           username: "@id:acter.global"); // no display name
-      when(() => item.body()).thenReturn(msg);
       when(() => item.sender()).thenReturn(sender);
-      when(() => item.pushStyle()).thenReturn("reaction");
       when(() => item.parent()).thenReturn(null);
       final (title, body) = genTitleAndBody(item);
       expect(title, '"❤️"');
