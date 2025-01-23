0939f9e9052fc80f8d4c5d74ffd3b25f4ebefea0
kumarpalsinh25 <info@dajiraj.com>
2025-01-23 18:00:17 +0530

    Added test for reaction on Event, Task-List and Task Item

--- packages/acter_notifify/test/reaction_notification_test.dart
+++ packages/acter_notifify/test/reaction_notification_test.dart
@@ -65,6 +65,58 @@ void main() {
       expect(title, '❤️ Washington Johnson liked');
       expect(body, "📌 Candlesticks");
     });
+
+    test("Reaction on Event", () {
+      // Arrange: Set parent object data
+      when(() => parent.objectTypeStr()).thenReturn(ActerObject.event.name);
+      when(() => parent.emoji()).thenReturn(ActerObject.event.emoji);
+      when(() => parent.title()).thenReturn("Social Hours 2025");
+
+      // Arrange: Set reaction data
+      when(() => item.reactionKey()).thenReturn(PushStyles.reaction.emoji);
+
+      // Act: process data and get tile and body
+      final (title, body) = genTitleAndBody(item);
+
+      // Assert: Check if tile and body are as expected
+      expect(title, '❤️ Washington Johnson liked');
+      expect(body, "🗓️ Social Hours 2025");
+    });
+
+    test("Reaction on Task-List", () {
+      // Arrange: Set parent object data
+      when(() => parent.objectTypeStr()).thenReturn(ActerObject.taskList.name);
+      when(() => parent.emoji()).thenReturn(ActerObject.taskList.emoji);
+      when(() => parent.title()).thenReturn("New Year Goals");
+
+      // Arrange: Set reaction data
+      when(() => item.reactionKey()).thenReturn(PushStyles.reaction.emoji);
+
+      // Act: process data and get tile and body
+      final (title, body) = genTitleAndBody(item);
+
+      // Assert: Check if tile and body are as expected
+      expect(title, '❤️ Washington Johnson liked');
+      expect(body, "📋 New Year Goals");
+    });
+
+    test("Reaction on Task-Item", () {
+      // Arrange: Set parent object data
+      when(() => parent.objectTypeStr()).thenReturn(ActerObject.taskItem.name);
+      when(() => parent.emoji()).thenReturn(ActerObject.taskItem.emoji);
+      when(() => parent.title()).thenReturn("Website Redesign");
+
+      // Arrange: Set reaction data
+      when(() => item.reactionKey()).thenReturn(PushStyles.reaction.emoji);
+
+      // Act: process data and get tile and body
+      final (title, body) = genTitleAndBody(item);
+
+      // Assert: Check if tile and body are as expected
+      expect(title, '❤️ Washington Johnson liked');
+      expect(body, "☑️ Website Redesign");
+    });
+
     test("Reaction with parent", () {
       // Arrange: Set sender and parent object data
       final sender = MockNotificationSender(
