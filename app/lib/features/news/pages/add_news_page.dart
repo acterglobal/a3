87ca5c2d6a208e93c6239d8ae2fbc62e794d9832
kumarpalsinh25 <info@dajiraj.com>
2025-01-30 16:33:16 +0530

    Draft setup for Link Action in Boost

--- app/lib/features/news/pages/add_news_page.dart
+++ app/lib/features/news/pages/add_news_page.dart
@@ -221,6 +221,11 @@ class AddNewsState extends ConsumerState<AddNewsPage> {
               final notifier = ref.read(newsStateProvider.notifier);
               await notifier.selectTaskListToShare(buildContext);
             },
+            onShareLinkSelected: () async {
+              Navigator.pop(context);
+              final notifier = ref.read(newsStateProvider.notifier);
+              await notifier.enterLinkToShare(buildContext);
+            },
           ),
         );
       },
