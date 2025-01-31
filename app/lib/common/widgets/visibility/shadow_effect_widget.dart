0a5d96f2b3fadeccfa40c4a39b05d2d1ca6757f9
anisha-e10 <anisha.e10@dajiraj.com>
2025-01-31 12:00:58 +0530

    Increased contrast of icons on boosts & stories.

---
+++ app/lib/common/widgets/visibility/shadow_effect_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ShadowEffectWidget extends StatelessWidget {
+
+  final Widget child;
+
+  const ShadowEffectWidget({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Theme.of(context).shadowColor.withValues(alpha: 0.3), // Shadow color
+            spreadRadius: 1, // How much the shadow spreads
+            blurRadius: 15, // How much the shadow blurs
+            offset: const Offset(0, 4), // Adjust the shadow to be slightly below the icon
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+}
