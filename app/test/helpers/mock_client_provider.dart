d48438e825b2559026c8e8823d58c0bf527a7b1f
Benjamin Kampmann <ben@acter.global>
2025-02-04 12:18:52 +0000

    tests: Return client from build

--- app/test/helpers/mock_client_provider.dart
+++ app/test/helpers/mock_client_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:acter/features/home/providers/notifiers/client_notifier.dart';
 import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
 import 'package:mocktail/mocktail.dart';
@@ -9,4 +11,7 @@ class MockClientNotifier extends AsyncNotifier<Client?>
   final Client? client;
 
   MockClientNotifier({required this.client});
+
+  @override
+  FutureOr<Client?> build() => client;
 }
