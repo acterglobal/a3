25dd03322232bf6691dbc2c9a16e5b6468ccc32f
kumarpalsinh25 <info@dajiraj.com>
2025-02-12 20:11:58 +0530

    Invite code list test

---
+++ app/test/features/super_invites/invite_list_item_widget_test.dart
@@ -0,0 +1,86 @@
+import 'package:acter/features/super_invites/widgets/invite_list_item.dart';
+import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockingjay/mockingjay.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+import '../../helpers/mock_tasks_providers.dart';
+import '../../helpers/test_util.dart';
+
+class MockSuperInviteToken extends Mock implements SuperInviteToken {}
+
+void main() {
+  group('InviteListItem Widget Tests', () {
+    // Helper function to create the widget under test
+    Future<void> createWidgetUnderTest({
+      required WidgetTester tester,
+      required MockSuperInviteToken inviteToken,
+    }) async {
+      await tester.pumpProviderWidget(
+        child: InviteListItem(inviteToken: inviteToken),
+      );
+      await tester.pumpAndSettle();
+    }
+
+    testWidgets('displays invite token and accepted count correctly',
+        (tester) async {
+      final inviteToken = MockSuperInviteToken();
+
+      // Mock the methods of SuperInviteToken
+      when(() => inviteToken.token()).thenReturn('invite_token_123');
+      when(() => inviteToken.acceptedCount()).thenReturn(5);
+      final mockFfiList = MockFfiListFfiString();
+      mockFfiList.add(MockFfiString('room1')); // Add a room string to the list
+      when(() => inviteToken.rooms()).thenReturn(mockFfiList);
+
+      await createWidgetUnderTest(tester: tester, inviteToken: inviteToken);
+      await tester.pumpAndSettle();
+      expect(
+        find.text('invite_token_123'),
+        findsOneWidget,
+      ); // Check if token is displayed
+      expect(
+        find.text('Used 5 times'),
+        findsOneWidget,
+      ); // Check if accepted count is displayed
+      expect(find.byIcon(PhosphorIcons.share()), findsOneWidget);
+    });
+
+    testWidgets('display share button when room is available',
+        (tester) async {
+      final inviteToken = MockSuperInviteToken();
+
+      // Mock the methods of SuperInviteToken
+      when(() => inviteToken.token()).thenReturn('invite_token_123');
+      when(() => inviteToken.acceptedCount()).thenReturn(5);
+      final mockFfiList = MockFfiListFfiString();
+      mockFfiList.add(MockFfiString('room1')); // Add a room string to the list
+      when(() => inviteToken.rooms()).thenReturn(mockFfiList);
+
+      await createWidgetUnderTest(tester: tester, inviteToken: inviteToken);
+      await tester.pumpAndSettle();
+
+      // Step 3: Verify that the share button (icon) is not displayed
+      expect(find.byIcon(PhosphorIcons.share()), findsOne);
+    });
+
+    testWidgets('does not display share button when room is unavailable',
+        (tester) async {
+      final inviteToken = MockSuperInviteToken();
+
+      // Mock the methods of SuperInviteToken
+      when(() => inviteToken.token()).thenReturn('invite_token_123');
+      when(() => inviteToken.acceptedCount()).thenReturn(5);
+      final mockFfiList = MockFfiListFfiString();
+      when(() => inviteToken.rooms()).thenReturn(mockFfiList);
+
+      await createWidgetUnderTest(tester: tester, inviteToken: inviteToken);
+      await tester.pumpAndSettle();
+
+      // Step 3: Verify that the share button (icon) is not displayed
+      expect(find.byIcon(PhosphorIcons.share()), findsNothing);
+    });
+  });
+}
