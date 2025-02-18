ef7157ad544d81043d9c7d8ea1f33b4dbdfdee14
Benjamin Kampmann <ben@acter.global>
2025-02-18 11:07:47 +0000

    stories providers draft

--- app/lib/features/news/providers/news_providers.dart
+++ app/lib/features/news/providers/news_providers.dart
@@ -1,13 +1,37 @@
 import 'package:acter/common/providers/reactions_providers.dart';
 import 'package:acter/features/news/providers/notifiers/news_list_notifier.dart';
+import 'package:acter/features/news/types.dart';
 import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart'
     show NewsEntry, ReactionManager;
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:riverpod/riverpod.dart';
 
 final newsListProvider = AsyncNotifierProvider.family<AsyncNewsListNotifier,
     List<NewsEntry>, String?>(
   () => AsyncNewsListNotifier(),
 );
+
+final storyListProvider =
+    AsyncNotifierProvider.family<AsyncNewsListNotifier, List<Story>, String?>(
+  () => AsyncStoryListNotifier(),
+);
+
+final updatesProvider =
+    FutureProvider.family<List<UpdateEntry>, String?>((ref, arg) async {
+  final news = (await ref.watch(newsListProvider(arg).future))
+      .map((inner) => UpdateNewsEntry(inner))
+      .toList();
+  final stories = (await ref.watch(storyListProvider(arg).future))
+      .map((inner) => UpdateStory(inner))
+      .toList();
+
+  final List<UpdateEntry> entries = [];
+  entries.addAll(news);
+  entries.addAll(stories);
+
+  // sort
+  return entries;
+});
 
 final newsReactionsProvider =
     FutureProvider.family<ReactionManager, NewsEntry>((ref, news) async {
---
+++ app/lib/features/news/providers/notifiers/story_list_notifier.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+
+import 'package:acter/features/home/providers/client_providers.dart';
+import 'package:acter_flutter_sdk/acter_flutter_sdk.dart';
+import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart' show NewsEntry;
+import 'package:logging/logging.dart';
+import 'package:riverpod/riverpod.dart';
+
+final _log = Logger('a3::news::list_notifier');
+
+class AsyncNewsListNotifier
+    extends FamilyAsyncNotifier<List<NewsEntry>, String?> {
+  late Stream<bool> _listener;
+  late StreamSubscription<bool> _poller;
+
+  @override
+  Future<List<NewsEntry>> build(String? arg) async {
+    final spaceId = arg;
+    final client = await ref.watch(alwaysClientProvider.future);
+
+    //GET ALL NEWS
+    if (spaceId == null) {
+      _listener = client.subscribeSectionStream('stories');
+    } else {
+      //GET SPACE NEWS
+      _listener = client.subscribeRoomSectionStream(spaceId, 'stories');
+    }
+    _poller = _listener.listen(
+      (data) async {
+        _log.info('stories subscribe received');
+        state = await AsyncValue.guard(() => _fetchNews(client, spaceId));
+      },
+      onError: (e, s) {
+        _log.severe('stream errored', e, s);
+      },
+      onDone: () {
+        _log.info('stream ended');
+      },
+    );
+    ref.onDispose(() => _poller.cancel());
+    return await _fetchNews(client, spaceId);
+  }
+
+  Future<List<NewsEntry>> _fetchNews(Client client, String? spaceId) async {
+    //GET ALL NEWS
+    if (spaceId == null) {
+      final newsEntries =
+          await client.latestNewsEntries(25); // this might throw internally
+      return sortNewsListDscTime(newsEntries.toList());
+    } else {
+      //GET SPACE NEWS
+      final space = await client.space(spaceId);
+      final newsEntries =
+          await space.latestNewsEntries(100); // this might throw internally
+      return sortNewsListDscTime(newsEntries.toList());
+    }
+  }
+}
+
+Future<List<NewsEntry>> sortNewsListDscTime(List<NewsEntry> newsList) async {
+  newsList.sort((a, b) => b.originServerTs().compareTo(a.originServerTs()));
+  return newsList;
+}
