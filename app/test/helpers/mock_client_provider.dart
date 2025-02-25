e1234fdd5fc5e771507708659d9466c8ac8d8f30
Benjamin Kampmann <ben@acter.global>
2025-02-25 14:45:14 +0000

    Testing for double always client triggering

--- app/test/helpers/mock_client_provider.dart
+++ app/test/helpers/mock_client_provider.dart
@@ -1,17 +1,56 @@
 import 'dart:async';
 
+import 'package:acter/common/providers/notifiers/sync_notifier.dart';
 import 'package:acter/features/home/providers/notifiers/client_notifier.dart';
-import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
+import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart' as ffi;
 import 'package:mocktail/mocktail.dart';
 import 'package:riverpod/riverpod.dart';
 
-class MockClientNotifier extends AsyncNotifier<Client?>
+class MockClientNotifier extends AsyncNotifier<ffi.Client?>
     with Mock
     implements ClientNotifier {
-  final Client? client;
+  final ffi.Client? client;
 
   MockClientNotifier({required this.client});
 
   @override
-  FutureOr<Client?> build() => client;
+  FutureOr<ffi.Client?> build() => client;
+}
+
+class PendingUntilFoundMockClientNotifier extends AsyncNotifier<ffi.Client?>
+    with Mock
+    implements ClientNotifier {
+  late Completer<ffi.Client> completer;
+
+  PendingUntilFoundMockClientNotifier() {
+    completer = Completer();
+  }
+
+  @override
+  FutureOr<ffi.Client?> build() => completer.future;
+
+  @override
+  void setClient(ffi.Client? client) {
+    completer.complete(client!);
+  }
+}
+
+class MockSyncNotifier extends SyncNotifier {
+  int restarted = 0;
+  int clientSet = 0;
+  ffi.Client? _client;
+
+  @override
+  set client(ffi.Client? value) {
+    clientSet += 1;
+    _client;
+  }
+
+  @override
+  ffi.Client get client => _client!;
+
+  @override
+  void restartSync() {
+    restarted += 1;
+  }
 }
