f9a4bd4fc19c41de925a87913afb31c5442e0541
kumarpalsinh25 <info@dajiraj.com>
2025-02-27 14:42:55 +0530

    Add Story action button in Space Overview screen

--- app/lib/features/space/widgets/space_sections/space_actions_section.dart
+++ app/lib/features/space/widgets/space_sections/space_actions_section.dart
@@ -50,7 +50,14 @@ class SpaceActionsSection extends ConsumerWidget {
     return Wrap(
       crossAxisAlignment: WrapCrossAlignment.start,
       children: [
-        addPostUpdateButton(
+        addNewsButton(
+          context,
+          ref,
+          canChangeSetting,
+          membership,
+          settings,
+        ),
+        addStoryButton(
           context,
           ref,
           canChangeSetting,
@@ -157,7 +164,7 @@ class SpaceActionsSection extends ConsumerWidget {
     return const SizedBox.shrink();
   }
 
-  Widget addPostUpdateButton(
+  Widget addNewsButton(
     BuildContext context,
     WidgetRef ref,
     bool canChangeSetting,
@@ -179,6 +186,43 @@ class SpaceActionsSection extends ConsumerWidget {
               ref: ref,
               spaceId: spaceId,
               feature: SpaceFeature.boosts,
+            );
+            if (!result) return;
+          }
+          if (context.mounted) {
+            context.pushNamed(
+              Routes.actionAddUpdate.name,
+              queryParameters: {'spaceId': spaceId},
+            );
+          }
+        },
+      );
+    }
+    return const SizedBox.shrink();
+  }
+
+  Widget addStoryButton(
+    BuildContext context,
+    WidgetRef ref,
+    bool canChangeSetting,
+    Member? membership,
+    ActerAppSettings? settings,
+  ) {
+    final isActive = settings?.stories().active() == true;
+    final canPostUpdate =
+        isActive && membership?.canString('CanPostStories') == true;
+    if (canPostUpdate || canChangeSetting) {
+      return simpleActionButton(
+        context: context,
+        iconData: PhosphorIcons.newspaper(),
+        title: L10n.of(context).addStory,
+        onPressed: () async {
+          if (!isActive && canChangeSetting) {
+            final result = await offerToActivateFeature(
+              context: context,
+              ref: ref,
+              spaceId: spaceId,
+              feature: SpaceFeature.stories,
             );
             if (!result) return;
           }
