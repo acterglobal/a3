f62fc4f719e9cc3caed0d89f0bb792be96d918db
kumarpalsinh25 <info@dajiraj.com>
2025-03-03 10:41:02 +0530

    Updated colors

--- app/lib/features/news/model/news_post_color_data.dart
+++ app/lib/features/news/model/news_post_color_data.dart
@@ -16,12 +16,12 @@ final List<PostColorScheme> postColorSchemes = [
   PostColorScheme(
     backgroundColor: Colors.pinkAccent, // Purple 200
     foregroundColor: Colors.white, // Deep Purple 900
-    linkColor: Colors.blue, // Blue 800
+    linkColor: Colors.lightBlueAccent.shade700, // Blue 800
   ),
   PostColorScheme(
     backgroundColor: Colors.green, // Purple 200
     foregroundColor: Colors.white, // Deep Purple 900
-    linkColor: Colors.blue.shade700, // Blue 800
+    linkColor: Colors.lightBlueAccent.shade100, // Blue 800
   ),
   PostColorScheme(
     backgroundColor: Colors.brown, // Purple 200
@@ -36,32 +36,27 @@ final List<PostColorScheme> postColorSchemes = [
   PostColorScheme(
     backgroundColor: Colors.amber, // Purple 200
     foregroundColor: Colors.white, // Deep Purple 900
-    linkColor: Colors.lightBlueAccent, // Blue 800
+    linkColor: Colors.blue.shade600, // Blue 800
   ),
   PostColorScheme(
-    backgroundColor: Colors.cyan, // Purple 200
+    backgroundColor: Colors.cyan.shade800, // Purple 200
     foregroundColor: Colors.white, // Deep Purple 900
-    linkColor: Colors.blue.shade800, // Blue 800
+    linkColor: Colors.lightBlueAccent.shade100, // Blue 800
   ),
   PostColorScheme(
-    backgroundColor: Colors.deepOrange, // Purple 200
-    foregroundColor: Colors.white, // Deep Purple 900
-    linkColor: Colors.blue, // Blue 800
-  ),
-  PostColorScheme(
-    backgroundColor: Colors.deepPurple, // Purple 200
+    backgroundColor: Colors.orange.shade400, // Purple 200
     foregroundColor: Colors.white, // Deep Purple 900
     linkColor: Colors.blue, // Blue 800
   ),
   PostColorScheme(
-    backgroundColor: Colors.indigo, // Purple 200
+    backgroundColor: Colors.indigo.shade700, // Purple 200
     foregroundColor: Colors.white, // Deep Purple 900
-    linkColor: Colors.lightBlueAccent, // Blue 800
+    linkColor: Colors.lightBlueAccent.shade100, // Blue 800
   ),
   PostColorScheme(
-    backgroundColor: Colors.pink, // Purple 200
+    backgroundColor: Colors.pink.shade300, // Purple 200
     foregroundColor: Colors.white, // Deep Purple 900
-    linkColor: Colors.lightBlue, // Blue 800
+    linkColor: Colors.blue.shade600, // Blue 800
   ),
   PostColorScheme(
     backgroundColor: Colors.black, // Purple 200
