3083b9cf1c8ee1ba2359709d8e54c41c5de4df92
kumarpalsinh25 <info@dajiraj.com>
2025-03-06 16:21:23 +0530

    Added more tests for activity attachment item widget

--- app/test/features/activity/item_widgets/activity_attachment_item_widget_test.dart
+++ app/test/features/activity/item_widgets/activity_attachment_item_widget_test.dart
@@ -54,4 +54,85 @@ void main() {
     // Verify user info
     expect(find.text('User-1'), findsOneWidget);
   });
+
+  testWidgets('Add attachment on Event Object', (tester) async {
+    MockActivity mockActivity = MockActivity(
+      mockType: PushStyles.attachment.name,
+      mockObject: MockActivityObject(
+        mockType: SpaceObjectTypes.event.name,
+        mockEmoji: SpaceObjectTypes.event.emoji,
+        mockTitle: 'Team Meeting',
+      ),
+    );
+    await createWidgetUnderTest(tester: tester, mockActivity: mockActivity);
+
+    // Verify action title
+    expect(
+      find.text('${PushStyles.attachment.emoji} Added attachment on'),
+      findsOneWidget,
+    );
+
+    // Verify object info
+    expect(
+      find.text('${SpaceObjectTypes.event.emoji} Team Meeting'),
+      findsOneWidget,
+    );
+
+    // Verify user info
+    expect(find.text('User-1'), findsOneWidget);
+  });
+
+  testWidgets('Add attachment on TaskList Object', (tester) async {
+    MockActivity mockActivity = MockActivity(
+      mockType: PushStyles.attachment.name,
+      mockObject: MockActivityObject(
+        mockType: SpaceObjectTypes.taskList.name,
+        mockEmoji: SpaceObjectTypes.taskList.emoji,
+        mockTitle: 'Project Tasks',
+      ),
+    );
+    await createWidgetUnderTest(tester: tester, mockActivity: mockActivity);
+
+    // Verify action title
+    expect(
+      find.text('${PushStyles.attachment.emoji} Added attachment on'),
+      findsOneWidget,
+    );
+
+    // Verify object info
+    expect(
+      find.text('${SpaceObjectTypes.taskList.emoji} Project Tasks'),
+      findsOneWidget,
+    );
+
+    // Verify user info
+    expect(find.text('User-1'), findsOneWidget);
+  });
+
+  testWidgets('Add attachment on TaskItem Object', (tester) async {
+    MockActivity mockActivity = MockActivity(
+      mockType: PushStyles.attachment.name,
+      mockObject: MockActivityObject(
+        mockType: SpaceObjectTypes.taskItem.name,
+        mockEmoji: SpaceObjectTypes.taskItem.emoji,
+        mockTitle: 'Complete Documentation',
+      ),
+    );
+    await createWidgetUnderTest(tester: tester, mockActivity: mockActivity);
+
+    // Verify action title
+    expect(
+      find.text('${PushStyles.attachment.emoji} Added attachment on'),
+      findsOneWidget,
+    );
+
+    // Verify object info
+    expect(
+      find.text('${SpaceObjectTypes.taskItem.emoji} Complete Documentation'),
+      findsOneWidget,
+    );
+
+    // Verify user info
+    expect(find.text('User-1'), findsOneWidget);
+  });
 }
