fa266cebf913eda8fe625fcf73de9fc5b2324090
kumarpalsinh25 <info@dajiraj.com>
2025-03-06 16:15:50 +0530

    Added more test for comment on object activity item

--- app/test/features/activity/item_widgets/activity_comment_item_widget_test.dart
+++ app/test/features/activity/item_widgets/activity_comment_item_widget_test.dart
@@ -61,4 +61,136 @@ void main() {
     // Verify comment content
     expect(find.text('This is a comment on a pin object'), findsOneWidget);
   });
+
+  testWidgets('Comment on Event Object', (tester) async {
+    MockActivity mockActivity = MockActivity(
+      mockType: PushStyles.comment.name,
+      mockMsgContent: MockMsgContent(
+        bodyText: 'This is a comment on an event',
+      ),
+      mockObject: MockActivityObject(
+        mockType: SpaceObjectTypes.event.name,
+        mockEmoji: SpaceObjectTypes.event.emoji,
+        mockTitle: 'Team Meeting',
+      ),
+    );
+    await createWidgetUnderTest(tester: tester, mockActivity: mockActivity);
+
+    // Verify action title
+    expect(
+      find.text('${PushStyles.comment.emoji} Commented on'),
+      findsOneWidget,
+    );
+
+    // Verify object info
+    expect(
+      find.text('${SpaceObjectTypes.event.emoji} Team Meeting'),
+      findsOneWidget,
+    );
+
+    // Verify user info
+    expect(find.text('User-1'), findsOneWidget);
+
+    // Verify comment content
+    expect(find.text('This is a comment on an event'), findsOneWidget);
+  });
+
+  testWidgets('Comment on TaskList Object', (tester) async {
+    MockActivity mockActivity = MockActivity(
+      mockType: PushStyles.comment.name,
+      mockMsgContent: MockMsgContent(
+        bodyText: 'This is a comment on a task list',
+      ),
+      mockObject: MockActivityObject(
+        mockType: SpaceObjectTypes.taskList.name,
+        mockEmoji: SpaceObjectTypes.taskList.emoji,
+        mockTitle: 'Project Tasks',
+      ),
+    );
+    await createWidgetUnderTest(tester: tester, mockActivity: mockActivity);
+
+    // Verify action title
+    expect(
+      find.text('${PushStyles.comment.emoji} Commented on'),
+      findsOneWidget,
+    );
+
+    // Verify object info
+    expect(
+      find.text('${SpaceObjectTypes.taskList.emoji} Project Tasks'),
+      findsOneWidget,
+    );
+
+    // Verify user info
+    expect(find.text('User-1'), findsOneWidget);
+
+    // Verify comment content
+    expect(find.text('This is a comment on a task list'), findsOneWidget);
+  });
+
+  testWidgets('Comment on TaskItem Object', (tester) async {
+    MockActivity mockActivity = MockActivity(
+      mockType: PushStyles.comment.name,
+      mockMsgContent: MockMsgContent(
+        bodyText: 'This is a comment on a task',
+      ),
+      mockObject: MockActivityObject(
+        mockType: SpaceObjectTypes.taskItem.name,
+        mockEmoji: SpaceObjectTypes.taskItem.emoji,
+        mockTitle: 'Complete Documentation',
+      ),
+    );
+    await createWidgetUnderTest(tester: tester, mockActivity: mockActivity);
+
+    // Verify action title
+    expect(
+      find.text('${PushStyles.comment.emoji} Commented on'),
+      findsOneWidget,
+    );
+
+    // Verify object info
+    expect(
+      find.text('${SpaceObjectTypes.taskItem.emoji} Complete Documentation'),
+      findsOneWidget,
+    );
+
+    // Verify user info
+    expect(find.text('User-1'), findsOneWidget);
+
+    // Verify comment content
+    expect(find.text('This is a comment on a task'), findsOneWidget);
+  });
+
+  testWidgets('Comment on News Object', (tester) async {
+    MockActivity mockActivity = MockActivity(
+      mockType: PushStyles.comment.name,
+      mockMsgContent: MockMsgContent(
+        bodyText: 'This is a comment on a news item',
+      ),
+      mockObject: MockActivityObject(
+        mockType: SpaceObjectTypes.news.name,
+        mockEmoji: SpaceObjectTypes.news.emoji,
+        mockTitle: 'Product Launch',
+      ),
+    );
+    await createWidgetUnderTest(tester: tester, mockActivity: mockActivity);
+
+    // Verify action title
+    expect(
+      find.text('${PushStyles.comment.emoji} Commented on'),
+      findsOneWidget,
+    );
+
+    // Verify object info
+    expect(
+      find.text('${SpaceObjectTypes.news.emoji} Product Launch'),
+      findsOneWidget,
+    );
+
+    // Verify user info
+    expect(find.text('User-1'), findsOneWidget);
+
+    // Verify comment content
+    expect(find.text('This is a comment on a news item'), findsOneWidget);
+  });
 }
