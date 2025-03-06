26ccf78d0f011f6b8daac6b0f0ebcc22bed4a75b
kumarpalsinh25 <info@dajiraj.com>
2025-03-06 19:50:55 +0530

    File renamed

--- app/test/features/activity/providers/activity_dates_providers_test.dart
+++ app/test/features/activity/providers/activity_dates_providers_test.dart
@@ -1,8 +1,6 @@
 import 'package:acter/features/activities/providers/activities_providers.dart';
-import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
 import '../mock_data/mock_activity.dart';
 
 void main() {
