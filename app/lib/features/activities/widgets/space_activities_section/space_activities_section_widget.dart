2274c0183a8cd3df46bb2a0556e7a01ecb8dedf4
kumarpalsinh25 <info@dajiraj.com>
2025-03-06 17:02:17 +0530

    Minor fixes

--- app/lib/features/activities/widgets/space_activities_section/space_activities_section_widget.dart
+++ app/lib/features/activities/widgets/space_activities_section/space_activities_section_widget.dart
@@ -3,7 +3,6 @@ import 'package:acter/features/activities/widgets/space_activities_section/item_
 import 'package:acter/features/space/widgets/space_sections/section_header.dart';
 import 'package:flutter/material.dart';
 import 'package:acter/l10n/generated/l10n.dart';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Widget? buildSpaceActivitiesSectionWidget(
