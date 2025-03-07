3fe97e1f0209a78de02e0c7d5432c392926de571
Benjamin Kampmann <ben@acter.global>
2025-03-07 18:22:08 +0000

    appflowy editor with our html

--- app/lib/common/toolkit/html/render_html.dart
+++ app/lib/common/toolkit/html/render_html.dart
@@ -3,7 +3,6 @@ import 'package:acter/common/toolkit/html/render_html_ng.dart';
 import 'package:acter/features/labs/model/labs_features.dart';
 import 'package:acter/features/labs/providers/labs_providers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_matrix_html/flutter_html.dart' as matrixHtml;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 typedef PillBuilder =
@@ -12,40 +11,6 @@ typedef PillBuilder =
       required String url,
       void Function(String)? onTap,
     });
-
-class _MatrixRenderHtml extends ConsumerWidget {
-  final String text;
-  final TextStyle? defaultTextStyle;
-  final TextStyle? linkTextStyle;
-  final bool shrinkToFit;
-  final int? maxLines;
-  final bool renderNewlines;
-  final PillBuilder? pillBuilder;
-  const _MatrixRenderHtml({
-    required this.text,
-    this.defaultTextStyle,
-    this.linkTextStyle,
-    this.shrinkToFit = false,
-    this.maxLines,
-    this.renderNewlines = false,
-    this.pillBuilder,
-  });
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return matrixHtml.Html(
-      onLinkTap: (target) async {
-        await openLink(ref, target.toString(), context);
-      },
-      maxLines: maxLines,
-      shrinkToFit: shrinkToFit,
-      renderNewlines: renderNewlines,
-      data: text,
-      pillBuilder: pillBuilder,
-      defaultTextStyle: defaultTextStyle,
-      linkStyle: linkTextStyle,
-    );
-  }
-}
 
 class RenderHtml extends ConsumerWidget {
   final String text;
@@ -70,27 +35,27 @@ class RenderHtml extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final nextHtml = ref.watch(isActiveProvider(LabsFeature.htmlNext));
-    if (nextHtml) {
-      return RenderHtmlNg(
-        text: text,
-        defaultTextStyle: defaultTextStyle,
-        linkTextStyle: linkTextStyle,
-        shrinkToFit: shrinkToFit,
-        maxLines: maxLines,
-        renderNewlines: renderNewlines,
-        roomId: roomId,
-      );
-    } else {
-      return _MatrixRenderHtml(
-        text: text,
-        defaultTextStyle: defaultTextStyle,
-        linkTextStyle: linkTextStyle,
-        shrinkToFit: shrinkToFit,
-        maxLines: maxLines,
-        renderNewlines: renderNewlines,
-        pillBuilder: pillBuilder,
-      );
-    }
+    // final nextHtml = ref.watch(isActiveProvider(LabsFeature.htmlNext));
+    // if (nextHtml) {
+    return RenderHtmlNg(
+      text: text,
+      defaultTextStyle: defaultTextStyle,
+      linkTextStyle: linkTextStyle,
+      shrinkToFit: shrinkToFit,
+      maxLines: maxLines,
+      renderNewlines: renderNewlines,
+      roomId: roomId,
+    );
+    // } else {
+    //   return _MatrixRenderHtml(
+    //     text: text,
+    //     defaultTextStyle: defaultTextStyle,
+    //     linkTextStyle: linkTextStyle,
+    //     shrinkToFit: shrinkToFit,
+    //     maxLines: maxLines,
+    //     renderNewlines: renderNewlines,
+    //     pillBuilder: pillBuilder,
+    //   );
+    // }
   }
 }
