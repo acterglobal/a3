88c7d6b385880d7d8e50e85ee79216b1aa4ecc6a
Benjamin Kampmann <ben@acter.global>
2025-03-09 14:41:57 +0000

    Highlight support for user pills if itis me

--- app/lib/common/toolkit/buttons/user_chip.dart
+++ app/lib/common/toolkit/buttons/user_chip.dart
@@ -1,17 +1,22 @@
+import 'package:acter/common/providers/common_providers.dart';
 import 'package:acter/common/providers/room_providers.dart';
 import 'package:acter/features/member/dialogs/show_member_info_drawer.dart';
+import 'package:acter/l10n/generated/l10n.dart';
 import 'package:acter_avatar/acter_avatar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path/path.dart';
 
 class UserChip extends ConsumerWidget {
   final String roomId;
   final String memberId;
+  final TextStyle? style;
 
   const UserChip({
     super.key,
     required this.roomId,
     required this.memberId,
+    this.style,
   });
 
   @override
@@ -19,7 +24,9 @@ class UserChip extends ConsumerWidget {
     final memberInfo = ref.watch(
       memberAvatarInfoProvider((roomId: roomId, userId: memberId)),
     );
-    final fontSize = Theme.of(context).textTheme.bodySmall?.fontSize ?? 12.0;
+    final isMe = memberId == ref.watch(myUserIdStrProvider);
+    final style = this.style ?? Theme.of(context).textTheme.bodySmall;
+    final fontSize = style?.fontSize ?? 12.0;
     return Tooltip(
       message: memberId,
       child: InkWell(
@@ -27,14 +34,16 @@ class UserChip extends ConsumerWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             ActerAvatar(
-              options: AvatarOptions.DM(
-                memberInfo,
-                size: fontSize / 2,
-              ),
+              options: AvatarOptions.DM(memberInfo, size: fontSize / 2),
             ),
             SizedBox(width: 4),
-            Text(memberInfo.displayName ?? memberId),
-            SizedBox(width: 4),
+            if (isMe)
+              Text(
+                memberInfo.displayName ?? memberId,
+                style: style?.copyWith(fontStyle: FontStyle.italic),
+              )
+            else
+              Text(memberInfo.displayName ?? memberId, style: style),
           ],
         ),
         onTap: () async {
