4c5ca651d967626dbb5357e05151a25e87ce27e7
anisha-e10 <anisha.e10@dajiraj.com>
2025-03-26 13:05:20 +0530

    Removed _tryRedeem() function from register action.

--- app/lib/features/auth/actions/register_action.dart
+++ app/lib/features/auth/actions/register_action.dart
@@ -1,21 +1,11 @@
 import 'package:acter/features/auth/providers/auth_providers.dart';
 import 'package:acter_flutter_sdk/acter_flutter_sdk.dart';
-import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logging/logging.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final _log = Logger('a3::auth::register');
-
-Future<void> _tryRedeem(SuperInvites superInvites, String token) async {
-  // try to redeem the token in a fire-and-forget-manner
-  try {
-    await superInvites.redeem(token);
-  } catch (error, stack) {
-    _log.severe('redeeming super invite `$token` failed: $error', error, stack);
-  }
-}
 
 Future<bool> register({
   required BuildContext context,
