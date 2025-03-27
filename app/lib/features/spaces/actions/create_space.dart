d7c2c0e871b8ade4754f990ae53a1e297afb32d4
kumarpalsinh25 <info@dajiraj.com>
2025-03-27 18:11:18 +0530

    Change logic of permission based on the new structure

--- app/lib/features/spaces/actions/create_space.dart
+++ app/lib/features/spaces/actions/create_space.dart
@@ -7,8 +7,6 @@ import 'package:acter/features/chat/actions/create_chat.dart';
 import 'package:acter/features/home/providers/client_providers.dart';
 import 'package:acter/features/room/model/room_join_rule.dart';
 import 'package:acter/features/space/actions/set_acter_feature.dart';
-import 'package:acter/features/space/settings/pages/apps_settings_page.dart';
-import 'package:acter/features/spaces/model/permission_config.dart';
 import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
@@ -55,6 +53,11 @@ Future<String?> createSpace(
     if (roomJoinRule != null) {
       config.joinRule(roomJoinRule.name);
     }
+    final permissionsBuilder = await generatePermissions(ref, lang);
+    if (permissionsBuilder != null) {
+      config.setPermissions(permissionsBuilder);
+    }
+
     final client = await ref.read(alwaysClientProvider.future);
     final result = await client.createActerSpace(config.build());
     final roomId = result.toString();
@@ -82,11 +85,7 @@ Future<String?> createSpace(
       }
     }
 
-    EasyLoading.show(status: lang.applyingFeatureSettings);
-    // Add a delay to ensure that space creation is completed
-    await Future.delayed(const Duration(milliseconds: 1000));
     if (!context.mounted) return null;
-    await applySpaceFeatures(context, ref, roomId);
     EasyLoading.dismiss();
     return roomId;
   } catch (e, s) {
@@ -103,110 +102,91 @@ Future<String?> createSpace(
   }
 }
 
-Future<void> applySpaceFeatures(
-  BuildContext context,
+Future<AppPermissionsBuilder?> generatePermissions(
   WidgetRef ref,
-  String spaceId,
+  L10n lang,
 ) async {
-  final lang = L10n.of(context);
   try {
+    //Get permissions builder
+    final sdk = await ref.read(sdkProvider.future);
+    final permissionsBuilder = sdk.api.newAppPermissionsBuilder();
+
+    //Get feature states
     final featureStates = ref.read(featureActivationStateProvider);
-    ref.invalidate(featureActivationStateProvider);
-    final appSettingsAndMembership = await ref.read(
-      spaceAppSettingsProvider(spaceId).future,
-    );
-    final space = appSettingsAndMembership.space;
-    final appSettings = appSettingsAndMembership.settings;
-    final powerLevels = appSettingsAndMembership.powerLevels;
 
-    if (context.mounted) {
-      for (final entry in featureStates.entries) {
-        final feature = entry.key;
-        final state = entry.value;
+    for (final entry in featureStates.entries) {
+      final feature = entry.key;
+      final state = entry.value;
 
-        if (state.isActivated) {
-          //Activate the feature
-          final builder = appSettings.setActivatedBuilder(feature, true);
-          await space.updateAppSettings(builder);
-
-          //Set the power level based on the permission level
-          await setPowerLevel(space, powerLevels, state.permissions);
-
-          // Add a delay between each update to ensure they complete in sequence
-          await Future.delayed(const Duration(milliseconds: 500));
-        }
+      switch (feature) {
+        case SpaceFeature.boosts:
+          permissionsBuilder.news(state.isActivated);
+          for (final permission in state.permissions) {
+            if (permission.key == 'boost-post') {
+              permissionsBuilder.newsPermisisons(
+                permission.permissionLevel.value,
+              );
+            }
+          }
+          break;
+        case SpaceFeature.stories:
+          permissionsBuilder.stories(state.isActivated);
+          for (final permission in state.permissions) {
+            if (permission.key == 'story-post') {
+              permissionsBuilder.storiesPermisisons(
+                permission.permissionLevel.value,
+              );
+            }
+          }
+          break;
+        case SpaceFeature.pins:
+          permissionsBuilder.pins(state.isActivated);
+          for (final permission in state.permissions) {
+            if (permission.key == 'pin-post') {
+              permissionsBuilder.pinsPermisisons(
+                permission.permissionLevel.value,
+              );
+            }
+          }
+          break;
+        case SpaceFeature.events:
+          permissionsBuilder.calendarEvents(state.isActivated);
+          for (final permission in state.permissions) {
+            if (permission.key == 'event-post') {
+              permissionsBuilder.calendarEventsPermisisons(
+                permission.permissionLevel.value,
+              );
+            } else if (permission.key == 'event-rsvp') {
+              permissionsBuilder.rsvpPermisisons(
+                permission.permissionLevel.value,
+              );
+            }
+          }
+          break;
+        case SpaceFeature.tasks:
+          permissionsBuilder.tasks(state.isActivated);
+          for (final permission in state.permissions) {
+            if (permission.key == 'task-list-post') {
+              permissionsBuilder.tasksPermisisons(
+                permission.permissionLevel.value,
+              );
+            } else if (permission.key == 'task-item-post') {
+              permissionsBuilder.tasksPermisisons(
+                permission.permissionLevel.value,
+              );
+            }
+          }
+          break;
       }
     }
+
+    return permissionsBuilder;
   } catch (e, s) {
     _log.severe('Failed to apply features settings', e, s);
     EasyLoading.showError(
       lang.creatingSpaceFailed(e),
       duration: const Duration(seconds: 3),
     );
+    return null;
   }
-}
-
-Future<void> setPowerLevel(
-  Space space,
-  RoomPowerLevels powerLevels,
-  List<PermissionConfig> permissions,
-) async {
-  for (final permissionEntry in permissions) {
-    PowerLevelKeyAndValue? powerLevelKeyAndValue = getPowerLevelKeyAndValue(
-      powerLevels,
-      permissionEntry,
-    );
-    if (powerLevelKeyAndValue != null) {
-      await space.updateFeaturePowerLevels(
-        powerLevelKeyAndValue.key,
-        powerLevelKeyAndValue.value,
-      );
-      // Add a delay between each update to ensure they complete in sequence
-      await Future.delayed(const Duration(milliseconds: 500));
-    }
-  }
-}
-
-class PowerLevelKeyAndValue {
-  final String key;
-  final int value;
-
-  const PowerLevelKeyAndValue({required this.key, required this.value});
-}
-
-PowerLevelKeyAndValue? getPowerLevelKeyAndValue(
-  RoomPowerLevels powerLevels,
-  PermissionConfig permissionConfig,
-) {
-  return switch (permissionConfig.key) {
-    'boost-post' => PowerLevelKeyAndValue(
-      key: powerLevels.newsKey().toString(),
-      value: permissionConfig.permissionLevel.value,
-    ),
-    'story-post' => PowerLevelKeyAndValue(
-      key: powerLevels.storiesKey().toString(),
-      value: permissionConfig.permissionLevel.value,
-    ),
-    'pin-post' => PowerLevelKeyAndValue(
-      key: powerLevels.pinsKey().toString(),
-      value: permissionConfig.permissionLevel.value,
-    ),
-    'event-post' => PowerLevelKeyAndValue(
-      key: powerLevels.eventsKey().toString(),
-      value: permissionConfig.permissionLevel.value,
-    ),
-    'event-rsvp' => PowerLevelKeyAndValue(
-      key: powerLevels.rsvpKey().toString(),
-      value: permissionConfig.permissionLevel.value,
-    ),
-    'task-list-post' => PowerLevelKeyAndValue(
-      key: powerLevels.taskListsKey().toString(),
-      value: permissionConfig.permissionLevel.value,
-    ),
-    'task-item-post' => PowerLevelKeyAndValue(
-      key: powerLevels.tasksKey().toString(),
-      value: permissionConfig.permissionLevel.value,
-    ),
-    _ => null,
-  };
 }
