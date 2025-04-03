5283bdeec83f2509e1397afb5a80589f27333cda
kumarpalsinh25 <info@dajiraj.com>
2025-04-03 13:20:26 +0530

    Update chat_list_showcase_page.dart

--- app/lib/features/chat_ui_showcase/pages/chat_list_showcase_page.dart
+++ app/lib/features/chat_ui_showcase/pages/chat_list_showcase_page.dart
@@ -1,3 +1,5 @@
+import 'package:acter/common/widgets/acter_search_widget.dart';
+import 'package:acter/common/widgets/plus_icon_widget.dart';
 import 'package:acter/features/chat_ui_showcase/widgets/chat_list_item_showcase.dart';
 import 'package:flutter/material.dart';
 
@@ -7,17 +9,28 @@ class ChatListShowcasePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Chat List Showcase')),
-      body: ListView.separated(
-        separatorBuilder: (context, index) {
-          return Divider(
-            color: Theme.of(
-              context,
-            ).unselectedWidgetColor.withValues(alpha: 0.5),
-          );
-        },
-        itemCount: 20,
-        itemBuilder: (context, index) => ChatListItemShowcase(index: index),
+      appBar: AppBar(
+        title: Text('Chat List Showcase'),
+        actions: [PlusIconWidget(onPressed: () {})],
+      ),
+      body: Column(
+        children: [
+          ActerSearchWidget(onChanged: (value) {}, onClear: () {}),
+          Expanded(
+            child: ListView.separated(
+              separatorBuilder: (context, index) {
+                return Divider(
+                  color: Theme.of(
+                    context,
+                  ).unselectedWidgetColor.withValues(alpha: 0.5),
+                );
+              },
+              itemCount: 20,
+              itemBuilder:
+                  (context, index) => ChatListItemShowcase(index: index),
+            ),
+          ),
+        ],
       ),
     );
   }
