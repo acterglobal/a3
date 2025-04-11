f4d4701dedb620edbe6ee07c77042648f9b9a507
kumarpalsinh25 <info@dajiraj.com>
2025-04-11 11:51:46 +0530

    Use ConvoCard if ChatNG is not enabled

--- app/lib/features/chat_ng/widgets/chat_item_widget.dart
+++ app/lib/features/chat_ng/widgets/chat_item_widget.dart
@@ -1,4 +1,5 @@
 import 'package:acter/common/providers/chat_providers.dart';
+import 'package:acter/features/chat/widgets/convo_card.dart';
 import 'package:acter/features/chat/widgets/room_avatar.dart';
 import 'package:acter/features/chat_ng/widgets/chat_item/chat_typing_lastmessage_container_widget.dart';
 import 'package:acter/features/chat_ng/widgets/chat_item/display_name_widget.dart';
@@ -6,6 +7,8 @@ import 'package:acter/features/chat_ng/widgets/chat_item/last_message_time_widge
 import 'package:acter/features/chat_ng/widgets/chat_item/bookmark_icon_widget.dart';
 import 'package:acter/features/chat_ng/widgets/chat_item/mute_icon_widget.dart';
 import 'package:acter/features/chat_ng/widgets/chat_item/unread_count_widget.dart';
+import 'package:acter/features/labs/model/labs_features.dart';
+import 'package:acter/features/labs/providers/labs_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -25,8 +28,19 @@ class ChatItemWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final inner = _buildChatItem(context, ref);
+    //If chatNG is not enabled, we render the ConvoCard
+    final isChatNG = ref.watch(isActiveProvider(LabsFeature.chatNG));
+    if (!isChatNG) {
+      return ConvoCard(
+        animation: animation,
+        showSelectedIndication: showSelectedIndication,
+        roomId: roomId,
+        onTap: onTap,
+      );
+    }
 
+    //If chatNG is enabled, we render the ChatItemWidget
+    final inner = _buildChatItem(context, ref);
     return animation != null
         ? SizeTransition(sizeFactor: animation!, child: inner)
         : inner;
@@ -35,7 +49,6 @@ class ChatItemWidget extends ConsumerWidget {
   Widget _buildChatItem(BuildContext context, WidgetRef ref) {
     final isChatSelected =
         showSelectedIndication && roomId == ref.watch(selectedChatIdProvider);
-
     return ListTile(
       selected: isChatSelected,
       selectedTileColor: Theme.of(context).colorScheme.primary,
