30bc21e7afa7c185b3644731dffbe5b21ef69560
Benjamin Kampmann <ben@acter.global>
2025-04-16 12:40:07 +0100

    manual converter function

--- native/acter/src/api_next/support.rs
+++ native/acter/src/api_next/support.rs
@@ -6,10 +6,13 @@ use acter_core::client;
 
 use crate::api::{Client};
 use super::client::UniffiClient;
+use std::os::raw::c_void;
 
 
 #[no_mangle]
-pub extern "C" fn to_client_ref(client_ref: isize) -> UniffiClient {
+pub extern "C" fn to_uniffi_client(client_ref: isize) -> *const c_void {
+    // manually convert the pointer to a Client, then wrap it into a uniffi client in an Arc
     let client = unsafe { &mut *(client_ref as *mut Client) };
-    UniffiClient { client: Arc::new(client.clone()) }
+    let uniffi_client = UniffiClient::wrap(client.clone());
+    Arc::into_raw(Arc::new(uniffi_client)) as *const c_void
 }
