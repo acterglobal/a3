be3175955c5dc57c1135e4422d424d60760adf20
anisha-e10 <anisha.e10@dajiraj.com>
2025-04-17 18:37:11 +0530

    notification settings if else condition managed

--- app/lib/features/notifications/widgets/labs_notifications_settings_tile.dart
+++ app/lib/features/notifications/widgets/labs_notifications_settings_tile.dart
@@ -1,18 +1,12 @@
 import 'dart:io';
 import 'package:acter/config/notifications/init.dart';
-import 'package:acter/features/device_permissions/notification.dart';
-import 'package:acter/features/home/providers/client_providers.dart';
-import 'package:acter/features/notifications/pages/notification_permission_page.dart';
 import 'package:acter/features/notifications/providers/notification_settings_providers.dart';
-import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:acter/l10n/generated/l10n.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:logging/logging.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:acter/features/notifications/actions/notification_sync_actions.dart';
 
-final _log = Logger('a3::settings::labs_notifications');
 
 final isOnSupportedPlatform = Platform.isAndroid || Platform.isIOS;
 
@@ -29,87 +23,18 @@ class _LabNotificationSettingsTile extends ConsumerWidget {
         (!isOnSupportedPlatform && ntfyServer.isNotEmpty);
     return SettingsTile.switchTile(
       title: Text(title ?? lang.mobilePushNotifications),
-      description: !canPush ? Text(lang.noPushServerConfigured) : null,
+      description: canPush ? Text(lang.noPushServerConfigured) : null,
       initialValue:
-          canPush &&
+          !canPush &&
           (ref.watch(isPushNotificationsActiveProvider).valueOrNull ?? true),
-      enabled: canPush,
-      onToggle: (newVal) => onToggle(context, ref, newVal),
+      enabled: !canPush,
+      onToggle:
+          (newVal) => handleNotificationSyncToggle(
+            context: context,
+            ref: ref,
+            newValue: newVal,
+          ),
     );
-  }
-
-  Future<void> onToggle(
-    BuildContext context,
-    WidgetRef ref,
-    bool newVal,
-  ) async {
-    if (newVal) {
-      final askPermission = await shouldShowNotificationPermissionInfoPage();
-      if (askPermission) {
-        if (!context.mounted) return;
-        final granted = await showDialog<bool>(
-          context: context,
-          barrierDismissible: false,
-          builder: (BuildContext context) {
-            return Dialog.fullscreen(
-              child: const NotificationPermissionWidget(),
-            );
-          },
-        );
-
-        if (granted != true) {
-          ref.read(isPushNotificationsActiveProvider.notifier).set(false);
-          return;
-        }
-      }
-      if (context.mounted) {
-        await _setupPushNotifications(context, ref);
-      }
-    }
-  }
-
-  Future<void> _setupPushNotifications(
-    BuildContext context,
-    WidgetRef ref,
-  ) async {
-    final lang = L10n.of(context);
-    final client = await ref.read(alwaysClientProvider.future);
-
-    EasyLoading.show(status: lang.changingSettings);
-    try {
-      var granted = await setupPushNotifications(client, forced: true);
-      if (granted) {
-        EasyLoading.dismiss();
-        ref.read(isPushNotificationsActiveProvider.notifier).set(true);
-        return;
-      }
-
-      await AppSettings.openAppSettings(type: AppSettingsType.notification);
-      granted = await setupPushNotifications(client, forced: true);
-      if (granted) {
-        EasyLoading.dismiss();
-        ref.read(isPushNotificationsActiveProvider.notifier).set(true);
-        return;
-      }
-      // second attempt, even sending the user to the settings, they do not
-      // approve. Let’s kick it back off
-      ref.read(isPushNotificationsActiveProvider.notifier).set(false);
-      if (!context.mounted) {
-        EasyLoading.dismiss();
-        return;
-      }
-      EasyLoading.showToast(lang.changedPushNotificationSettingsSuccessfully);
-    } catch (e, s) {
-      _log.severe('Failed to change settings of push notification', e, s);
-      if (!context.mounted) {
-        EasyLoading.dismiss();
-        return;
-      }
-      EasyLoading.showError(
-        lang.failedToChangePushNotificationSettings(e),
-        duration: const Duration(seconds: 3),
-      );
-    }
   }
 }
 
