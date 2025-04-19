804e33958671dc70ba41f5dc4d1de94f6fc5dfdd
kumarpalsinh25 <info@dajiraj.com>
2025-04-19 11:52:54 +0530

    Added golden tests

---
+++ app/test/features/chat_ng/widgets/chat_item/last_meessage_widgets/profile_changes_message_event.dart
@@ -0,0 +1,38 @@
+import 'package:acter/features/chat_ng/rooms_list/widgets/chat_item_widget.dart';
+import 'package:acter/features/chat_ui_showcase/models/convo_showcase_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../../../../../helpers/font_loader.dart';
+import '../../../../../helpers/test_util.dart';
+
+void main() {
+  group('Chat NG - Last message widgets', () {
+    testWidgets('Profile changes message event widget', (tester) async {
+      await loadTestFonts();
+
+      await tester.pumpProviderWidget(
+        overrides: [],
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            ChatItemWidget(roomId: profileEventDisplayNameChangedRoom36.roomId),
+            ChatItemWidget(roomId: profileEventDisplayNameSetRoom37.roomId),
+            ChatItemWidget(roomId: profileEventDisplayNameUnsetRoom38.roomId),
+            ChatItemWidget(roomId: profileEventAvatarChangedRoom39.roomId),
+            ChatItemWidget(roomId: profileEventAvatarSetRoom40.roomId),
+            ChatItemWidget(roomId: profileEventAvatarUnsetRoom41.roomId),
+          ],
+        ),
+      );
+
+      await tester.pump(Duration(seconds: 1));
+      await tester.pump(Duration(seconds: 1));
+      await tester.pump(Duration(seconds: 1));
+
+      await expectLater(
+        find.byType(ListView),
+        matchesGoldenFile('goldens/profile_changes_message_event.png'),
+      );
+    });
+  });
+}
---
+++ app/test/features/chat_ng/widgets/chat_item/last_meessage_widgets/text_and_media_message_event.dart
@@ -0,0 +1,38 @@
+import 'package:acter/features/chat_ng/rooms_list/widgets/chat_item_widget.dart';
+import 'package:acter/features/chat_ui_showcase/models/convo_showcase_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../../../../../helpers/font_loader.dart';
+import '../../../../../helpers/test_util.dart';
+
+void main() {
+  group('Chat NG - Last message widgets', () {
+    testWidgets('Text and media message event widget', (tester) async {
+      await loadTestFonts();
+
+      await tester.pumpProviderWidget(
+        overrides: [],
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            ChatItemWidget(roomId: davidDmRoom15.roomId),
+            ChatItemWidget(roomId: imageMessageDmRoom16.roomId),
+            ChatItemWidget(roomId: videoMessageDmRoom17.roomId),
+            ChatItemWidget(roomId: audioMessageDmRoom18.roomId),
+            ChatItemWidget(roomId: fileMessageDmRoom19.roomId),
+            ChatItemWidget(roomId: locationMessageDmRoom20.roomId),
+          ],
+        ),
+      );
+
+      await tester.pump(Duration(seconds: 1));
+      await tester.pump(Duration(seconds: 1));
+      await tester.pump(Duration(seconds: 1));
+
+      await expectLater(
+        find.byType(ListView),
+        matchesGoldenFile('goldens/text_and_media_message_event.png'),
+      );
+    });
+  });
+}
