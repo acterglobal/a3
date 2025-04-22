13bfad28def4be38d6751f771cf6d75fddecd4fa
anisha-e10 <anisha.e10@dajiraj.com>
2025-04-22 11:38:58 +0530

    PR feedback

--- app/lib/features/activities/widgets/space_activities_section/item_widgets/type_widgets/roomAvatar.dart
+++ app/lib/features/activities/widgets/space_activities_section/item_widgets/type_widgets/roomAvatar.dart
@@ -1,18 +1,23 @@
+import 'package:acter/common/providers/room_providers.dart';
 import 'package:acter/features/activities/widgets/space_activities_section/item_widgets/activity_space_container_widget.dart';
 import 'package:acter/l10n/generated/l10n.dart';
+import 'package:acter_avatar/acter_avatar.dart';
 import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
 import 'package:flutter/material.dart';
-
-class ActivityRoomAvatarItemWidget extends StatelessWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+class ActivityRoomAvatarItemWidget extends ConsumerWidget {
   final Activity activity;
   const ActivityRoomAvatarItemWidget({super.key, required this.activity});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final avatarInfo = ref.watch(roomAvatarInfoProvider(activity.roomIdStr()));
+     final memberInfo = ref.watch(
+      memberAvatarInfoProvider((roomId: activity.roomIdStr(), userId: activity.senderIdStr())),
+    );
     return ActivitySpaceProfileChangeContainerWidget(
-      updatedText: L10n.of(context).spaceAvatarUpdate,
-      userId: activity.senderIdStr(),
-      roomId: activity.roomIdStr(),
+      leadingWidget: ActerAvatar(options: AvatarOptions(avatarInfo, size: 50)),
+      titleText: L10n.of(context).spaceAvatarUpdate(memberInfo.displayName ?? activity.senderIdStr()),
       originServerTs: activity.originServerTs(),
     );
   }
--- app/lib/features/activities/widgets/space_activities_section/item_widgets/type_widgets/roomTopic.dart
+++ app/lib/features/activities/widgets/space_activities_section/item_widgets/type_widgets/roomTopic.dart
@@ -1,27 +1,30 @@
+import 'package:acter/common/providers/room_providers.dart';
 import 'package:acter/features/activities/widgets/space_activities_section/item_widgets/activity_space_container_widget.dart';
 import 'package:acter/l10n/generated/l10n.dart';
 import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
-class ActivityRoomTopicItemWidget extends StatelessWidget {
+class ActivityRoomTopicItemWidget extends ConsumerWidget {
   final Activity activity;
   const ActivityRoomTopicItemWidget({super.key, required this.activity});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
    final subTitle = activity.roomTopic() ?? '';
+    final memberInfo = ref.watch(
+      memberAvatarInfoProvider((roomId: activity.roomIdStr(), userId: activity.senderIdStr())),
+    );
     return ActivitySpaceProfileChangeContainerWidget(
-      actionIcon: PhosphorIconsRegular.pencilSimpleLine,
-      updatedText: L10n.of(context).spaceDescriptionUpdate, 
-      subtitle: Text(
+      leadingWidget: Icon(PhosphorIconsRegular.pencilSimpleLine, size: 40),
+      titleText: L10n.of(context).spaceDescriptionUpdate(memberInfo.displayName ?? activity.senderIdStr()), 
+      subtitleWidget: Text(
         subTitle,
         style: Theme.of(context).textTheme.labelMedium,
         maxLines: 2,
         overflow: TextOverflow.ellipsis,
       ),
-      userId: activity.senderIdStr(),
-      roomId: activity.roomIdStr(),
       originServerTs: activity.originServerTs(),
     );
   }
