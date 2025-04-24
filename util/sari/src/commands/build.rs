aa2b30670bdc613078f9ca058434e8c6b4ac714a
Benjamin Kampmann <ben@acter.global>
2025-04-24 11:29:15 +0100

    new ffi generation code

--- util/sari/src/commands/build.rs
+++ util/sari/src/commands/build.rs
@@ -5,16 +5,29 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 pub struct BuildOpts {
-    #[arg(long, default_value = "./native/acter/src/acter.udl")]
+    #[arg(long, default_value = "native/acter/src/acter.udl")]
     udl_path: String,
 
-    #[arg(long, default_value = "./native/acter/uniffi.toml")]
+    #[arg(long, default_value = "native/acter/uniffi.toml")]
     uniffi_toml: String,
 
-    #[arg(long, default_value = "./packages/rust_sdk/lib/")]
+    #[arg(long, default_value = "packages/rust_sdk/lib/")]
     out_dir: String,
 
-    #[arg(long, default_value = "./packages/rust_sdk/linux/libacter.so")]
+    #[cfg(target_os = "linux")]
+    #[arg(long, default_value = "packages/rust_sdk/linux/libacter.so")]
+    lib_path: String,
+
+    #[cfg(target_os = "macos")]
+    #[arg(long, default_value = "packages/rust_sdk/macos/libacter.dylib")]
+    lib_path: String,
+
+    #[cfg(target_os = "windows")]
+    #[arg(long, default_value = "packages/rust_sdk/windows/acter.dll")]
+    lib_path: String,
+    
+    #[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
+    #[arg(long)]
     lib_path: String,
 }
 
