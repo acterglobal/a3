e153256f141d5141b89582772cdcf87cf2c52db1
anisha-e10 <anisha.e10@dajiraj.com>
2025-05-08 12:20:33 +0530

    Added hasSpaceRedeemedInInviteCodeProvider

--- app/lib/features/onboarding/pages/onboarding_page.dart
+++ app/lib/features/onboarding/pages/onboarding_page.dart
@@ -48,6 +48,8 @@ class _OnboardingPageState extends ConsumerState<OnboardingPage> {
   }
 
   List<Widget> _buildOnboardingScreens(OnboardingPermissions permissions) {
+    final hasSpaceRedeemedInvites = ref.watch(hasSpaceRedeemedInInviteCodeProvider);
+
     return [
       if (!widget.isLoginOnboarding!) ...[
         SaveUsernamePage(
@@ -59,7 +61,7 @@ class _OnboardingPageState extends ConsumerState<OnboardingPage> {
         LinkEmailPage(callNextPage: () => _nextPage()),
         UploadAvatarPage(callNextPage: () => _nextPage()),
         CustomizationPage(callNextPage: () => _nextPage()),
-        if (ref.watch(hasSpacesProvider) == false)
+        if (!hasSpaceRedeemedInvites)
           RecommendedSpacesPage(callNextPage: () => _nextPage()),
       ],
       if (permissions.showNotificationPermission)
