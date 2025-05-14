f5b26b2e7514e8cb1659f10e195e1f806718a405
kumarpalsinh25 <info@dajiraj.com>
2025-05-14 15:14:43 +0530

    Improved reaction UI on message

--- app/lib/features/chat_ng/widgets/reactions/reaction_chips_widget.dart
+++ app/lib/features/chat_ng/widgets/reactions/reaction_chips_widget.dart
@@ -1,4 +1,3 @@
-import 'package:acter/common/extensions/acter_build_context.dart';
 import 'package:acter/common/themes/app_theme.dart';
 import 'package:acter/features/chat_ng/providers/chat_room_messages_provider.dart';
 import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart'
@@ -19,37 +18,26 @@ class ReactionChipsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-    final isLargeScreen = context.isLargeScreen;
-    return Card(
-      elevation: 8,
-      margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
-      color: colorScheme.surface,
-      shape: RoundedRectangleBorder(
-        side: BorderSide(color: colorScheme.secondaryContainer),
-        borderRadius: BorderRadius.circular(12),
+    return Container(
+      constraints: BoxConstraints(
+        maxWidth: MediaQuery.of(context).size.width * 0.65,
       ),
-      child: Padding(
-        padding:
-            isLargeScreen
-                ? EdgeInsets.all(3)
-                : EdgeInsets.symmetric(horizontal: 3),
-        child: Wrap(
-          direction: Axis.horizontal,
-          spacing: 3,
-          runSpacing: 3,
-          children:
-              reactions
-                  .map(
-                    (reaction) => _ReactionChip(
-                      emoji: reaction.$1,
-                      records: reaction.$2,
-                      onTap: () => onReactionTap(reaction.$1),
-                      onLongPress: onReactionLongPress,
-                    ),
-                  )
-                  .toList(),
-        ),
+      margin: EdgeInsets.symmetric(horizontal: 6),
+      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+      child: Wrap(
+        spacing: 6,
+        runSpacing: 6,
+        children:
+            reactions
+                .map(
+                  (reaction) => _ReactionChip(
+                    emoji: reaction.$1,
+                    records: reaction.$2,
+                    onTap: () => onReactionTap(reaction.$1),
+                    onLongPress: onReactionLongPress,
+                  ),
+                )
+                .toList(),
       ),
     );
   }
@@ -79,21 +67,30 @@ class _ReactionChip extends StatelessWidget {
       onTap: onTap,
       onLongPress: onLongPress,
       child: Chip(
-        padding: const EdgeInsets.symmetric(horizontal: 6),
+        padding: EdgeInsets.only(right: moreThanOne ? 6 : 3),
         color: WidgetStatePropertyAll(
-          sentByMe ? colorScheme.secondaryContainer : colorScheme.surface,
+          sentByMe
+              ? colorScheme.primary.withValues(alpha: 0.25)
+              : colorScheme.surface,
         ),
         visualDensity: VisualDensity.compact,
-        labelPadding:
-            sentByMe ? EdgeInsets.symmetric(horizontal: 3) : EdgeInsets.zero,
-        shape: const StadiumBorder(side: BorderSide(color: Colors.transparent)),
+        labelPadding: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+          side: BorderSide(
+            color:
+                sentByMe
+                    ? colorScheme.primary.withValues(alpha: 0.7)
+                    : Colors.white12,
+          ),
+        ),
         avatar: _buildEmojiText(),
-        labelStyle: Theme.of(context).textTheme.labelLarge,
+        labelStyle: Theme.of(context).textTheme.labelSmall,
         label:
             moreThanOne
                 ? Text(
                   records.length.toString(),
-                  style: Theme.of(context).textTheme.labelLarge,
+                  style: Theme.of(context).textTheme.labelSmall,
                 )
                 : const SizedBox.shrink(),
       ),
@@ -103,7 +100,7 @@ class _ReactionChip extends StatelessWidget {
   Widget _buildEmojiText() {
     return Text(
       emoji,
-      style: EmojiConfig.emojiTextStyle?.copyWith(fontSize: 18),
+      style: EmojiConfig.emojiTextStyle?.copyWith(fontSize: 14),
     );
   }
 }
