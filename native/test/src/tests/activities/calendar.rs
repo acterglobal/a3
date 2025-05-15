9cff98c1d2ac605b0364559dc5686b26ee53d793
bitfriend <bitcapture@hotmail.com>
2025-05-15 17:44:46 +0800

    Implement the missed test case from codecov

--- native/test/src/tests/activities/calendar.rs
+++ native/test/src/tests/activities/calendar.rs
@@ -118,5 +118,10 @@ async fn calendar_update_activity() -> Result<()> {
         Some(end_time)
     );
 
+    let object = activity.object().expect("we have an object");
+    assert_eq!(object.type_str(), "event");
+    assert_eq!(object.utc_start(), None);
+    assert_eq!(object.utc_end(), Some(end_time));
+
     Ok(())
 }
