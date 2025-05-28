acb1e5b86cd1a9cd782efc82e1c4da2a42b6502a
bitfriend <bitcapture@hotmail.com>
2025-05-28 18:48:33 +0800

    Fix lint error from git workflow

--- native/test/src/tests/news.rs
+++ native/test/src/tests/news.rs
@@ -791,7 +791,7 @@ async fn news_read_receipt_test() -> Result<()> {
         .await?;
 
         let receipts_manager = main_receipts_manager.clone();
-        Retry::spawn(retry_strategy, move || {
+        Retry::spawn(retry_strategy.clone(), move || {
             let receipts_manager = receipts_manager.clone();
             async move {
                 let new_receipts_manager = receipts_manager.reload().await?;
@@ -950,7 +950,7 @@ async fn multi_news_read_receipt_test() -> Result<()> {
         .await?;
 
         let receipts_manager = older_slide_rr_manager.clone();
-        Retry::spawn(retry_strategy, move || {
+        Retry::spawn(retry_strategy.clone(), move || {
             let receipts_manager = receipts_manager.clone();
             async move {
                 let new_receipts_manager = receipts_manager.reload().await?;
