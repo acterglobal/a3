3465a5b2eb5244654c7e2513cee22fb39b2628df
kumarpalsinh25 <info@dajiraj.com>
2025-05-28 12:56:49 +0530

    Added golden test

---
+++ app/test/features/attachments/golden_attachment_selection_modal_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:acter/features/attachments/actions/select_attachment.dart';
+import '../../helpers/test_util.dart';
+
+typedef OnAttachmentSelected =
+    Future<void> Function(List files, dynamic attachmentType);
+typedef OnLinkSelected = Future<void> Function(String title, String link);
+
+void main() {
+  testWidgets('AttachmentSelectionModal golden', (tester) async {
+    await tester.pumpProviderWidget(
+      child: Material(
+        child: AttachmentSelectionModal(
+          onSelected: (files, type) async {},
+          onLinkSelected: (title, link) async {},
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+    await expectLater(
+      find.byType(AttachmentSelectionModal),
+      matchesGoldenFile('goldens/attachment_selection_modal.png'),
+    );
+  });
+}
