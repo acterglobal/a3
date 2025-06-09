331e58a1580f2cd2ccef9059ab11eea4297f65c1
bitfriend <bitcapture@hotmail.com>
2025-06-09 22:16:10 +0800

    Organize logic inside fn

--- native/test/src/utils.rs
+++ native/test/src/utils.rs
@@ -80,19 +80,19 @@ pub async fn random_users_with_random_space(
         "The count of observers must be more than 0"
     );
     let (main_user, uuid) = random_user_with_uuid(prefix).await?;
-    let mut settings = CreateSpaceSettingsBuilder::default();
-    settings.name(format!("it-room-{prefix}-{uuid}"));
+    let (settings, mut users) = {
+        let mut builder = CreateSpaceSettingsBuilder::default();
+        builder.name(format!("it-room-{prefix}-{uuid}"));
 
-    let mut users = vec![];
-    for _x in 0..observer_count {
-        let (new_user, _uuid) = random_user_with_uuid(prefix).await?;
-        settings.add_invitee(new_user.user_id()?.to_string())?;
-        users.push(new_user)
-    }
-
-    let room_id = main_user
-        .create_acter_space(Box::new(settings.build()?))
-        .await?;
+        let mut users = vec![];
+        for _x in 0..observer_count {
+            let (new_user, _uuid) = random_user_with_uuid(prefix).await?;
+            builder.add_invitee(new_user.user_id()?.to_string())?;
+            users.push(new_user);
+        }
+        (builder.build()?, users)
+    };
+    let room_id = main_user.create_acter_space(Box::new(settings)).await?;
 
     for user in users.iter() {
         loop {
