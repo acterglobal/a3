15d29dd9157ac93639e77f80689d80c2f5fda829
anisha-e10 <anisha.e10@dajiraj.com>
2025-06-10 17:07:02 +0530

    event detail page test cases added

--- app/test/helpers/mock_calendar_event.dart
+++ app/test/helpers/mock_calendar_event.dart
@@ -1,32 +1,73 @@
 // Mock classes
 import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
 import 'package:mocktail/mocktail.dart';
-
-import '../features/activity/item_widgets/activity_event_item_widget_test.dart';
-import 'mock_event_providers.dart';
+import 'mock_event_location.dart';
+import 'mock_a3sdk.dart' as a3sdk;
 
 class MockCalendarEvent extends Mock implements CalendarEvent {
+  final RsvpStatus? rsvpStatus;
+  final String? _title;
+  final String? _eventId;
+  final String? _roomId;
+  final TextMessageContent? _description;
+  final List<EventLocationInfo>? _locations;
+  final int? _startTime;
+  final int? _endTime;
+
+  MockCalendarEvent({
+    this.rsvpStatus,
+    String? title,
+    String? eventId,
+    String? roomId,
+    TextMessageContent? description,
+    List<EventLocationInfo>? locations,
+    int? startTime,
+    int? endTime,
+  }) : _title = title,
+       _eventId = eventId,
+       _roomId = roomId,
+       _description = description,
+       _locations = locations,
+       _startTime = startTime,
+       _endTime = endTime;
+
   @override
-  String title() => 'Test Event';
-  
+  String title() => _title ?? 'Test Event';
+
   @override
-  UtcDateTime utcStart() => MockUtcDateTime();
-  
+  EventId eventId() => a3sdk.MockEventId(id: _eventId ?? 'test-event-id');
+
   @override
-  UtcDateTime utcEnd() => MockUtcDateTime();
-  
+  String roomIdStr() => _roomId ?? 'test-room-id';
+
   @override
-  String roomIdStr() => 'test-space-id';
-  
+  TextMessageContent? description() => _description;
+
+  @override
+  FfiListEventLocationInfo locations() =>
+      MockFfiListEventLocationInfo(items: _locations ?? []);
+
+  @override
+  UtcDateTime utcStart() => a3sdk.MockUtcDateTime(
+    millis: _startTime ?? DateTime.now().millisecondsSinceEpoch,
+  );
+
+  @override
+  UtcDateTime utcEnd() => a3sdk.MockUtcDateTime(
+    millis:
+        _endTime ??
+        (_startTime ?? DateTime.now().millisecondsSinceEpoch) + 3600000,
+  );
+
   @override
   Future<RsvpManager> rsvps() async => MockRsvpManager();
-  
+
   @override
-  TextMessageContent? description() => null;
+  Future<OptionRsvpStatus> respondedByMe() async =>
+      a3sdk.MockOptionRsvpStatus(rsvpStatus);
 }
 
-class MockCalendarEventDraft extends Mock implements CalendarEventDraft {
-}
+class MockCalendarEventDraft extends Mock implements CalendarEventDraft {}
 
 class MockRsvpManager extends Mock implements RsvpManager {
   @override
@@ -35,8 +76,8 @@ class MockRsvpManager extends Mock implements RsvpManager {
 
 class MockRsvpDraft extends Mock implements RsvpDraft {
   @override
-  Future<EventId> send() async => MockEventId('test-rsvp-id');
-  
+  Future<EventId> send() async => a3sdk.MockEventId(id: 'test-rsvp-id');
+
   @override
   void status(String status) {}
 }
--- app/test/helpers/mock_event_location.dart
+++ app/test/helpers/mock_event_location.dart
@@ -6,4 +6,58 @@ class MockEventDraftLocationsNotifier extends EventDraftLocationsNotifier {
   MockEventDraftLocationsNotifier() : super();
 }
 
-class MockEventLocationInfo extends Mock implements EventLocationInfo {}
+class MockEventLocationInfo extends Mock implements EventLocationInfo {
+  final String? _name;
+  final String _locationType;
+  final String? _address;
+  final String? _uri;
+  final String? _notes;
+  final TextMessageContent? _description;
+  final String? _coordinates;
+
+  MockEventLocationInfo({
+    String? name,
+    String? locationType,
+    String? address,
+    String? uri,
+    String? notes,
+    TextMessageContent? description,
+    String? coordinates,
+  })  : _name = name,
+        _locationType = locationType ?? 'physical',
+        _address = address,
+        _uri = uri,
+        _notes = notes,
+        _description = description,
+        _coordinates = coordinates;
+
+  @override
+  String? name() => _name;
+
+  @override
+  String locationType() => _locationType;
+
+  @override
+  String? address() => _address;
+
+  @override
+  String? uri() => _uri;
+
+  @override
+  String? notes() => _notes;
+
+  @override
+  TextMessageContent? description() => _description;
+
+  @override
+  String? coordinates() => _coordinates;
+}
+
+class MockFfiListEventLocationInfo extends Mock implements FfiListEventLocationInfo {
+  final List<EventLocationInfo> items;
+
+  MockFfiListEventLocationInfo({this.items = const []});
+
+  @override
+  List<EventLocationInfo> toList({bool growable = false}) => items;
+}
