fb9cb3ae71fe371ead0ffabc51fe2f632d391350
kumarpalsinh25 <info@dajiraj.com>
2025-06-10 12:57:56 +0530

    Added MockTimelineVirtualItem

---
+++ app/lib/features/chat_ui_showcase/mocks/convo/timeline/mock_timeline_virtual_item.dart
@@ -0,0 +1,14 @@
+import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockTimelineVirtualItem extends Mock implements TimelineVirtualItem {
+  final String? mockEventId;
+  final String? mockEventType;
+  final String? mockDesc;
+
+  MockTimelineVirtualItem({
+    this.mockEventId,
+    this.mockEventType,
+    this.mockDesc,
+  });
+}
