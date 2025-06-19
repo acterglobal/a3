c1b1b05ce196e174ee11ac85e5d4665e6f5d0d56
kumarpalsinh25 <info@dajiraj.com>
2025-06-19 16:46:17 +0530

    Update audio_message_event.dart

--- app/lib/features/chat_ng/widgets/events/audio_message_event.dart
+++ app/lib/features/chat_ng/widgets/events/audio_message_event.dart
@@ -37,10 +37,6 @@ class _AudioMessageEventState extends ConsumerState<AudioMessageEvent> {
   @override
   void initState() {
     super.initState();
-    _init();
-  }
-
-  void _init() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       // listen to the audio player state changes
       ref.listenManual(audioPlayerStateProvider, (previous, next) {
@@ -66,6 +62,12 @@ class _AudioMessageEventState extends ConsumerState<AudioMessageEvent> {
   }
 
   @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final ChatMessageInfo messageInfo = (
       messageId: widget.messageId,
@@ -74,40 +76,16 @@ class _AudioMessageEventState extends ConsumerState<AudioMessageEvent> {
     final mediaState = ref.watch(mediaChatStateProvider(messageInfo));
     final isDownloading =
         mediaState.mediaChatLoadingState.isLoading || mediaState.isDownloading;
-    final mediaFile = mediaState.mediaFile;
 
-    return audioUI(mediaFile, isDownloading: isDownloading);
+    return _buildAudioEventUI(mediaState.mediaFile, isDownloading);
   }
 
-  Widget audioUI(File? mediaFile, {bool isDownloading = false}) {
-    final audioPlayerInfo = ref.watch(audioPlayerStateProvider);
-    final isPlaying =
-        audioPlayerInfo.state == PlayerState.playing &&
-        audioPlayerInfo.messageId == widget.messageId;
+  Widget _buildAudioEventUI(File? mediaFile, bool isDownloading) {
     final msgSize = widget.content.size();
 
     return Row(
       children: [
-        if (isDownloading)
-          CircularProgressIndicator()
-        else if (mediaFile == null)
-          IconButton.outlined(
-            onPressed: () async {
-              final notifier = ref.read(
-                mediaChatStateProvider((
-                  messageId: widget.messageId,
-                  roomId: widget.roomId,
-                )).notifier,
-              );
-              await notifier.downloadMedia();
-            },
-            icon: Icon(Icons.download),
-          )
-        else
-          IconButton.outlined(
-            onPressed: () => _handlePlayAudio(mediaFile),
-            icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-          ),
+        _buildAudioControls(mediaFile, isDownloading),
         SizedBox(width: 10),
         Expanded(
           child: Column(
@@ -132,8 +110,39 @@ class _AudioMessageEventState extends ConsumerState<AudioMessageEvent> {
     );
   }
 
+  Widget _buildAudioControls(File? mediaFile, bool isDownloading) {
+    final audioPlayerInfo = ref.watch(audioPlayerStateProvider);
+    final isPlaying =
+        audioPlayerInfo.state == PlayerState.playing &&
+        audioPlayerInfo.messageId == widget.messageId;
+
+    // if the media is still downloading, show a loading indicator
+    if (isDownloading) return const CircularProgressIndicator();
+
+    // if the media is not downloaded, show a download button
+    if (mediaFile == null) {
+      return IconButton.outlined(
+        onPressed: () async {
+          final notifier = ref.read(
+            mediaChatStateProvider((
+              messageId: widget.messageId,
+              roomId: widget.roomId,
+            )).notifier,
+          );
+          await notifier.downloadMedia();
+        },
+        icon: Icon(Icons.download),
+      );
+    } else {
+      // if the media is downloaded, show a play/pause button
+      return IconButton.outlined(
+        onPressed: () => _handlePlayAudio(mediaFile),
+        icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+      );
+    }
+  }
+
   Future<void> _handlePlayAudio(File mediaFile) async {
-    final mimetype = widget.content.mimetype();
     try {
       PlayerState playerState;
       switch (_player.state) {
@@ -148,7 +157,10 @@ class _AudioMessageEventState extends ConsumerState<AudioMessageEvent> {
         case PlayerState.stopped:
         default:
           await _player.play(
-            DeviceFileSource(mediaFile.path, mimeType: mimetype),
+            DeviceFileSource(
+              mediaFile.path,
+              mimeType: widget.content.mimetype(),
+            ),
           );
           playerState = PlayerState.playing;
           break;
@@ -160,11 +172,5 @@ class _AudioMessageEventState extends ConsumerState<AudioMessageEvent> {
     } catch (e, st) {
       debugPrint('Error playing audio: $e, \n $st');
     }
-  }
-
-  @override
-  void dispose() {
-    _player.dispose();
-    super.dispose();
   }
 }
