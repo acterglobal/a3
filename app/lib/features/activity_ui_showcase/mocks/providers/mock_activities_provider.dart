eadb7bc2493c4d9b8c9991b5a74df80fa091ac35
anisha-e10 <anisha.e10@dajiraj.com>
2025-06-26 16:04:40 +0530

    structure changes

--- app/lib/features/activity_ui_showcase/mocks/providers/mock_activities_provider.dart
+++ app/lib/features/activity_ui_showcase/mocks/providers/mock_activities_provider.dart
@@ -1,43 +1,8 @@
 import 'package:acter/features/activities/providers/activities_providers.dart';
+import 'package:acter/features/activity_ui_showcase/mocks/data/mock_activities_list_generator.dart';
 import 'package:acter/features/activity_ui_showcase/mocks/general/mock_activity.dart';
-import 'package:acter/features/activity_ui_showcase/mocks/showcase/data/biggervisual_usecases.dart';
-import 'package:acter/features/activity_ui_showcase/mocks/showcase/data/individual_actions_usecases.dart';
-import 'package:acter/features/activity_ui_showcase/mocks/showcase/data/membership_usecases.dart';
-import 'package:acter/features/activity_ui_showcase/mocks/showcase/data/social_usecases.dart';
-import 'package:acter/features/activity_ui_showcase/mocks/showcase/data/space_core_usecases.dart';
 import 'package:acter_flutter_sdk/acter_flutter_sdk_ffi.dart';
 import 'package:riverpod/riverpod.dart';
-
-final List<MockActivity> mockActivitiesListGenerator = [
-  pinCommentActivity1,
-  documentAttachmentActivity1,
-  reactionActivity2,
-  eventReferenceActivity1,
-  kickedActivity1,
-  bannedActivity1,
-  unbannedActivity1,
-  taskAddActivity1,
-  taskTitleChangeActivity1,
-  roomTopicChangeActivity1,
-  descriptionChangeActivity1,
-  eventCreationActivity1,
-  reactionActivity3,
-  rsvpYesActivity1,
-  rsvpMaybeActivity1,
-  knockAcceptedActivity1,
-  knockRetractedActivity1,
-  knockDeniedActivity1,
-  taskCompleteActivity1,
-  taskReopenActivity1,
-  joinedActivity1,
-  invitationAcceptedActivity1,
-  invitedActivity1,
-  leftActivity1,
-  invitationRejectedActivity1,
-  reactionActivity1,
-  roomNameActivity1,
-  roomAvatarActivity1,
-];
 
 final mockActivitiesProvider = Provider<List<MockActivity>>((ref) {
   return mockActivitiesListGenerator;
