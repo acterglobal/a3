93ed218ab880b5dc04389b5b8b8e7e5b5961fba8
anisha-e10 <anisha.e10@dajiraj.com>
2025-06-27 17:07:07 +0530

    changes

--- app/lib/features/activities/actions/activity_item_click_action.dart
+++ app/lib/features/activities/actions/activity_item_click_action.dart
@@ -16,37 +16,31 @@ void onTapActivityItem(BuildContext context, ActivityObject? activityObject) {
     return;
   }
 
-  final activityActions = {
-    'pin': () => context.pushNamed(
+  switch (activityType) {
+    case 'pin': context.pushNamed(
           Routes.pin.name,
           pathParameters: {'pinId': objectId},
-        ),
-    'task': () => context.pushNamed(
+        );
+    case 'task': context.pushNamed(
           Routes.taskItemDetails.name,
           pathParameters: {'taskId': objectId, 'taskListId': taskListId},
-        ),
-    'task-list': () => context.pushNamed(
+        );
+    case 'task-list': context.pushNamed(
           Routes.taskListDetails.name,
           pathParameters: {'taskListId': objectId},
-        ),
-    'event': () => context.pushNamed(
+        );
+    case 'event': context.pushNamed(
           Routes.calendarEvent.name,
           pathParameters: {'calendarId': objectId},
-        ),
-    'news': () => context.pushNamed(
+        );
+    case 'news': context.pushNamed(
           Routes.update.name,
           pathParameters: {'updateId': objectId},
-        ),
-    'story': () => context.pushNamed(
+        );
+    case 'story': context.pushNamed(
           Routes.update.name,
           pathParameters: {'updateId': objectId},
-        ),
+        );
+    default: _log.warning('Unknown activity type: $activityType');
   };
-
-  final action = activityActions[activityType];
-  if (action != null) {
-    action();
-  } else {
-    _log.warning('Unknown activity type: $activityType');
-  }
 }
